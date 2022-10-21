9c9d3b7eb7c290b99a320176ab1aadb28ecd7c6c
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2022-10-21 15:36:19 +0300

    Updated 5.3 Lists - Ex. 5

--- 02. C# - Fundamentals/5.3 Lists - More Exercise/05. Drum Set/Program.cs
+++ 02. C# - Fundamentals/5.3 Lists - More Exercise/05. Drum Set/Program.cs
@@ -1,12 +1,47 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace _05._Drum_Set
+namespace ConsoleApp4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double initialSavings = double.Parse(Console.ReadLine());
+            List<int> drumSet = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            List<int> initialQualities = drumSet.ToList();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Hit it again, Gabsy!")
+            {
+                int currentHit = int.Parse(command);
+
+                for (int i = 0; i < initialQualities.Count; i++)
+                {
+                    drumSet[i] -= currentHit;
+
+                    if (drumSet[i] <= 0)
+                    {
+                        if (initialSavings < (initialQualities[i] * 3))
+                        {
+                            initialQualities.RemoveAt(i);
+                            drumSet.RemoveAt(i);
+                            i--;
+                        }
+                        else
+                        {
+                            initialSavings -= (initialQualities[i] * 3);
+                            drumSet[i] = initialQualities[i];
+                        }
+
+                    }
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", drumSet));
+            Console.WriteLine($"Gabsy has {initialSavings:F2}lv.");
         }
     }
 }
