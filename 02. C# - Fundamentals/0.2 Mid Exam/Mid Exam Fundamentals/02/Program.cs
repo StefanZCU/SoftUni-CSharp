a6e7f0d7b875a269b77a3c3e677c1d002c22fbc3
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2022-10-23 09:46:13 +0300

    Added Mid Exam

---
+++ 02. C# - Fundamentals/0.2 Mid Exam/Mid Exam Fundamentals/02/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _02_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> coffeeNames = Console.ReadLine().Split(" ").ToList();
+            int numCMDS = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numCMDS; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                if (input[0] == "Include")
+                {
+                    coffeeNames.Add(input[1]);
+                }
+                else if (input[0] == "Remove")
+                {
+                    if (int.Parse(input[2]) >= 0 && int.Parse(input[2]) < coffeeNames.Count)
+                    {
+                        if (input[1] == "first")
+                        {
+                            for (int j = 0; j < int.Parse(input[2]); j++)
+                            {
+                                coffeeNames.RemoveAt(0);
+                            }
+                        }
+                        else if (input[1] == "last")
+                        {
+                            for (int j = 0; j < int.Parse(input[2]); j++)
+                            {
+                                coffeeNames.RemoveAt(coffeeNames.Count - 1);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (input[0] == "Prefer")
+                {
+                    if (int.Parse(input[1]) >= 0 && int.Parse(input[1]) < coffeeNames.Count && int.Parse(input[2]) >= 0 && int.Parse(input[2]) < coffeeNames.Count)
+                    {
+                        string tempCoffee = coffeeNames[int.Parse(input[1])];
+                        coffeeNames[int.Parse(input[1])] = coffeeNames[int.Parse(input[2])];
+                        coffeeNames[int.Parse(input[2])] = tempCoffee;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (input[0] == "Reverse")
+                {
+                    coffeeNames.Reverse();
+                }
+            }
+            Console.WriteLine("Coffees:");
+            Console.WriteLine(String.Join(" ", coffeeNames));
+        }
+    }
+}
---
+++ 02. C# - Fundamentals/0.2 Mid Exam/Mid Exam Fundamentals/03/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _03_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> bookShelf = Console.ReadLine().Split("&").ToList();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Done")
+            {
+                string[] input = command.Split(" | ");
+
+                if (input[0] == "Add Book")
+                {
+                    if (!bookShelf.Contains(input[1]))
+                    {
+                        bookShelf.Insert(0, input[1]);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (input[0] == "Take Book")
+                {
+                    if (bookShelf.Contains(input[1]))
+                    {
+                        bookShelf.Remove(input[1]);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (input[0] == "Swap Books")
+                {
+                    if (bookShelf.Contains(input[1]) && bookShelf.Contains(input[2]))
+                    {
+                        for (int i = 0; i < bookShelf.Count; i++)
+                        {
+                            if (bookShelf[i] == input[1])
+                            {
+                                bookShelf[i] = input[2];
+                            }
+                            else if (bookShelf[i] == input[2])
+                            {
+                                bookShelf[i] = input[1];
+                            }
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (input[0] == "Insert Book")
+                {
+                    if (!bookShelf.Contains(input[1]))
+                    {
+                        bookShelf.Add(input[1]);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (input[0] == "Check Book")
+                {
+                    if (int.Parse(input[1]) >= 0 && int.Parse(input[1]) < bookShelf.Count)
+                    {
+                        Console.WriteLine(bookShelf[int.Parse(input[1])]);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", bookShelf));
+        }
+    }
+}
