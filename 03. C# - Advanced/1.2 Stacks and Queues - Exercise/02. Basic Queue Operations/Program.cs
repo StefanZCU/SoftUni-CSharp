24aabdb728352c629f50e7a793e7182f5c280abd
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2022-11-15 12:20:03 +0200

    Added 02. Basic Queue Operations

--- 03. C# - Advanced/1.2 Stacks and Queues - Exercise/02. Basic Queue Operations/Program.cs
+++ 03. C# - Advanced/1.2 Stacks and Queues - Exercise/02. Basic Queue Operations/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02._Basic_Queue_Operations
 {
@@ -6,7 +8,51 @@ namespace _02._Basic_Queue_Operations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] NSX = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] numArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> numbers = new Queue<int>();
+
+            int smallestNum = int.MaxValue;
+
+            for (int i = 0; i < NSX[0]; i++)
+            {
+                numbers.Enqueue(numArray[i]);
+            }
+
+            for (int i = 0; i < NSX[1]; i++)
+            {
+                numbers.Dequeue();
+            }
+
+            if (numbers.Count >= 1)
+            {
+                if (numbers.Contains(NSX[2]))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    int originalCount = numbers.Count;
+
+                    for (int i = 0; i < originalCount; i++)
+                    {
+                        int currentNum = numbers.Dequeue();
+
+                        if (smallestNum > currentNum)
+                        {
+                            smallestNum = currentNum;
+                        }
+                    }
+
+                    Console.WriteLine(smallestNum);
+                }
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
         }
     }
 }
