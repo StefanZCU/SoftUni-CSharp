c9a5f0d15448ca014a8899f542cde7b141fc71b6
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2022-11-16 11:23:26 +0200

    Added 07. Truck Tour

--- 03. C# - Advanced/1.2 Stacks and Queues - Exercise/07. Truck Tour/Program.cs
+++ 03. C# - Advanced/1.2 Stacks and Queues - Exercise/07. Truck Tour/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 
 namespace _07._Truck_Tour
 {
@@ -8,8 +9,43 @@ namespace _07._Truck_Tour
     {
         static void Main(string[] args)
         {
-            //TODO - Figure out answer
-            
+            int numPumps = int.Parse(Console.ReadLine());
+
+            Queue<string> petrolPumps = new Queue<string>();
+
+            for (int i = 0; i < numPumps; i++)
+            {
+                petrolPumps.Enqueue(Console.ReadLine());
+            }
+
+            for (int i = 0; i < numPumps; i++)
+            {
+                BigInteger currentPetrolAmt = 0;
+                bool flag = true;
+
+                for (int j = 0; j < numPumps; j++)
+                {
+                    BigInteger[] currPump = petrolPumps.Dequeue().Split().Select(BigInteger.Parse).ToArray();
+                    petrolPumps.Enqueue(string.Join(" ", currPump));
+
+                    currentPetrolAmt += currPump[0];
+                    currentPetrolAmt -= currPump[1];
+
+                    if (currentPetrolAmt < 0)
+                    {
+                        flag = false;
+                    }
+                }
+
+                if (flag)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+
+                string temp = petrolPumps.Dequeue();
+                petrolPumps.Enqueue(temp);
+            }
         }
     }
 }
--- 03. C# - Advanced/1.2 Stacks and Queues - Exercise/09. Simple Text Editor/Program.cs
+++ 03. C# - Advanced/1.2 Stacks and Queues - Exercise/09. Simple Text Editor/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace _09._Simple_Text_Editor
 {
@@ -6,7 +8,36 @@ namespace _09._Simple_Text_Editor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi");
+            int n = int.Parse(Console.ReadLine());
+
+            //stack of queues
+
+            Queue<char> text = new Queue<char>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] cmdArgs = Console.ReadLine().Split();
+
+                if (cmdArgs[0] == "1")
+                {
+                    for (int j = 0; j < cmdArgs[1].Length; j++)
+                    {
+                        text.Enqueue(cmdArgs[1][j]);
+                    }
+                }
+                else if (cmdArgs[0] == "2")
+                {
+                    
+                }
+                else if (cmdArgs[0] == "3")
+                {
+                    
+                }
+                else if (cmdArgs[0] == "4")
+                {
+                    
+                }
+            }
         }
     }
 }
