2023fb9b7e45750a99cc9c3043113cd4669c84a8
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2022-11-19 10:49:04 +0200

    Added 12. Cups and Bottles

--- 03. C# - Advanced/1.2 Stacks and Queues - Exercise/12. Cups and Bottles/Program.cs
+++ 03. C# - Advanced/1.2 Stacks and Queues - Exercise/12. Cups and Bottles/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _12._Cups_and_Bottles
 {
@@ -6,7 +9,52 @@ namespace _12._Cups_and_Bottles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] cupCapacityArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] bottlesArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> bottles = new Stack<int>(bottlesArray);
+            Queue<int> cups = new Queue<int>(cupCapacityArray);
+
+            int wastedLiters = 0;
+
+            while (true)
+            {
+                if (bottles.Count != 0 && cups.Count != 0)
+                {
+                    if (bottles.Peek() - cups.Peek() > 0)
+                    {
+                        wastedLiters += bottles.Pop() - cups.Dequeue();
+                    }
+                    else if (bottles.Peek() - cups.Peek() == 0)
+                    {
+                        bottles.Pop();
+                        cups.Dequeue();
+                    }
+                    else if (bottles.Peek() - cups.Peek() < 0)
+                    {
+                        cups.Enqueue(cups.Dequeue() - bottles.Pop());
+
+                        for (int i = 1; i < cups.Count; i++)
+                        {
+                            cups.Enqueue(cups.Dequeue());
+                        }
+                    }
+
+                }
+                else if (bottles.Count == 0)
+                {
+                    Console.WriteLine($"Cups: {string.Join(" ", cups)}");
+                    Console.WriteLine($"Wasted litters of water: {wastedLiters}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Bottles: {string.Join(" ", bottles)}");
+                    Console.WriteLine($"Wasted litters of water: {wastedLiters}");
+                    break;
+                }
+            }
+
         }
     }
 }
