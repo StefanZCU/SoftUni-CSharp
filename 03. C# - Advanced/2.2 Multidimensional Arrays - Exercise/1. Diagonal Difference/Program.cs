fce0334b1fc612260fb63e231420afbc08859ccb
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2022-11-19 17:28:13 +0200

    Added 01. Diagonal Difference

--- 03. C# - Advanced/2.2 Multidimensional Arrays - Exercise/1. Diagonal Difference/Program.cs
+++ 03. C# - Advanced/2.2 Multidimensional Arrays - Exercise/1. Diagonal Difference/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _1._Diagonal_Difference
 {
@@ -6,7 +7,47 @@ namespace _1._Diagonal_Difference
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int rowAndColsLength = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[rowAndColsLength, rowAndColsLength];
+
+            int firstDiagonal = 0;
+            int secondDiagonal = 0;
+
+            for (int i = 0; i < rowAndColsLength; i++)
+            {
+                int[] cols = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int j = 0; j < rowAndColsLength; j++)
+                {
+                    matrix[i, j] = cols[j];
+
+                    if (i == j)
+                    {
+                        firstDiagonal += cols[j];
+                    }
+                }
+            }
+
+            int k = 0;
+            int l = matrix.GetLength(1) - 1;
+
+            while (true)
+            {
+                secondDiagonal += matrix[k, l];
+
+                if (k == matrix.GetLength(1) - 1 || l < 0)
+                {
+                    break;
+                }
+
+                k++;
+                l--;
+                
+            }
+
+            Console.WriteLine(Math.Abs(firstDiagonal - secondDiagonal));
+
         }
     }
 }
