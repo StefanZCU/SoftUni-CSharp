b3701e3648f5dfcfd4907d02fda58a8a578379db
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2022-11-19 14:25:08 +0200

    Added 07. Pascal Triangle

--- 03. C# - Advanced/2.1 Multidimensional Arrays - Lab/7. Pascal Triangle/Program.cs
+++ 03. C# - Advanced/2.1 Multidimensional Arrays - Lab/7. Pascal Triangle/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace _7._Pascal_Triangle
 {
@@ -6,7 +7,49 @@ namespace _7._Pascal_Triangle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int triangleLength = int.Parse(Console.ReadLine());
+
+            BigInteger[][] triangle = new BigInteger[triangleLength][];
+
+            int size = 3;
+
+            for (int i = 0; i < triangleLength; i++)
+            {
+                triangle[i] = new BigInteger[size];
+                size++;
+            }
+
+            triangle[0][0] = 0;
+            triangle[0][1] = 1;
+            triangle[0][2] = 0;
+
+            for (int i = 1; i < triangleLength; i++)
+            {
+                for (int j = 0; j < triangle[i].Length; j++)
+                {
+                    if (j == 0 || j + 1 == triangle[i].Length)
+                    {
+                        triangle[i][j] = 0;
+                    }
+                    else
+                    {
+                        triangle[i][j] = triangle[i -1][j - 1] + triangle[i - 1][j];
+                    }
+                }
+            }
+
+            for (int i = 0; i < triangleLength; i++)
+            {
+                for (int k = 0; k < triangle[i].Length; k++)
+                {
+                    if (triangle[i][k] != 0)
+                    {
+                        Console.Write($"{triangle[i][k]} ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
