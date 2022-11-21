01133d839ff7ba642e56f79cd5537e0a264bb1ad
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2022-11-21 18:03:55 +0200

    Added 02. Average Student Grades

--- 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/02. Average Student Grades/Program.cs
+++ 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/02. Average Student Grades/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02._Average_Student_Grades
 {
@@ -6,7 +8,38 @@ namespace _02._Average_Student_Grades
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<decimal>> studentsGrades = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] students = Console.ReadLine().Split();
+
+                if (!studentsGrades.ContainsKey(students[0]))
+                {
+                    studentsGrades.Add(students[0], new List<decimal>());
+                    studentsGrades[students[0]].Add(decimal.Parse(students[1]));
+                }
+                else
+                {
+                    studentsGrades[students[0]].Add(decimal.Parse(students[1]));
+                }
+            }
+
+            foreach (var student in studentsGrades)
+            {
+                Console.Write($"{student.Key} -> ");
+
+                foreach (var number in student.Value)
+                {
+                    Console.Write($"{number:f2} ");
+                }
+
+                Console.Write($"(avg: {student.Value.Average():F2})");
+                Console.WriteLine();
+            }
+
         }
     }
 }
