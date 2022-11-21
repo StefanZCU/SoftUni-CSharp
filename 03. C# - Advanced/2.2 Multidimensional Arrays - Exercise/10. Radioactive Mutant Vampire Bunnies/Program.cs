9c248cdab1ede112cecc64dfa9093996ed3bbc77
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2022-11-21 13:10:50 +0200

    Added progress to 10.

--- 03. C# - Advanced/2.2 Multidimensional Arrays - Exercise/10. Radioactive Mutant Vampire Bunnies/Program.cs
+++ 03. C# - Advanced/2.2 Multidimensional Arrays - Exercise/10. Radioactive Mutant Vampire Bunnies/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _10._Radioactive_Mutant_Vampire_Bunnies
 {
@@ -6,7 +7,39 @@ namespace _10._Radioactive_Mutant_Vampire_Bunnies
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] matrixSize = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int rowSize = matrixSize[0];
+            int colSize = matrixSize[1];
+
+            int currentPosRow = 0;
+            int currentPosCol = 0;
+
+            char[,] matrix = new char[rowSize, colSize];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string line = Console.ReadLine().Trim();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = line[col];
+
+                    if (line[col] == 'P')
+                    {
+                        currentPosRow = row;
+                        currentPosCol = col;
+                    }
+                }
+            }
+
+            string commands = Console.ReadLine().Trim();
+
+            for (int i = 0; i < commands.Length; i++)
+            {
+                char direction = commands[i];
+
+            }
         }
     }
 }
