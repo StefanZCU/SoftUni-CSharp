ef96f11c75d76106b0506c5050859aa77628e0d2
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2022-11-21 17:08:03 +0200

    Added Sets and Dictionaries Advanced - Exercise

---
+++ 03. C# - Advanced/3.2 Sets and Dictionaries Advanced - Exercise/09. SoftUni Exam Results/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _09._SoftUni_Exam_Results
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
