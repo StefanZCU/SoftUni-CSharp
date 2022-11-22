0f10b8224a596ebab10660fa8812f7023a66b4e9
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2022-11-22 12:21:53 +0200

    Added 04. Product Shop

--- 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/04. Product Shop/Program.cs
+++ 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/04. Product Shop/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _04._Product_Shop
 {
@@ -6,7 +8,34 @@ namespace _04._Product_Shop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, Dictionary<string, double>> shopItems = new Dictionary<string, Dictionary<string, double>>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Revision")
+            {
+                string[] input = command.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                string store = input[0];
+                string item = input[1];
+                double price = double.Parse(input[2]);
+
+                if (!shopItems.ContainsKey(store))
+                {
+                    shopItems.Add(store, new Dictionary<string, double>());
+                }
+
+                shopItems[store].Add(item, price);
+            }
+
+            foreach (var store in shopItems.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{store.Key}->");
+
+                foreach (var items in store.Value)
+                {
+                    Console.WriteLine($"Product: {items.Key}, Price: {items.Value}");
+                }
+            }
         }
     }
 }
