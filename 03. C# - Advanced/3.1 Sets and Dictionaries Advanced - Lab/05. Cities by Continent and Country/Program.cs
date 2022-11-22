d4166b77d714e44f787a4b33ff6f0f87c0625c6c
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2022-11-22 12:29:05 +0200

    Added 05. Cities by Continent and Country

--- 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/05. Cities by Continent and Country/Program.cs
+++ 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/05. Cities by Continent and Country/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _05._Cities_by_Continent_and_Country
 {
@@ -6,7 +7,41 @@ namespace _05._Cities_by_Continent_and_Country
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, Dictionary<string, List<string>>> citiesPerCountryPerCont =
+                new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string continent = input[0];
+                string country = input[1];
+                string city = input[2];
+
+                if (!citiesPerCountryPerCont.ContainsKey(continent))
+                {
+                    citiesPerCountryPerCont.Add(continent, new Dictionary<string, List<string>>());
+                }
+
+                if (!citiesPerCountryPerCont[continent].ContainsKey(country))
+                {
+                    citiesPerCountryPerCont[continent].Add(country, new List<string>());
+                }
+
+                citiesPerCountryPerCont[continent][country].Add(city);
+            }
+
+            foreach (var continent in citiesPerCountryPerCont)
+            {
+                Console.WriteLine($"{continent.Key}:");
+
+                foreach (var country in continent.Value)
+                {
+                    Console.WriteLine($"  {country.Key} -> {string.Join(", ", country.Value)}");
+                }
+            }
         }
     }
 }
