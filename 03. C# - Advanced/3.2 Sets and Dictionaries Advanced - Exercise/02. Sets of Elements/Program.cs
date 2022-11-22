a331d8951317c78cbd255eeb18a2bee4f306278f
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2022-11-22 15:53:53 +0200

    Added temp 02.

--- 03. C# - Advanced/3.2 Sets and Dictionaries Advanced - Exercise/02. Sets of Elements/Program.cs
+++ 03. C# - Advanced/3.2 Sets and Dictionaries Advanced - Exercise/02. Sets of Elements/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _02._Sets_of_Elements
 {
@@ -6,7 +8,30 @@ namespace _02._Sets_of_Elements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+            int[] numCycles = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < numCycles[0] + numCycles[1]; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (!numbers.ContainsKey(num))
+                {
+                    numbers.Add(num, 0);
+                }
+
+                numbers[num]++;
+            }
+
+            foreach (var num in numbers)
+            {
+                if (num.Value > 1)
+                {
+                    Console.Write($"{num.Key} ");
+                }
+            }
+
         }
     }
 }
