e4966bf62d74ea509153b45071a64b52311a8221
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2022-11-22 12:48:18 +0200

    Added 08. SoftUni Party

--- 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/08. SoftUni Party/Program.cs
+++ 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/08. SoftUni Party/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _08._SoftUni_Party
 {
@@ -6,7 +8,43 @@ namespace _08._SoftUni_Party
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<string> allGuests = new HashSet<string>();
+            HashSet<string> vipGuests = new HashSet<string>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "PARTY")
+            {
+                allGuests.Add(command);
+            }
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                if (allGuests.Contains(command))
+                {
+                    allGuests.Remove(command);
+                }
+            }
+
+            Console.WriteLine(allGuests.Count);
+
+            foreach (var guest in allGuests)
+            {
+                if (char.IsDigit(guest[0]))
+                {
+                    vipGuests.Add(guest);
+                }
+            }
+
+            foreach (var vip in vipGuests)
+            {
+                allGuests.Remove(vip);
+                Console.WriteLine(vip);
+            }
+
+            foreach (var guest in allGuests)
+            {
+                Console.WriteLine(guest);
+            }
         }
     }
 }
