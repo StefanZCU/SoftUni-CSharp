47c51152cad230b91fd329c8a924c7ffaac01e1b
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2022-11-22 12:31:31 +0200

    Added 06. Record Unique Names

--- 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Program.cs
+++ 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _06._Record_Unique_Names
 {
@@ -6,7 +7,24 @@ namespace _06._Record_Unique_Names
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+
+                if (!names.Contains(name))
+                {
+                    names.Add(name);
+                }
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
