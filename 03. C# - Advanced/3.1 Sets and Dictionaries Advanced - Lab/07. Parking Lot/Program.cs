c90b6cd0e60af7e0a673ebaa7365894fc4860e6f
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2022-11-22 12:38:31 +0200

    Added 07. Parking Lot

--- 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
+++ 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _07._Parking_Lot
 {
@@ -6,7 +7,40 @@ namespace _07._Parking_Lot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<string> carPlates = new HashSet<string>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] input = command.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (input[0] == "IN")
+                {
+                    if (!carPlates.Contains(input[1]))
+                    {
+                        carPlates.Add(input[1]);
+                    }
+                }
+                else
+                {
+                    if (carPlates.Contains(input[1]))
+                    {
+                        carPlates.Remove(input[1]);
+                    }
+                }
+            }
+
+            if (carPlates.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var plate in carPlates)
+                {
+                    Console.WriteLine(plate);
+                }
+            }
         }
     }
 }
