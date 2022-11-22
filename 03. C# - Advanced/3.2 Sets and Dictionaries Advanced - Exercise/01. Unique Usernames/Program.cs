c4d79315df099264025218cadd6b243d477ac7f0
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2022-11-22 13:07:10 +0200

    Added 01. Unique Usernames

--- 03. C# - Advanced/3.2 Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/Program.cs
+++ 03. C# - Advanced/3.2 Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _01._Unique_Usernames
 {
@@ -6,7 +7,24 @@ namespace _01._Unique_Usernames
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+
+                if (!names.Contains(name))
+                {
+                    names.Add(name);
+                }
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
