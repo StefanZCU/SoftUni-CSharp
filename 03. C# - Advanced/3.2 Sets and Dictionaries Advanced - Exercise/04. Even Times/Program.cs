e5c635f5d17cb4d89d94370eca4f2fe06726ad50
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2022-11-23 08:45:13 +0200

    Added 04. Even Times

--- 03. C# - Advanced/3.2 Sets and Dictionaries Advanced - Exercise/04. Even Times/Program.cs
+++ 03. C# - Advanced/3.2 Sets and Dictionaries Advanced - Exercise/04. Even Times/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _04._Even_Times
 {
@@ -6,7 +7,29 @@ namespace _04._Even_Times
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<int, int> numOccurences = new Dictionary<int, int>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (!numOccurences.ContainsKey(num))
+                {
+                    numOccurences.Add(num, 0);
+                }
+
+                numOccurences[num]++;
+            }
+
+            foreach (var numOccurence in numOccurences)
+            {
+                if (numOccurence.Value % 2 == 0)
+                {
+                    Console.WriteLine(numOccurence.Key);
+                }
+            }
         }
     }
 }
