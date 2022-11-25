aa4290bd4c79b0f78aa7dc4f7040e146e3db4621
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2022-11-25 10:53:58 +0200

    Added 10. ForceBook

--- 03. C# - Advanced/3.2 Sets and Dictionaries Advanced - Exercise/10. ForceBook/Program.cs
+++ 03. C# - Advanced/3.2 Sets and Dictionaries Advanced - Exercise/10. ForceBook/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _10._ForceBook
 {
@@ -6,7 +8,100 @@ namespace _10._ForceBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, List<string>> usersPerSide = new Dictionary<string, List<string>>();
+            
+            string command;
+            while ((command = Console.ReadLine()) != "Lumpawaroo")
+            {
+                bool flag = true;
+
+                if (command.Contains("|"))
+                {
+                    int index = command.IndexOf("|");
+
+                    string forceSide = command.Substring(0, index).Trim();
+                    string forceUser = command.Substring(index + 1, (command.Length - 1) - index).Trim();
+
+                    if (!usersPerSide.ContainsKey(forceSide))
+                    {
+                        usersPerSide.Add(forceSide, new List<string>());
+                    }
+
+                    foreach (var kvp in usersPerSide)
+                    {
+                        foreach (var user in kvp.Value)
+                        {
+                            if (user == forceUser && kvp.Key != forceSide)
+                            {
+                                flag = false;
+                                break;
+                            }
+                        }
+
+                        if (!flag)
+                        {
+                            break;
+                        }
+                    }
+
+                    if (flag && !usersPerSide[forceSide].Contains(forceUser))
+                    {
+                        usersPerSide[forceSide].Add(forceUser);
+                    }
+                }
+                else if (command.Contains("->"))
+                {
+                    int index = command.IndexOf(">");
+
+                    string forceUser = command.Substring(0, index - 1).Trim();
+                    string forceSide = command.Substring(index + 1, (command.Length - 1) - index).Trim();
+
+                    if (!usersPerSide.ContainsKey(forceSide))
+                    {
+                        usersPerSide.Add(forceSide, new List<string>());
+                    }
+
+                    foreach (var kvp in usersPerSide)
+                    {
+                        foreach (var user in kvp.Value)
+                        {
+                            if (user == forceUser)
+                            {
+                                flag = false;
+                                break;
+                            }
+                        }
+
+                        if (!flag)
+                        {
+                            kvp.Value.Remove(forceUser);
+                            usersPerSide[forceSide].Add(forceUser);
+                            Console.WriteLine($"{forceUser} joins the {forceSide} side!");
+                            break;
+                        }
+                    }
+
+                    if (flag)
+                    {
+                        usersPerSide[forceSide].Add(forceUser);
+                        Console.WriteLine($"{forceUser} joins the {forceSide} side!");
+                    }
+                    
+                }
+            }
+
+            foreach (var kvp in usersPerSide.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+            {
+                if (kvp.Value.Count != 0)
+                {
+                    Console.WriteLine($"Side: {kvp.Key}, Members: {kvp.Value.Count}");
+
+                    foreach (var user in kvp.Value.OrderBy(x => x))
+                    {
+                        Console.WriteLine($"! {user}");
+                    }
+                }
+            }
         }
     }
 }
