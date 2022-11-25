b3e4f7fe02266a7e3e846655b0620a0dbbd5707b
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2022-11-25 13:36:16 +0200

    Added 01. Odd Lines

--- 03. C# - Advanced/4.1 Streams, Files and Directories - Lab/01. Odd Lines_Skeleton/Skeleton-Lab/Skeleton/OddLines/OddLines.cs
+++ 03. C# - Advanced/4.1 Streams, Files and Directories - Lab/01. Odd Lines_Skeleton/Skeleton-Lab/Skeleton/OddLines/OddLines.cs
@@ -1,7 +1,9 @@
-﻿namespace OddLines
+﻿using System;
+
+namespace OddLines
 {
     using System.IO;
-	
+
     public class OddLines
     {
         static void Main()
@@ -14,6 +16,25 @@
 
         public static void ExtractOddLines(string inputFilePath, string outputFilePath)
         {
+            using (StreamReader stream = new StreamReader(inputFilePath))
+            {
+                using (StreamWriter streamWriter = new StreamWriter(outputFilePath))
+                {
+                    string line = stream.ReadLine();
+                    int row = 0;
+
+                    while (line != null)
+                    {
+                        if (row % 2 == 1)
+                        {
+                            streamWriter.WriteLine(line);
+                        }
+
+                        line = stream.ReadLine();
+                        row++;
+                    }
+                }
+            }
         }
     }
 }
