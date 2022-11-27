3b9b93bbc1c7151535fd7b5245d6517570752a59
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2022-11-27 10:25:26 +0200

    Added 5.1 Functional Programming - Lab

---
+++ 03. C# - Advanced/5.1 Functional Programming - Lab/01. Sort Even Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Sort_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 03. C# - Advanced/5.1 Functional Programming - Lab/05. Filter By Age/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _05._Filter_By_Age
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
