cb1e5167ca4d6c1cfa8deab6d9a30bb4da1180e1
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2022-11-27 10:35:31 +0200

    Added 01. Sort Even Numbers

--- 03. C# - Advanced/5.1 Functional Programming - Lab/01. Sort Even Numbers/Program.cs
+++ 03. C# - Advanced/5.1 Functional Programming - Lab/01. Sort Even Numbers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _01._Sort_Even_Numbers
 {
@@ -6,7 +7,9 @@ namespace _01._Sort_Even_Numbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] nums = Console.ReadLine().Split(", ").Select(int.Parse).Where(x => x % 2 == 0).OrderBy(x => x).ToArray();
+
+            Console.WriteLine(string.Join(", ", nums));
         }
     }
 }
