4c520b58606ef2dd7e5bc4f463dd202304a50055
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2022-12-01 11:19:43 +0200

    Added 01. Car

--- 03. C# - Advanced/6.1 Defining Classes - Lab/01. Car/Program.cs
+++ 03. C# - Advanced/6.1 Defining Classes - Lab/01. Car/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
-namespace _01._Car
+namespace CarManufacturer
 {
-    internal class Program
+    public class StartUp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car car = new Car();
+
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
         }
     }
 }
