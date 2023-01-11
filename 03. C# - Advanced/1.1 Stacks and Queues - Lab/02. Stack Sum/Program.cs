076976e24286c63436c47a119ec3aaed51d847fd
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-11 11:00:40 +0200

    Added 02. Stack Sum

--- 03. C# - Advanced/1.1 Stacks and Queues - Lab/02. Stack Sum/Program.cs
+++ 03. C# - Advanced/1.1 Stacks and Queues - Lab/02. Stack Sum/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+Stack<int> numberStack = new Stack<int>(numbers);
+
+string command;
+while ((command = Console.ReadLine().ToLower()) != "end")
+{
+    string[] input = command.Split();
+
+    if (input[0] == "add")
+    {
+        int firstNum = int.Parse(input[1]);
+        int secondNum = int.Parse(input[2]);
+
+        numberStack.Push(firstNum);
+        numberStack.Push(secondNum);
+    }
+    else if (input[0] == "remove" && int.Parse(input[1]) <= numberStack.Count)
+    {
+        int numCount = int.Parse(input[1]);
+
+        for (int i = 0; i < numCount; i++)
+        {
+            numberStack.Pop();
+        }
+    }
+}
+
+Console.WriteLine($"Sum: {numberStack.Sum()}");
+
+
