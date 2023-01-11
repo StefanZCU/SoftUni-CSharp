a7b5800c13404ac04f614b39e46c29b3a5fc56a5
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-11 12:54:09 +0200

    Added 03. Maximum and Minimum Element

--- 03. C# - Advanced/1.2 Stacks and Queues - Exercise/03. Maximum and Minimum Element/Program.cs
+++ 03. C# - Advanced/1.2 Stacks and Queues - Exercise/03. Maximum and Minimum Element/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = int.Parse(Console.ReadLine());
+
+Stack<int> numbers = new Stack<int>();
+
+for (int i = 0; i < n; i++)
+{
+    int[] command = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+    int num = command[0];
+
+    if (num == 1)
+    {
+        numbers.Push(command[1]);
+    }
+    else if (num == 2)
+    {
+        numbers.Pop();
+    }
+    else if (num == 3)
+    {
+        if (numbers.Count != 0)
+        {
+            Console.WriteLine(numbers.Max());
+        }
+    }
+    else if (num == 4)
+    {
+        if (numbers.Count != 0)
+        {
+            Console.WriteLine(numbers.Min());
+        }
+    }
+}
+
+Console.WriteLine(string.Join(", ", numbers));
