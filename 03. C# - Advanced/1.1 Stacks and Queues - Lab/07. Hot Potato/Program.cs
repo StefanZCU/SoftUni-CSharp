c9ccf0c53006fc9ea738dc32208a3eeb0e99ed3d
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-11 11:50:50 +0200

    Added 07. Hot Potato

--- 03. C# - Advanced/1.1 Stacks and Queues - Lab/07. Hot Potato/Program.cs
+++ 03. C# - Advanced/1.1 Stacks and Queues - Lab/07. Hot Potato/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Queue<string> nameQueue = new Queue<string>(names);
+
+int numPasses = int.Parse(Console.ReadLine());
+
+while (nameQueue.Count != 1)
+{
+    for (int i = 1; i < numPasses; i++)
+    {
+        string currentName = nameQueue.Dequeue();
+        nameQueue.Enqueue(currentName);
+    }
+
+    Console.WriteLine($"Removed {nameQueue.Dequeue()}");
+}
+
+Console.WriteLine($"Last is {nameQueue.Peek()}");
