84b781d5147bab30fe331153f444cf8e60c5f9c2
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-11 13:21:24 +0200

    Added 05. Fashion Boutique

--- 03. C# - Advanced/1.2 Stacks and Queues - Exercise/05. Fashion Boutique/Program.cs
+++ 03. C# - Advanced/1.2 Stacks and Queues - Exercise/05. Fashion Boutique/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] clothesInBoxes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int rackCapacity = int.Parse(Console.ReadLine());
+Stack<int> clotheStack = new Stack<int>(clothesInBoxes);
+
+int clothesRacks = 1;
+
+int currentRack = 0;
+
+while (clotheStack.Count != 0)
+{
+    if (currentRack + clotheStack.Peek() > rackCapacity)
+    {
+        clothesRacks++;
+        currentRack = 0;
+        currentRack += clotheStack.Pop();
+    }
+    else if (currentRack + clotheStack.Peek() == rackCapacity)
+    {
+        clothesRacks++;
+        currentRack = 0;
+        clotheStack.Pop();
+    }
+    else
+    {
+        currentRack += clotheStack.Pop();
+    }
+}
+
+if (currentRack == 0)
+{
+    clothesRacks--;
+}
+
+Console.WriteLine(clothesRacks);
