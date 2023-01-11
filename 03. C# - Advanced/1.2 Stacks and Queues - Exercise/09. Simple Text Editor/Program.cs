5557fc2d764068384da6b9410a11bb716734d96e
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-11 14:32:50 +0200

    Added 09. Simple Text Editor

--- 03. C# - Advanced/1.2 Stacks and Queues - Exercise/09. Simple Text Editor/Program.cs
+++ 03. C# - Advanced/1.2 Stacks and Queues - Exercise/09. Simple Text Editor/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+int numOperations = int.Parse(Console.ReadLine());
+
+Stack<string> stringVersions = new Stack<string>();
+StringBuilder sb = new StringBuilder();
+stringVersions.Push(sb.ToString());
+
+for (int i = 0; i < numOperations; i++)
+{
+    string[] command = Console.ReadLine().Split();
+
+    if (command[0] == "1")
+    {
+        sb.Append(command[1]);
+        stringVersions.Push(sb.ToString());
+    }
+    else if (command[0] == "2")
+    {
+        sb.Remove(sb.Length - int.Parse(command[1]), int.Parse(command[1]));
+        stringVersions.Push(sb.ToString());
+
+    }
+    else if (command[0] == "3")
+    {
+        Console.WriteLine(sb.ToString().Substring(int.Parse(command[1]) - 1, 1));
+    }
+    else if (command[0] == "4")
+    {
+        sb.Clear();
+        stringVersions.Pop();
+        sb.Append(stringVersions.Peek());
+    }
+}
