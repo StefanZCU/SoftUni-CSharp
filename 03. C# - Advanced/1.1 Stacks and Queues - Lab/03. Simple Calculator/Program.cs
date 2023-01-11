d0f568c69125e04d25f4702bcf8b0340403f2178
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-11 11:16:23 +0200

    Added 03. Simple Calculator

--- 03. C# - Advanced/1.1 Stacks and Queues - Lab/03. Simple Calculator/Program.cs
+++ 03. C# - Advanced/1.1 Stacks and Queues - Lab/03. Simple Calculator/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] expressionString = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Stack<string> expression = new Stack<string>(expressionString.Reverse());
+
+while (expression.Count != 1)
+{
+    int num = int.Parse(expression.Pop());
+    char symbol = char.Parse(expression.Pop());
+    int num2 = int.Parse(expression.Pop());
+
+    if (symbol == '-')
+    {
+        expression.Push((num - num2).ToString());
+    }
+    else
+    {
+        expression.Push((num + num2).ToString());        
+    }
+}
+
+Console.WriteLine(expression.Pop());
