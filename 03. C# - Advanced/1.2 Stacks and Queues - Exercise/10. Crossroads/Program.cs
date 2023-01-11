3b0e45515511aa5b3236bbc30b7ac7dfae0de740
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-11 14:49:41 +0200

    Unfinished 10.

--- 03. C# - Advanced/1.2 Stacks and Queues - Exercise/10. Crossroads/Program.cs
+++ 03. C# - Advanced/1.2 Stacks and Queues - Exercise/10. Crossroads/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int greenLightDuration = int.Parse(Console.ReadLine());
+int freeWindowDuration = int.Parse(Console.ReadLine());
+Queue<string> carsInLine = new Queue<string>();
+
+string command;
+while ((command = Console.ReadLine()) != "END")
+{
+    if (command != "green")
+    {
+        carsInLine.Enqueue(command);
+    }
+    else
+    {
+        string currentCar = carsInLine.Dequeue();
+
+        for (int i = 0; i < greenLightDuration; i++)
+        {
+            
+        }
+    }
+}
