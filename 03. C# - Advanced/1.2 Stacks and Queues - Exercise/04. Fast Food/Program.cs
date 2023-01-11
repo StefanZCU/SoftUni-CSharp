5fda886b68f57eb85c669751dc8edcba113c03f1
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-11 13:03:21 +0200

    Added 04. Fast Food

--- 03. C# - Advanced/1.2 Stacks and Queues - Exercise/04. Fast Food/Program.cs
+++ 03. C# - Advanced/1.2 Stacks and Queues - Exercise/04. Fast Food/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int foodQuantity = int.Parse(Console.ReadLine());
+
+int[] orderSizes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+Queue<int> orders = new Queue<int>(orderSizes);
+Console.WriteLine(orders.Max());
+
+for (int i = 0; i < orderSizes.Length; i++)
+{
+    if (foodQuantity - orders.Peek() >= 0)
+    {
+        foodQuantity -= orders.Dequeue();
+    }
+    else
+    {
+        Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+        break;
+    }
+
+    if (orders.Count == 0)
+    {
+        Console.WriteLine("Orders complete");
+        break;
+    }
+}
