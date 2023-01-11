ccadb48e98b57b580eec02f7a7dec78ce461404c
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-11 11:25:48 +0200

    Added 04. Matching Brackets

--- 03. C# - Advanced/1.1 Stacks and Queues - Lab/04. Matching Brackets/Program.cs
+++ 03. C# - Advanced/1.1 Stacks and Queues - Lab/04. Matching Brackets/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string expression = Console.ReadLine();
+
+Stack<int> openBracketIndex = new Stack<int>();
+
+for (int i = 0; i < expression.Length; i++)
+{
+    if (expression[i] == '(')
+    {
+        openBracketIndex.Push(i);
+    }
+    else if (expression[i] == ')')
+    {
+        int openBracket = openBracketIndex.Pop();
+
+        Console.WriteLine(expression.Substring(openBracket, i - openBracket + 1));
+    }
+}
+
+
+
