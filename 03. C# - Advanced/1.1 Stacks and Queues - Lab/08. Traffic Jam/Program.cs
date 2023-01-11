402ba189da70be492df7360aff18b54646230a50
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-11 12:04:40 +0200

    Added 08. Traffic Jam

--- 03. C# - Advanced/1.1 Stacks and Queues - Lab/08. Traffic Jam/Program.cs
+++ 03. C# - Advanced/1.1 Stacks and Queues - Lab/08. Traffic Jam/Program.cs
@@ -1,2 +1,26 @@
-﻿
+﻿int numCarsToPass = int.Parse(Console.ReadLine());
+int carsPassed = 0;
 
+Queue<string> carsInLine = new Queue<string>();
+
+string command;
+while ((command = Console.ReadLine()) != "end")
+{
+    if (command != "green")
+    {
+        carsInLine.Enqueue(command);
+    }
+    else
+    {
+        for (int i = 0; i < numCarsToPass; i++)
+        {
+            if (carsInLine.Count != 0)
+            {
+                Console.WriteLine($"{carsInLine.Dequeue()} passed!");
+                carsPassed++;
+            }
+        }
+    }
+}
+
+Console.WriteLine($"{carsPassed} cars passed the crossroads.");
