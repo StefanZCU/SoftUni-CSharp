b80725eada4ee546db024f93fe571be06283e8f6
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-11 13:35:47 +0200

    Added 06. Songs Queue

--- 03. C# - Advanced/1.2 Stacks and Queues - Exercise/06. Songs Queue/Program.cs
+++ 03. C# - Advanced/1.2 Stacks and Queues - Exercise/06. Songs Queue/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] songs = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+Queue<string> songQueue = new Queue<string>(songs);
+
+while (songQueue.Count != 0)
+{
+    string command = Console.ReadLine();
+
+    if (command.Contains("Add"))
+    {
+        string songName = command.Substring(3, command.Length - 3).TrimStart();
+
+        if (!songQueue.Contains(songName))
+        {
+            songQueue.Enqueue(songName);
+        }
+        else
+        {
+            Console.WriteLine($"{songName} is already contained!");
+        }
+    }
+    else if (command == "Play")
+    {
+        songQueue.Dequeue();
+    }
+    else if (command == "Show")
+    {
+        Console.WriteLine(string.Join(", ", songQueue));
+    }
+}
+
+Console.WriteLine("No more songs!");
