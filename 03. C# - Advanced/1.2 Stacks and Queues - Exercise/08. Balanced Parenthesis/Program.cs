4a82de6a2aa0f775413e0fb26f7fd4cab15edf02
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-11 13:57:40 +0200

    Added 08. Balanced Parenthesis

--- 03. C# - Advanced/1.2 Stacks and Queues - Exercise/08. Balanced Parenthesis/Program.cs
+++ 03. C# - Advanced/1.2 Stacks and Queues - Exercise/08. Balanced Parenthesis/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string line = Console.ReadLine();
+Stack<char> openParenthesis = new Stack<char>();
+bool flag = true;
+
+foreach (var character in line)
+{
+    if (character == '(' || character == '{' || character == '[')
+    {
+        openParenthesis.Push(character);
+        continue;
+    }
+
+    if (openParenthesis.Count != 0)
+    {
+        if ((character == ')' && openParenthesis.Peek() == '(') 
+            || (character == '}' && openParenthesis.Peek() == '{')
+            || (character == ']' && openParenthesis.Peek() == '['))
+        {
+            openParenthesis.Pop();
+        }
+        else
+        {
+            Console.WriteLine("NO");
+            flag = false;
+            break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("NO");
+        flag = false;
+        break;
+    }
+}
+
+if (flag && openParenthesis.Count == 0)
+{
+    Console.WriteLine("YES");
+}
