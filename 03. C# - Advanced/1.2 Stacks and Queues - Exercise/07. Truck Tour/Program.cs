b8511161bc8df7f433adbf30b61f94a28a987091
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-11 13:43:02 +0200

    Added 07. Truck Tour

--- 03. C# - Advanced/1.2 Stacks and Queues - Exercise/07. Truck Tour/Program.cs
+++ 03. C# - Advanced/1.2 Stacks and Queues - Exercise/07. Truck Tour/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Numerics;
+
+int numPumps = int.Parse(Console.ReadLine());
+
+Queue<string> petrolPumps = new Queue<string>();
+
+for (int i = 0; i < numPumps; i++)
+{
+    petrolPumps.Enqueue(Console.ReadLine());
+}
+
+for (int i = 0; i < numPumps; i++)
+{
+    BigInteger currentPetrolAmt = 0;
+    bool flag = true;
+
+    for (int j = 0; j < numPumps; j++)
+    {
+        BigInteger[] currPump = petrolPumps.Dequeue().Split().Select(BigInteger.Parse).ToArray();
+        petrolPumps.Enqueue(string.Join(" ", currPump));
+
+        currentPetrolAmt += currPump[0];
+        currentPetrolAmt -= currPump[1];
+
+        if (currentPetrolAmt < 0)
+        {
+            flag = false;
+        }
+    }
+
+    if (flag)
+    {
+        Console.WriteLine(i);
+        break;
+    }
+
+    string temp = petrolPumps.Dequeue();
+    petrolPumps.Enqueue(temp);
+}
