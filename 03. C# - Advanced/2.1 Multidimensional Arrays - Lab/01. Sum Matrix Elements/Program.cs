0e167aca5cb58837c687d0deac56c2cb29d5f689
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-12 12:17:56 +0200

    Added 01. Sum Matrix Elements

--- 03. C# - Advanced/2.1 Multidimensional Arrays - Lab/01. Sum Matrix Elements/Program.cs
+++ 03. C# - Advanced/2.1 Multidimensional Arrays - Lab/01. Sum Matrix Elements/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] matrixSize = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+int[,] matrix = new int[matrixSize[0], matrixSize[1]];
+int sum = 0;
+
+for (int i = 0; i < matrixSize[0]; i++)
+{
+    int[] nums = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+    for (int j = 0; j < matrixSize[1]; j++)
+    {
+        matrix[i, j] = nums[j];
+        sum += nums[j];
+    }
+}
+
+Console.WriteLine(matrix.GetLength(0));
+Console.WriteLine(matrix.GetLength(1));
+Console.WriteLine(sum);
