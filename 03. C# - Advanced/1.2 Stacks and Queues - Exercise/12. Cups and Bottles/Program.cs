616b068560f439a75bd0ddb5f15f47f97f30c575
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-12 12:05:13 +0200

    Added 12. Cups and Bottles

--- 03. C# - Advanced/1.2 Stacks and Queues - Exercise/12. Cups and Bottles/Program.cs
+++ 03. C# - Advanced/1.2 Stacks and Queues - Exercise/12. Cups and Bottles/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] cupCapacityArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int[] bottlesArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+Stack<int> bottles = new Stack<int>(bottlesArray);
+Queue<int> cups = new Queue<int>(cupCapacityArray);
+
+int wastedLiters = 0;
+
+while (true)
+{
+    if (bottles.Count != 0 && cups.Count != 0)
+    {
+        if (bottles.Peek() - cups.Peek() > 0)
+        {
+            wastedLiters += bottles.Pop() - cups.Dequeue();
+        }
+        else if (bottles.Peek() - cups.Peek() == 0)
+        {
+            bottles.Pop();
+            cups.Dequeue();
+        }
+        else if (bottles.Peek() - cups.Peek() < 0)
+        {
+            cups.Enqueue(cups.Dequeue() - bottles.Pop());
+
+            for (int i = 1; i < cups.Count; i++)
+            {
+                cups.Enqueue(cups.Dequeue());
+            }
+        }
+
+    }
+    else if (bottles.Count == 0)
+    {
+        Console.WriteLine($"Cups: {string.Join(" ", cups)}");
+        Console.WriteLine($"Wasted litters of water: {wastedLiters}");
+        break;
+    }
+    else
+    {
+        Console.WriteLine($"Bottles: {string.Join(" ", bottles)}");
+        Console.WriteLine($"Wasted litters of water: {wastedLiters}");
+        break;
+    }
+}
