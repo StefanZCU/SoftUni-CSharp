8c23b707c764b728150c2fce518be85eb9af2ff8
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-12 12:40:53 +0200

    Added 04. Symbol in Matrix

--- 03. C# - Advanced/2.1 Multidimensional Arrays - Lab/04. Symbol in Matrix/Program.cs
+++ 03. C# - Advanced/2.1 Multidimensional Arrays - Lab/04. Symbol in Matrix/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int matrixSize = int.Parse(Console.ReadLine());
+
+char[,] matrix = new char[matrixSize, matrixSize];
+
+for (int i = 0; i < matrixSize; i++)
+{
+    string symbols = Console.ReadLine();
+
+    for (int j = 0; j < matrixSize; j++)
+    {
+        matrix[i, j] = symbols[j];
+    }
+}
+
+char symbolToLookFor = char.Parse(Console.ReadLine());
+
+for (int i = 0; i < matrixSize; i++)
+{
+    for (int j = 0; j < matrixSize; j++)
+    {
+        if (matrix[i, j] == symbolToLookFor)
+        {
+            Console.WriteLine($"({i}, {j})");
+            return;
+        }
+    }
+}
+
+Console.WriteLine($"{symbolToLookFor} does not occur in the matrix");
