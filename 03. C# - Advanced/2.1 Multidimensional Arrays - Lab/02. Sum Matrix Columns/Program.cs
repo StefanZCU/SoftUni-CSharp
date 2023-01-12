3e7d20c7af4def845241a89ff2bda7f1edd78b6a
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-12 12:22:03 +0200

    Added 02. Sum Matrix Columns

--- 03. C# - Advanced/2.1 Multidimensional Arrays - Lab/02. Sum Matrix Columns/Program.cs
+++ 03. C# - Advanced/2.1 Multidimensional Arrays - Lab/02. Sum Matrix Columns/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] matrixSize = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+int[,] matrix = new int[matrixSize[0], matrixSize[1]];
+
+for (int i = 0; i < matrixSize[0]; i++)
+{
+    int[] nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+    for (int j = 0; j < matrixSize[1]; j++)
+    {
+        matrix[i, j] = nums[j];
+    }
+}
+
+for (int i = 0; i < matrixSize[1]; i++)
+{
+    int sum = 0;
+
+    for (int j = 0; j < matrixSize[0]; j++)
+    {
+        sum += matrix[j, i];
+    }
+
+    Console.WriteLine(sum);
+}
