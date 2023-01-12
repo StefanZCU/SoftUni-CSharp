874e3891716c18fdd6b042b14b153ec99b6a8671
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-12 12:24:15 +0200

    Added 03. Primary Diagonal

--- 03. C# - Advanced/2.1 Multidimensional Arrays - Lab/03. Primary Diagonal/Program.cs
+++ 03. C# - Advanced/2.1 Multidimensional Arrays - Lab/03. Primary Diagonal/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int matrixSize = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[matrixSize, matrixSize];
+int sum = 0;
+
+for (int i = 0; i < matrixSize; i++)
+{
+    int[] nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+    for (int j = 0; j < matrixSize; j++)
+    {
+        matrix[i, j] = nums[j];
+
+        if (i == j)
+        {
+            sum += nums[j];
+        }
+    }
+}
+
+Console.WriteLine(sum);
