ad0e37b312c59cb222438823e5bf9c7740095c98
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-14 11:29:56 +0200

    Added 01. Diagonal Difference

--- 03. C# - Advanced/2.2 Multidimensional Arrays - Exercise/01. Diagonal Difference/Program.cs
+++ 03. C# - Advanced/2.2 Multidimensional Arrays - Exercise/01. Diagonal Difference/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int matrixSize = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[matrixSize, matrixSize];
+
+int mainDiagonalSum = 0;
+int secondaryDiagonalSum = 0;
+
+for (int row = 0; row < matrix.GetLength(0); row++)
+{
+    int[] cols = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+    for (int col = 0; col < matrix.GetLength(1); col++)
+    {
+        matrix[row, col] = cols[col];
+
+        if (row == col)
+        {
+            mainDiagonalSum += cols[col];
+        }
+
+        if (row + col == matrixSize - 1)
+        {
+            secondaryDiagonalSum += cols[col];
+        }
+    }
+}
+
+Console.WriteLine(Math.Abs(mainDiagonalSum - secondaryDiagonalSum));
+
+
