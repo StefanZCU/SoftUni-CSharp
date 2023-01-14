79159f0eb2047ab2dd286a9bb3e2fa4e5f242711
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-14 11:09:56 +0200

    Added 07. Pascal Triangle

--- 03. C# - Advanced/2.1 Multidimensional Arrays - Lab/07. Pascal Triangle/Program.cs
+++ 03. C# - Advanced/2.1 Multidimensional Arrays - Lab/07. Pascal Triangle/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numRows = int.Parse(Console.ReadLine());
+
+int[][] jaggedArray = new int[numRows][];
+
+jaggedArray[0] = new int[] { 1 };
+
+for (int i = 1; i < jaggedArray.GetLength(0); i++)
+{
+    jaggedArray[i] = new int[i + 1];
+
+    for (int j = 0; j < jaggedArray[i].Length; j++)
+    {
+        if (j < jaggedArray[i - 1].Length)
+        {
+            jaggedArray[i][j] += jaggedArray[i - 1][j];
+        }
+
+        if (j - 1 >= 0)
+        {
+            jaggedArray[i][j] += jaggedArray[i - 1][j - 1];
+        }
+
+    }
+}
+
+
+for (int i = 0; i < jaggedArray.GetLength(0); i++)
+{
+    for (int j = 0; j < jaggedArray[i].Length; j++)
+    {
+        Console.Write(jaggedArray[i][j] + " ");
+    }
+
+    Console.WriteLine();
+}
+
