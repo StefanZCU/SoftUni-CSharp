1c077d80c7a16374e459ebd514d2266d15123a43
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-14 11:44:05 +0200

    Added 02. Squares in Matrix

--- 03. C# - Advanced/2.2 Multidimensional Arrays - Exercise/02. Squares in Matrix/Program.cs
+++ 03. C# - Advanced/2.2 Multidimensional Arrays - Exercise/02. Squares in Matrix/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] matrixSize = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+char[,] matrix = new char[matrixSize[0], matrixSize[1]];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    char[] cols = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = cols[j];
+    }
+}
+
+int sum = 0;
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        if (i + 1 < matrix.GetLength(0) && j + 1 < matrix.GetLength(1))
+        {
+            char currentSymbol = matrix[i, j];
+
+            if (matrix[i + 1, j] == currentSymbol && matrix[i, j + 1] == currentSymbol &&  matrix[i + 1, j + 1] == currentSymbol)
+            {
+                sum++;
+            }
+        }
+    }
+}
+
+Console.WriteLine(sum);
