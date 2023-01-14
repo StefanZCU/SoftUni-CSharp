8af92d2433a07af00fed5777c526ac5cac35cc00
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-14 12:29:14 +0200

    Added 05. Snake Moves

--- 03. C# - Advanced/2.2 Multidimensional Arrays - Exercise/05. Snake Moves/Program.cs
+++ 03. C# - Advanced/2.2 Multidimensional Arrays - Exercise/05. Snake Moves/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] matrixSize = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+char[,] matrix = new char[matrixSize[0], matrixSize[1]];
+
+string snake = Console.ReadLine();
+
+Queue<char> snakeChars = new Queue<char>(snake);
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    if (i % 2 == 0)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            var currentSymbol = snakeChars.Dequeue();
+            matrix[i, j] = currentSymbol;
+            snakeChars.Enqueue(currentSymbol);
+        }
+    }
+    else
+    {
+        for (int j = matrix.GetLength(1) - 1; j >= 0; j--)
+        {
+            var currentSymbol = snakeChars.Dequeue();
+            matrix[i, j] = currentSymbol;
+            snakeChars.Enqueue(currentSymbol);
+        }
+    }
+
+}
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i, j]}");
+    }
+
+    Console.WriteLine();
+}
