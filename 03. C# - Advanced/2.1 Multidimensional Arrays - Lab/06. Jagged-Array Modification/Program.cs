6bfadec873f315e4c858feedafc8e606184a43a7
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-14 10:54:11 +0200

    Added 06. Jagged-Array Modification

--- 03. C# - Advanced/2.1 Multidimensional Arrays - Lab/06. Jagged-Array Modification/Program.cs
+++ 03. C# - Advanced/2.1 Multidimensional Arrays - Lab/06. Jagged-Array Modification/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int rowSize = int.Parse(Console.ReadLine());
+int[][] jaggedMatrix = new int[rowSize][];
+
+for (int i = 0; i < jaggedMatrix.GetLength(0); i++)
+{
+    int[] jaggedCol = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+    jaggedMatrix[i] = new int[jaggedCol.Length];
+
+    for (int j = 0; j < jaggedCol.Length; j++)
+    {
+        jaggedMatrix[i][j] = jaggedCol[j];
+    }
+}
+
+string command;
+while ((command = Console.ReadLine()) != "END")
+{
+    string[] input = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string cmdArg = input[0];
+    int row = int.Parse(input[1]);
+    int col = int.Parse(input[2]);
+    int value = int.Parse(input[3]);
+
+    if (CheckCoords(row, col, jaggedMatrix))
+    {
+        if (cmdArg == "Add")
+        {
+            jaggedMatrix[row][col] += value;
+        }
+        else if (cmdArg == "Subtract")
+        {
+            jaggedMatrix[row][col] -= value;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid coordinates");
+    }
+}
+
+for (int i = 0; i < jaggedMatrix.GetLength(0); i++)
+{
+    for (int j = 0; j < jaggedMatrix[i].Length; j++)
+    {
+        Console.Write(jaggedMatrix[i][j] + " ");
+    }
+
+    Console.WriteLine();
+}
+
+static bool CheckCoords(int row, int col, int[][] jaggedMatrix)
+{
+    if (row >= 0 && row < jaggedMatrix.GetLength(0) && col >= 0 && col < jaggedMatrix[1].Length)
+    {
+        return true;
+    }
+
+    return false;
+}
+
