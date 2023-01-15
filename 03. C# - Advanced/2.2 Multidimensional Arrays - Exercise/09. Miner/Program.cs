b8e8114361f34bdc82307cdbb3e17429b126040e
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-15 12:41:26 +0200

    Added 09. Miner

--- 03. C# - Advanced/2.2 Multidimensional Arrays - Exercise/09. Miner/Program.cs
+++ 03. C# - Advanced/2.2 Multidimensional Arrays - Exercise/09. Miner/Program.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int matrixSize = int.Parse(Console.ReadLine());
+string[] moves = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+int coals = 0;
+int minerRow = 0;
+int minerCol = 0;
+string[,] field = new string[matrixSize, matrixSize];
+
+for (int i = 0; i < matrixSize; i++)
+{
+    string[] row = Console.ReadLine().Split();
+
+    for (int j = 0; j < matrixSize; j++)
+    {
+        field[i, j] = row[j];
+
+        if (row[j] == "c")
+        {
+            coals++;
+        }
+
+        if (row[j] == "s")
+        {
+            minerRow = i;
+            minerCol = j;
+        }
+    }
+}
+
+for (int i = 0; i < moves.Length; i++)
+{
+    if (moves[i] == "up")
+    {
+        if (minerRow > 0)
+        {
+            minerRow--;
+        }
+    }
+    else if (moves[i] == "down")
+    {
+        if (minerRow < matrixSize - 1)
+        {
+            minerRow++;
+        }
+    }
+    else if (moves[i] == "left")
+    {
+        if (minerCol > 0)
+        {
+            minerCol--;
+        }
+    }
+    else if (moves[i] == "right")
+    {
+        if (minerCol < matrixSize - 1)
+        {
+            minerCol++;
+        }
+    }
+
+    if (field[minerRow, minerCol] == "c")
+    {
+        coals--;
+        field[minerRow, minerCol] = "*";
+        if (coals == 0)
+        {
+            Console.WriteLine($"You collected all coals! ({minerRow}, {minerCol})");
+            return;
+        }
+    }
+    else if (field[minerRow, minerCol] == "e")
+    {
+        Console.WriteLine($"Game over! ({minerRow}, {minerCol})");
+        return;
+    }
+}
+
+Console.WriteLine($"{coals} coals left. ({minerRow}, {minerCol})");
