990dbea2d708f06f08304c7a966ef9ddbf8031d0
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-16 13:04:24 +0200

    Added 06. Wardrobe

--- 03. C# - Advanced/3.2 Sets and Dictionaries Advanced - Exercise/06. Wardrobe/Program.cs
+++ 03. C# - Advanced/3.2 Sets and Dictionaries Advanced - Exercise/06. Wardrobe/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Dictionary<string, Dictionary<string, int>> clothesOccurrencesByColor =
+    new Dictionary<string, Dictionary<string, int>>();
+
+int numLines = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numLines; i++)
+{
+    string[] input = Console.ReadLine().Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+    string color = input[0];
+    string[] clothes = input[1].Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+    if (!clothesOccurrencesByColor.ContainsKey(color))
+    {
+        clothesOccurrencesByColor.Add(color, new Dictionary<string, int>());
+    }
+
+    for (int j = 0; j < clothes.Length; j++)
+    {
+        if (!clothesOccurrencesByColor[color].ContainsKey(clothes[j]))
+        {
+            clothesOccurrencesByColor[color].Add(clothes[j], 0);
+        }
+
+        clothesOccurrencesByColor[color][clothes[j]]++;
+    }
+}
+
+string[] clotheToFind = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+string colorToFind = clotheToFind[0];
+string clothe = clotheToFind[1];
+
+foreach (var color in clothesOccurrencesByColor)
+{
+    Console.WriteLine($"{color.Key} clothes:");
+
+    foreach (var clothes in color.Value)
+    {
+        if (color.Key == colorToFind && clothes.Key == clothe)
+        {
+            Console.WriteLine($"* {clothes.Key} - {clothes.Value} (found!)");
+        }
+        else
+        {
+            Console.WriteLine($"* {clothes.Key} - {clothes.Value}");
+        }
+    }
+}
