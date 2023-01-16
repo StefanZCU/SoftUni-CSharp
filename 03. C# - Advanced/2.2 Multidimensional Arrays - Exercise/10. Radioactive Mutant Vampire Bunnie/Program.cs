ca9b102293b3dfec0a20c9b00a400769b2f7f10a
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-16 12:24:33 +0200

    Added 10. Radiactive Mutant Vampire Bunnies

--- 03. C# - Advanced/2.2 Multidimensional Arrays - Exercise/10. Radioactive Mutant Vampire Bunnie/Program.cs
+++ 03. C# - Advanced/2.2 Multidimensional Arrays - Exercise/10. Radioactive Mutant Vampire Bunnie/Program.cs
@@ -1,162 +1,175 @@
 ﻿int[] matrixSize = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-char[,] matrix = new char[matrixSize[0], matrixSize[1]];
 
-int[] playerPos = new int[2];
+int rowSize = matrixSize[0];
+int colSize = matrixSize[1];
 
-for (int i = 0; i < matrix.GetLength(0); i++)
+int currentPosRow = 0;
+int currentPosCol = 0;
+
+char[,] matrix = new char[rowSize, colSize];
+char[,] oldBunnyMatrix = new char[rowSize, colSize];
+
+for (int row = 0; row < matrix.GetLength(0); row++)
 {
-    string line = Console.ReadLine();
+    string line = Console.ReadLine().Trim();
 
-    for (int j = 0; j < matrix.GetLength(1); j++)
+    for (int col = 0; col < matrix.GetLength(1); col++)
     {
-        matrix[i, j] = line[j];
+        matrix[row, col] = line[col];
+        oldBunnyMatrix[row, col] = line[col];
 
-        if (line[j] == 'P')
+        if (line[col] == 'P')
         {
-            playerPos[0] = i;
-            playerPos[1] = j;
+            currentPosRow = row;
+            currentPosCol = col;
+            matrix[row, col] = '.';
+            oldBunnyMatrix[row, col] = '.';
         }
     }
 }
 
-string directions = Console.ReadLine();
-int[] newPlayerPos = new int[2];
-char[,] bunnyMatrix = matrix;
-bool flag = false;
+string commands = Console.ReadLine().Trim();
+bool flagWin = false;
+bool flagLose = false;
 
-for (int i = 0; i < directions.Length; i++)
+int winningRow = 0;
+int winningCol = 0;
+
+for (int i = 0; i < commands.Length; i++)
 {
-    newPlayerPos = PlayerMover(directions[i], playerPos, newPlayerPos);
+    char direction = commands[i];
 
-    if (newPlayerPos[0] < 0 || newPlayerPos[0] >= matrix.GetLength(0) ||
-        newPlayerPos[1] < 0 || newPlayerPos[1] >= matrix.GetLength(1))
+    if (direction == 'U')
     {
-        BoardPrinter(flag, matrix, bunnyMatrix);
-        Console.WriteLine($"won: {playerPos[0]} {playerPos[1]}");
-        return;
+        if (IndexChecker(matrix, currentPosRow - 1, currentPosCol))
+        {
+            winningCol = currentPosCol;
+            winningRow = currentPosRow;
+            flagWin = true;
+        }
+        else
+        {
+            currentPosRow--;
+        }
+    }
+    else if (direction == 'D')
+    {
+        if (IndexChecker(matrix, currentPosRow + 1, currentPosCol))
+        {
+            winningCol = currentPosCol;
+            winningRow = currentPosRow;
+            flagWin = true;
+        }
+        else
+        {
+            currentPosRow++;
+        }
+    }
+    else if (direction == 'L')
+    {
+        if (IndexChecker(matrix, currentPosRow, currentPosCol - 1))
+        {
+            winningCol = currentPosCol;
+            winningRow = currentPosRow;
+            flagWin = true;
+        }
+        else
+        {
+            currentPosCol--;
+        }
+    }
+    else if (direction == 'R')
+    {
+        if (IndexChecker(matrix, currentPosRow, currentPosCol + 1))
+        {
+            winningCol = currentPosCol;
+            winningRow = currentPosRow;
+            flagWin = true;
+        }
+        else
+        {
+            currentPosCol++;
+        }
+    }
+
+    if (matrix[currentPosRow, currentPosCol] == 'B' && !flagWin)
+    {
+        flagLose = true;
     }
 
     for (int j = 0; j < matrix.GetLength(0); j++)
     {
         for (int k = 0; k < matrix.GetLength(1); k++)
         {
-            if (matrix[j, k] == 'B')
+            if (oldBunnyMatrix[j, k] == 'B')
             {
-                int[,] bunnyMoves =
+                if (!IndexChecker(matrix, j - 1, k))
                 {
-                    { j - 1, k },
-                    { j + 1, k },
-                    { j, k - 1 },
-                    { j, k + 1 }
-                };
-
-                for (int l = 0; l < bunnyMoves.GetLength(0); l++)
-                {
-                    if (IndexChecker(matrix, bunnyMoves[l, 0], bunnyMoves[l, 1]))
-                    {
-                        if (matrix[bunnyMoves[l, 0], bunnyMoves[l, 1]] == 'P')
-                        {
-                            flag = true;
-                        }
-
-                        bunnyMatrix[bunnyMoves[l, 0], bunnyMoves[l, 1]] = 'B';
-                    }
+                    matrix[j - 1, k] = 'B';
                 }
-
-                matrix = bunnyMatrix;
+                if (!IndexChecker(matrix, j + 1, k))
+                {
+                    matrix[j + 1, k] = 'B';
+                }
+                if (!IndexChecker(matrix, j, k - 1))
+                {
+                    matrix[j, k - 1] = 'B';
+                }
+                if (!IndexChecker(matrix, j, k + 1))
+                {
+                    matrix[j, k + 1] = 'B';
+                }
             }
         }
+    }
 
-        if (flag)
+    for (int j = 0; j < matrix.GetLength(0); j++)
+    {
+        for (int k = 0; k < matrix.GetLength(1); k++)
         {
-            BoardPrinter(flag, matrix, bunnyMatrix);
-            Console.WriteLine($"dead: {playerPos[0]} {playerPos[1]}");
-            return;
+            oldBunnyMatrix[j, k] = matrix[j, k];
         }
-
-        flag = false;
     }
 
-    if (matrix[newPlayerPos[0], newPlayerPos[1]] != 'B')
-    {
-        matrix[playerPos[0], playerPos[1]] = '.';
-        playerPos = newPlayerPos;
-        matrix[playerPos[0], playerPos[1]] = 'P';
-    }
-
-}
-
-int[] PlayerMover(char direction, int[] playerPos, int[] newPlayerPos)
-{
-    newPlayerPos = new int[]
-    {
-        playerPos[0],
-        playerPos[1]
-    };
-
-    if (direction == 'U')
-    {
-        newPlayerPos[0]--;
-    }
-    else if (direction == 'D')
-    {
-        newPlayerPos[0]++;
-    }
-    else if (direction == 'L')
-    {
-        newPlayerPos[1]--;
-    }
-    else if (direction == 'R')
-    {
-        newPlayerPos[1]++;
-    }
-
-    return newPlayerPos;
-}
-
-bool IndexChecker(char[,] matrix, int row, int col)
-{
-    return row >= 0 && row < matrix.GetLength(0) &&
-           col >= 0 && col < matrix.GetLength(1);
-}
-
-void BoardPrinter(bool flag, char[,] matrix, char[,] bunnyMatrix)
-{
-    if (!flag)
+    if ((matrix[currentPosRow, currentPosCol] == 'B' && !flagWin) || flagLose)
     {
         for (int j = 0; j < matrix.GetLength(0); j++)
         {
             for (int k = 0; k < matrix.GetLength(1); k++)
             {
-                if (matrix[j, k] == 'B')
-                {
-                    int[,] bunnyMoves =
-                    {
-                        { j - 1, k },
-                        { j + 1, k },
-                        { j, k - 1 },
-                        { j, k + 1 }
-                    };
-
-                    for (int l = 0; l < bunnyMoves.GetLength(0); l++)
-                    {
-                        if (IndexChecker(matrix, bunnyMoves[l, 0], bunnyMoves[l, 1]))
-                        {
-                            bunnyMatrix[bunnyMoves[l, 0], bunnyMoves[l, 1]] = 'B';
-                        }
-                    }
-                }
+                Console.Write($"{matrix[j, k]}");
             }
+
+            Console.WriteLine();
         }
+
+        Console.WriteLine($"dead: {currentPosRow} {currentPosCol}");
+        break;
     }
 
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    if (flagWin)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(0); j++)
         {
-            Console.Write($"{bunnyMatrix[i, j]}");
+            for (int k = 0; k < matrix.GetLength(1); k++)
+            {
+                Console.Write($"{matrix[j, k]}");
+            }
+
+            Console.WriteLine();
         }
 
-        Console.WriteLine();
+        Console.WriteLine($"won: {winningRow} {winningCol}");
+        break;
     }
+}
+
+bool IndexChecker(char[,] matrix, int row, int col)
+{
+    if (row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1))
+    {
+        return false;
+    }
+
+    return true;
 }
