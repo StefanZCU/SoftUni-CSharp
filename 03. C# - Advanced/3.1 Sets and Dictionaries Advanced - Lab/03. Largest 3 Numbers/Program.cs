6fd6521b53e9cf93e6bca0a049530796eb077c6a
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-16 10:11:57 +0200

    Added 03. Largest 3 Numbers

--- 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/03. Largest 3 Numbers/Program.cs
+++ 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/03. Largest 3 Numbers/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] sortedArray = Console.ReadLine().Split().Select(int.Parse).OrderByDescending(x => x).ToArray();
+
+if (sortedArray.Length >= 3)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        Console.Write($"{sortedArray[i]} ");
+    }
+}
+else
+{
+    Console.WriteLine(string.Join(" ", sortedArray));
+}
