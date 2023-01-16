a541c0c54d4b867da154b29329183aec30bf3988
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-16 10:44:54 +0200

    Added 06. Record Unique Names

--- 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Program.cs
+++ 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿HashSet<string> names = new HashSet<string>();
+int numCycles = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numCycles; i++)
+{
+    string name = Console.ReadLine();
+    names.Add(name);
+}
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
