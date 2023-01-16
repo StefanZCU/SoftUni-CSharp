23789262e7c3ad56036d188877bca9586184a96f
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-16 10:48:19 +0200

    Added 07. Parking Lot

--- 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
+++ 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿HashSet<string> licensePlates = new HashSet<string>();
+string command;
+while ((command = Console.ReadLine()) != "END")
+{
+    string[] input = command.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+    if (input[0] == "IN")
+    {
+        licensePlates.Add(input[1]);
+    }
+    else if (input[0] == "OUT")
+    {
+        licensePlates.Remove(input[1]);
+    }
+}
+
+if (licensePlates.Count != 0)
+{
+    foreach (var licensePlate in licensePlates)
+    {
+        Console.WriteLine(licensePlate);
+    }
+}
+else
+{
+    Console.WriteLine("Parking Lot is Empty");
+}
