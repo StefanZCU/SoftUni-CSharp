2468f0c3178fd5e5801923b9168ad893066dc9f7
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-16 10:04:29 +0200

    Added 02. Average Student Grades

--- 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/02. Average Student Grades/Program.cs
+++ 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/02. Average Student Grades/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numNames = int.Parse(Console.ReadLine());
+
+Dictionary<string, List<decimal>> gradesPerName = new Dictionary<string, List<decimal>>();
+
+for (int i = 0; i < numNames; i++)
+{
+    string[] nameGrade = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    if (!gradesPerName.ContainsKey(nameGrade[0]))
+    {
+        gradesPerName.Add(nameGrade[0], new List<decimal>());
+    }
+
+    gradesPerName[nameGrade[0]].Add(decimal.Parse(nameGrade[1]));
+}
+
+foreach (var name in gradesPerName)
+{
+    Console.WriteLine($"{name.Key} -> {string.Join(" ", name.Value.ConvertAll(x => string.Format("{0:0.00}", x)))} (avg: {name.Value.Average():F2})");
+}
