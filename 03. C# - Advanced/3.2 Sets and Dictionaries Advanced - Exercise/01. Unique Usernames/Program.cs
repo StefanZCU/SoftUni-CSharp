4349fcbd8ec3811430c6c0638d4eb7378ecd0c98
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-16 12:31:22 +0200

    Added 01. Unique Usernames

--- 03. C# - Advanced/3.2 Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/Program.cs
+++ 03. C# - Advanced/3.2 Sets and Dictionaries Advanced - Exercise/01. Unique Usernames/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numNames = int.Parse(Console.ReadLine());
+HashSet<string> uniqueNames = new HashSet<string>();
+
+for (int i = 0; i < numNames; i++)
+{
+    string name = Console.ReadLine();
+    uniqueNames.Add(name);
+}
+
+foreach (var uniqueName in uniqueNames)
+{
+    Console.WriteLine(uniqueName);
+}
