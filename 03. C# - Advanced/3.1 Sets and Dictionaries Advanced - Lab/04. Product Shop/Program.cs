69dbb214f665de733e1bba03ed3910dd39befec6
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-16 10:34:55 +0200

    Added 04. Product Shop

--- 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/04. Product Shop/Program.cs
+++ 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/04. Product Shop/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Dictionary<string, Dictionary<string, double>> stores = new Dictionary<string, Dictionary<string, double>>();
+
+string command;
+while ((command = Console.ReadLine()) != "Revision")
+{
+    string[] input = command.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+    if (!stores.ContainsKey(input[0]))
+    {
+        stores.Add(input[0], new Dictionary<string, double>());
+    }
+
+    if (!stores[input[0]].ContainsKey(input[1]))
+    {
+        stores[input[0]].Add(input[1], double.Parse(input[2]));
+    }
+}
+
+foreach (var store in stores.OrderBy(x => x.Key))
+{
+    Console.WriteLine($"{store.Key}->");
+
+    foreach (var product in store.Value)
+    {
+        Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+    }
+}
