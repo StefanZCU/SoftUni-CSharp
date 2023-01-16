bfbbc2dc2296f96b1a32da86dfae0a241b2e5f4b
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-16 12:40:34 +0200

    Added 02. Sets of Elements

--- 03. C# - Advanced/3.2 Sets and Dictionaries Advanced - Exercise/02. Sets of Elements/Program.cs
+++ 03. C# - Advanced/3.2 Sets and Dictionaries Advanced - Exercise/02. Sets of Elements/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] length = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+Queue<int> firstSetOfNums = new Queue<int>();
+HashSet<int> secondSet = new HashSet<int>();
+
+for (int i = 0; i < length[0]; i++)
+{
+    int num = int.Parse(Console.ReadLine());
+
+    firstSetOfNums.Enqueue(num);
+}
+
+for (int i = 0; i < length[1]; i++)
+{
+    int num = int.Parse(Console.ReadLine());
+    secondSet.Add(num);
+}
+
+List<int> printedNums = new List<int>();
+
+for (int i = 0; i < length[0]; i++)
+{
+    int currentNum = firstSetOfNums.Dequeue();
+
+    if (secondSet.Contains(currentNum) && !printedNums.Contains(currentNum))
+    {
+        printedNums.Add(currentNum);
+        Console.Write($"{currentNum} ");
+    }
+}
