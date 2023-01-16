d08677e7fbc4db0948a5a4e6f1a0d64076f971a2
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-16 10:55:28 +0200

    Added 08. SoftUni Party

--- 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/08. SoftUni Party/Program.cs
+++ 03. C# - Advanced/3.1 Sets and Dictionaries Advanced - Lab/08. SoftUni Party/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿HashSet<string> VIPGuests = new HashSet<string>();
+HashSet<string> regularGuests = new HashSet<string>();
+
+string command;
+while ((command = Console.ReadLine()) != "PARTY")
+{
+    if (char.IsDigit(command[0]))
+    {
+        VIPGuests.Add(command);
+    }
+    else
+    {
+        regularGuests.Add(command);
+    }
+}
+
+while ((command = Console.ReadLine()) != "END")
+{
+    if (VIPGuests.Contains(command))
+    {
+        VIPGuests.Remove(command);
+    }
+    else if (regularGuests.Contains(command))
+    {
+        regularGuests.Remove(command);
+    }
+}
+
+Console.WriteLine(VIPGuests.Count + regularGuests.Count);
+
+foreach (var vipGuest in VIPGuests)
+{
+    Console.WriteLine(vipGuest);
+}
+
+foreach (var regularGuest in regularGuests)
+{
+    Console.WriteLine(regularGuest);
+}
