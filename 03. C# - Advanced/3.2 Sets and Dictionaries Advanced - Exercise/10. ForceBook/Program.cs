0ebe64d325844f0eb17954af57b119934218eef6
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-16 17:29:42 +0200

    Added temp 10. ForceBook

--- 03. C# - Advanced/3.2 Sets and Dictionaries Advanced - Exercise/10. ForceBook/Program.cs
+++ 03. C# - Advanced/3.2 Sets and Dictionaries Advanced - Exercise/10. ForceBook/Program.cs
@@ -1,2 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Dictionary<string, List<string>> usersPerForce = new Dictionary<string, List<string>>();
+
+string command;
+while((command = Console.ReadLine()) != "Lumpawaroo")
+{
+    if (command.Contains('|'))
+    {
+        string[] input = command.Split(" | ", StringSplitOptions.RemoveEmptyEntries);
+
+        string forceSide = input[0];
+        string forceUser = input[1];
+
+        if (!usersPerForce.ContainsKey(forceSide))
+        {
+            usersPerForce.Add(forceSide, new List<string>());
+        }
+
+        if (!usersPerForce[forceSide].Contains(forceUser))
+        {
+            usersPerForce[forceSide].Add(forceUser);
+        }
+    }
+    else if (command.Contains("->"))
+    {
+        string[] input = command.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+
+        string forceUser = input[0];
+        string forceSide = input[1];
+        bool flag = true;
+
+        foreach (var force in usersPerForce)
+        {
+            if (force.Value.Contains(forceUser))
+            {
+                if (!usersPerForce.ContainsKey(forceSide))
+                {
+                    usersPerForce.Add(forceSide, new List<string>());
+                }
+
+                usersPerForce[forceSide].Add(forceUser);
+                usersPerForce[force.Key].Remove(forceUser);
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag)
+        {
+            if (!usersPerForce.ContainsKey(forceSide))
+            {
+                usersPerForce.Add(forceSide, new List<string>());
+            }
+
+            usersPerForce[forceSide].Add(forceUser);
+        }
+
+        Console.WriteLine($"{forceUser} joins the {forceSide} side!");
+    }
+}
+
+foreach (var force in usersPerForce.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key))
+{
+    if (force.Value.Count != 0)
+    {
+        Console.WriteLine($"Side: {force.Key}, Members: {force.Value.Count}");
+
+        foreach (var user in force.Value.OrderBy(x => x))
+        {
+            Console.WriteLine($"! {user}");
+        }
+    }
+}
