bfde3b4abc7bdb1a914ba702dbd1f6e7cf5a734c
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-16 12:44:48 +0200

    Added 03. Periodic Table

--- 03. C# - Advanced/3.2 Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
+++ 03. C# - Advanced/3.2 Sets and Dictionaries Advanced - Exercise/03. Periodic Table/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numLines = int.Parse(Console.ReadLine());
+
+HashSet<string> uniqueChemicals = new HashSet<string>();
+
+for (int i = 0; i < numLines; i++)
+{
+    string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    for (int j = 0; j < input.Length; j++)
+    {
+        uniqueChemicals.Add(input[j]);
+    }
+}
+
+foreach (var uniqueChemical in uniqueChemicals.OrderBy(x => x))
+{
+    Console.Write($"{uniqueChemical} ");
+}
