e7fac426c1994e77946e067128bddef1f05d50ba
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-16 12:49:51 +0200

    Added 04. Even Times

--- 03. C# - Advanced/3.2 Sets and Dictionaries Advanced - Exercise/04. Even Times/Program.cs
+++ 03. C# - Advanced/3.2 Sets and Dictionaries Advanced - Exercise/04. Even Times/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numLines = int.Parse(Console.ReadLine());
+Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+for (int i = 0; i < numLines; i++)
+{
+    int currNum = int.Parse(Console.ReadLine());
+
+    if (!numbers.ContainsKey(currNum))
+    {
+        numbers.Add(currNum, 0);
+    }
+
+    numbers[currNum]++;
+}
+
+foreach (var number in numbers)
+{
+    if (number.Value % 2 == 0)
+    {
+        Console.WriteLine(number.Key);
+    }
+}
