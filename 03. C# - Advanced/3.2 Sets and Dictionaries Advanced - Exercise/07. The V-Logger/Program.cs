01e6d17fdc1314c1d7f8010a7714e63661700d0d
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-16 13:41:19 +0200

    Added 07. The V-Logger

--- 03. C# - Advanced/3.2 Sets and Dictionaries Advanced - Exercise/07. The V-Logger/Program.cs
+++ 03. C# - Advanced/3.2 Sets and Dictionaries Advanced - Exercise/07. The V-Logger/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Dictionary<string, VloggerStats> statsPerVlogger = new Dictionary<string, VloggerStats>();
+
+string command;
+while ((command = Console.ReadLine()) != "Statistics")
+{
+    string[] input = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string name = input[0];
+    string action = input[1];
+
+    if (!statsPerVlogger.ContainsKey(name) && action == "joined")
+    {
+        statsPerVlogger.Add(name, new VloggerStats());
+    }
+
+    string personToFollow = input[2];
+
+    if (action == "followed" && statsPerVlogger.ContainsKey(name) && statsPerVlogger.ContainsKey(personToFollow))
+    {
+        if (!statsPerVlogger[name].following.Contains(personToFollow) && personToFollow != name)
+        {
+            statsPerVlogger[name].following.Add(personToFollow);
+            statsPerVlogger[personToFollow].followers.Add(name);
+        }
+    }
+
+}
+
+Console.WriteLine($"The V-Logger has a total of {statsPerVlogger.Keys.Count} vloggers in its logs.");
+
+int index = 1;
+
+foreach (var vlogger in statsPerVlogger.OrderByDescending(x => x.Value.followers.Count).ThenBy(x => x.Value.following.Count))
+{
+    if (index == 1)
+    {
+        Console.WriteLine($"{index}. {vlogger.Key} : {vlogger.Value.followers.Count} followers, {vlogger.Value.following.Count} following");
+
+        foreach (var follower in vlogger.Value.followers.OrderBy(x => x))
+        {
+            Console.WriteLine($"*  {follower}");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"{index}. {vlogger.Key} : {vlogger.Value.followers.Count} followers, {vlogger.Value.following.Count} following");
+    }
+
+
+    index++;
+}
+
+
+class VloggerStats
+{
+    public List<string> following { get; set; } = new List<string>();
+    public List<string> followers { get; set; } = new List<string>();
+
+}
