f8d23a1c61ccdc3e1e6a37ea654928944867a706
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-18 10:06:06 +0200

    Added 04. Merge Files

--- 03. C# - Advanced/4.1 Streams, Files and Directories - Lab/Skeleton-Lab/MergeFiles/MergeFiles.cs
+++ 03. C# - Advanced/4.1 Streams, Files and Directories - Lab/Skeleton-Lab/MergeFiles/MergeFiles.cs
@@ -15,6 +15,29 @@
 
         public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
         {
+            using (var input1 = new StreamReader(firstInputFilePath))
+            {
+                using (var input2 = new StreamReader(secondInputFilePath))
+                {
+                    using (var writer = new StreamWriter(outputFilePath))
+                    {
+                        string line1;
+                        string line2;
+                        while ((line1 = input1.ReadLine()) != null && (line2 = input2.ReadLine()) != null)
+                        {
+                            if (line2 != null)
+                            {
+                                writer.WriteLine(line1);
+                            }
+
+                            if (line1 != null)
+                            {
+                                writer.WriteLine(line2);
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
