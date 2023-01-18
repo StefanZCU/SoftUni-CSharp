844af10c58ed449e1f9aa276691d53ac312793f1
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-18 09:05:57 +0200

    Added 02. Line Numbers

--- 03. C# - Advanced/4.1 Streams, Files and Directories - Lab/Skeleton-Lab/LineNumbers/LineNumbers.cs
+++ 03. C# - Advanced/4.1 Streams, Files and Directories - Lab/Skeleton-Lab/LineNumbers/LineNumbers.cs
@@ -13,6 +13,20 @@
 
         public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
         {
+            using (var reader = new StreamReader(inputFilePath))
+            {
+                using (var writer = new StreamWriter(outputFilePath))
+                {
+                    string line;
+                    int counter = 1;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        writer.WriteLine($"{counter}. {line}");
+
+                        counter++;
+                    }
+                }
+            }
         }
     }
 }
