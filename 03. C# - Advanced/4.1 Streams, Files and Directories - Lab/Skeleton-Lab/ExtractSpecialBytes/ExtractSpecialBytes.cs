e63eeca8d1c5059f1015b562c0438d0c1ad230fe
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-18 10:54:31 +0200

    Added 04. Extract Special Bytes

--- 03. C# - Advanced/4.1 Streams, Files and Directories - Lab/Skeleton-Lab/ExtractSpecialBytes/ExtractSpecialBytes.cs
+++ 03. C# - Advanced/4.1 Streams, Files and Directories - Lab/Skeleton-Lab/ExtractSpecialBytes/ExtractSpecialBytes.cs
@@ -1,4 +1,7 @@
-﻿namespace ExtractSpecialBytes
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace ExtractBytes
 {
     using System;
     using System.IO;
@@ -15,6 +18,32 @@
 
         public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
         {
+            List<byte> bytes = new List<byte>();
+            using (var reader = new StreamReader(bytesFilePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    bytes.Add(byte.Parse(line));
+                }
+            }
+
+            List<byte> occurences = new List<byte>();
+            using (var binaryBytes = new FileStream(binaryFilePath, FileMode.Open))
+            {
+                byte[] buffer = new byte[binaryBytes.Length];
+                binaryBytes.Read(buffer, 0, buffer.Length);
+                for (int i = 0; i < buffer.Length; i++)
+                {
+                    if (bytes.Contains(buffer[i]))
+                        occurences.Add(buffer[i]);
+                }
+            }
+
+            using (var outputFile = new FileStream(outputPath, FileMode.Create))
+            {
+                outputFile.Write(occurences.ToArray(), 0, occurences.Count);
+            }
         }
     }
 }
