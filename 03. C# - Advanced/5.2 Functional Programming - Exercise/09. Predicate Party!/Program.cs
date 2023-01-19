f0dc3f7a49a96a477321afea3019f44c0b055d54
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-19 12:18:09 +0200

    Added 09. Predicate Party!

--- 03. C# - Advanced/5.2 Functional Programming - Exercise/09. Predicate Party!/Program.cs
+++ 03. C# - Advanced/5.2 Functional Programming - Exercise/09. Predicate Party!/Program.cs
@@ -1,2 +1,90 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<string> names = Console.ReadLine().Split().ToList();
+
+string command;
+while ((command = Console.ReadLine()) != "Party!")
+{
+    string[] input = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string action = input[0];
+    string criteria = input[1];
+    string value = input[2];
+
+    if (action == "Remove")
+    {
+        switch (criteria)
+        {
+            case "StartsWith":
+
+                Manipulator(action, names, x => x.Substring(0, value.Length) == value);
+
+                break;
+
+            case "EndsWith":
+
+                Manipulator(action, names, x => x.Substring(x.Length - value.Length, value.Length) == value);
+
+                break;
+
+            case "Length":
+
+                Manipulator(action, names, x => x.Length == int.Parse(value));
+
+                break;
+        }
+    }
+    else if (action == "Double")
+    {
+        switch (criteria)
+        {
+            case "StartsWith":
+
+                Manipulator(action, names, x => x.Substring(0, value.Length) == value);
+
+                break;
+
+            case "EndsWith":
+
+                Manipulator(action, names, x => x.Substring(x.Length - value.Length, value.Length) == value);
+
+                break;
+
+            case "Length":
+
+                Manipulator(action, names, x => x.Length == int.Parse(value));
+
+                break;
+        }
+    }
+}
+
+if (names.Count != 0)
+{
+    Console.WriteLine($"{string.Join(", ", names)} are going to the party!");
+}
+else
+{
+    Console.WriteLine("Nobody is going to the party!");
+}
+
+
+
+
+void Manipulator(string action, List<string> names, Predicate<string> op)
+{
+    if (action == "Remove")
+    {
+        names.RemoveAll(op);
+    }
+    else
+    {
+        for (int i = 0; i < names.Count; i++)
+        {
+            if (op(names[i]))
+            {
+                names.Insert(i, names[i]);
+                i++;
+            }
+        }
+    }
+}
+
