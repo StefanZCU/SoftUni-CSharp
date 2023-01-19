049719e1dc3d98718f17a49047e24c684e0c534f
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-19 10:14:49 +0200

    Added 03. Count Uppercase Words

--- 03. C# - Advanced/5.1 Functional Programming - Lab/03. Count Uppercase Words/Program.cs
+++ 03. C# - Advanced/5.1 Functional Programming - Lab/03. Count Uppercase Words/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] sentence = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Func<string, bool> CheckUpper = x => char.IsUpper(x[0]);
+
+for (int i = 0; i < sentence.Length; i++)
+{
+    if (CheckUpper(sentence[i]))
+    {
+        Console.WriteLine(sentence[i]);
+    }
+}
+
