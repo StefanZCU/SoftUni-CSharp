4116f60080e3955356c405b6f2f0d5adb485f9b4
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-19 10:54:12 +0200

    Added 03. Custom Min Function

--- 03. C# - Advanced/5.2 Functional Programming - Exercise/03. Custom Min Function/Program.cs
+++ 03. C# - Advanced/5.2 Functional Programming - Exercise/03. Custom Min Function/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int smallest = FindSmallestNumber(numbers, (arr) =>
+{
+    int min = int.MaxValue;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    return min;
+});
+
+Console.WriteLine(smallest);
+
+int FindSmallestNumber(int[] numbers, Func<int[], int> func)
+{
+    return func(numbers);
+}
