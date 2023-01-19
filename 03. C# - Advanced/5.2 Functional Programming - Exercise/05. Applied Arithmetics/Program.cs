360d0fe7e54cddbed4fa5338172b8b7e027a3f1e
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-19 11:31:56 +0200

    Added 05. Applied Arithmetics

--- 03. C# - Advanced/5.2 Functional Programming - Exercise/05. Applied Arithmetics/Program.cs
+++ 03. C# - Advanced/5.2 Functional Programming - Exercise/05. Applied Arithmetics/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+string command;
+while ((command = Console.ReadLine()) != "end")
+{
+    if (command == "add")
+    {
+        Calculator(numbers, x => x + 1);
+    }
+    else if (command == "multiply")
+    {
+        Calculator(numbers, x => x * 2);
+    }
+    else if (command == "subtract")
+    {
+        Calculator(numbers, x => x - 1);
+    }
+    else if (command == "print")
+    {
+        Console.WriteLine(string.Join(" ", numbers));
+    }
+}
+
+void Calculator(int[] numbers, Func<int, int> op)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = op(numbers[i]);
+    }
+}
