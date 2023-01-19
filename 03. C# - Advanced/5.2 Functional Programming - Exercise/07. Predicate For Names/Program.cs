670a632de3bc44259125da80be9701192a13df5a
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-19 11:44:14 +0200

    Added 07. Predicate For Names

--- 03. C# - Advanced/5.2 Functional Programming - Exercise/07. Predicate For Names/Program.cs
+++ 03. C# - Advanced/5.2 Functional Programming - Exercise/07. Predicate For Names/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int nameLength = int.Parse(Console.ReadLine());
+
+string[] names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Where(x => x.Length <= nameLength)
+    .ToArray();
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
