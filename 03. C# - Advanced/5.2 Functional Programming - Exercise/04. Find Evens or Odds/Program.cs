1cfdf709af3e07270e2e7cef212e812c9a5d748e
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-19 11:11:08 +0200

    Added 04. Find Even or Odds

--- 03. C# - Advanced/5.2 Functional Programming - Exercise/04. Find Evens or Odds/Program.cs
+++ 03. C# - Advanced/5.2 Functional Programming - Exercise/04. Find Evens or Odds/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] rangeNum = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+string oddOrEven = Console.ReadLine();
+
+List<int> newNumbers = new List<int>();
+
+for(int i = rangeNum[0]; i <= rangeNum[1]; i++)
+{
+    if (oddOrEven == "odd")
+    {
+        if (NumberChecker(i, n => n % 2 == 1))
+        {
+            newNumbers.Add(i);
+        }
+    }
+    else
+    {
+        if (NumberChecker(i, n => n % 2 == 0))
+        {
+            newNumbers.Add(i);
+        }
+    }
+}
+
+Console.WriteLine(string.Join(" ", newNumbers));
+
+bool NumberChecker(int number, Predicate<int> oddEven)
+{
+    return oddEven(number);
+}
