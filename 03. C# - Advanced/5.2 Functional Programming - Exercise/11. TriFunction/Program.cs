42b1fc888f0aeb5f8327b2baa93e08649cce6028
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-19 13:55:44 +0200

    Added 11. TriFunction

--- 03. C# - Advanced/5.2 Functional Programming - Exercise/11. TriFunction/Program.cs
+++ 03. C# - Advanced/5.2 Functional Programming - Exercise/11. TriFunction/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = int.Parse(Console.ReadLine());
+
+string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Func<string, int, bool> nameCheckerFunc = NameChecker;
+
+string name = names.FirstOrDefault(s => nameCheckerFunc(s, n));
+
+Console.WriteLine(name);
+
+bool NameChecker(string s, int n)
+{
+    int sum = s.Sum(ch =>
+    {
+        return (int)ch;
+
+    });
+
+    return sum >= n;
+}
