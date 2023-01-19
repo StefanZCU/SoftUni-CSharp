1a56e4d2a6a33b79e05dadaa49241b19d8d1b4e6
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-19 11:50:18 +0200

    Added 08. List Of Predicates

--- 03. C# - Advanced/5.2 Functional Programming - Exercise/08. List Of Predicates/Program.cs
+++ 03. C# - Advanced/5.2 Functional Programming - Exercise/08. List Of Predicates/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int endRange = int.Parse(Console.ReadLine());
+
+List<int> dividers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+List<int> numbers = new List<int>();
+
+for (int i = 1; i <= endRange; i++)
+{
+    numbers.Add(i);
+}
+
+foreach (var divider in dividers)
+{
+    Func<int, bool> filterFunc = x => x % divider == 0;
+
+    numbers = numbers.Where(filterFunc).ToList();
+}
+
+Console.WriteLine(string.Join(" ", numbers));
