08d73210417909fdc88d4857db4dd5142420164a
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-19 10:26:33 +0200

    Added 05. Filter By Age

--- 03. C# - Advanced/5.1 Functional Programming - Lab/05. Filter By Age/Program.cs
+++ 03. C# - Advanced/5.1 Functional Programming - Lab/05. Filter By Age/Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int peopleCount = int.Parse(Console.ReadLine());
+
+var people = new List<Person>();
+
+for (int i = 0; i < peopleCount; i++)
+{
+    var tokens = Console.ReadLine()
+        .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+        .ToArray();
+
+    string personName = tokens[0];
+    int personAge = int.Parse(tokens[1]);
+
+    var person = new Person()
+    {
+        Age = personAge,
+        Name = personName
+    };
+
+    people.Add(person);
+}
+
+string condition = Console.ReadLine();
+int age = int.Parse(Console.ReadLine());
+
+Func<Person, bool> filterFunc = GetFilter(condition, age);
+
+string format = Console.ReadLine();
+
+Func<Person, string> selectFunc = GetSelector(format);
+
+var result = people
+    .Where(filterFunc)
+    .Select(selectFunc)
+    .ToList();
+
+foreach (var person in result)
+{
+    Console.WriteLine(person);
+}
+
+static Func<Person, string> GetSelector(string format)
+{
+    if (format == "name age")
+    {
+        return p => $"{p.Name} - {p.Age}";
+    }
+    else if (format == "age")
+    {
+        return p => $"{p.Age}";
+    }
+    else
+    {
+        return p => $"{p.Name}";
+    }
+}
+
+static Func<Person, bool> GetFilter(string condition, int age)
+{
+    if (condition == "older")
+    {
+        return x => x.Age >= age;
+    }
+    else
+    {
+        return x => x.Age < age;
+    }
+}
+
+public class Person
+{
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+}
