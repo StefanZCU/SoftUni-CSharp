070de4707bff9d1e88366a13afd32fedc3a7610e
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-20 13:28:31 +0200

    Added 04. Opinion Poll

---
+++ 03. C# - Advanced/6.2 Defining Classes - Exercise/04. Opinion Poll/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        public string Name { get; set; } = default!;
+        public int Age { get; set; }
+
+        public Person()
+        {
+            Name = "No name";
+            Age = 1;
+        }
+
+        public Person(int age)
+        {
+            Name = "No name";
+            Age = age;
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
--- 03. C# - Advanced/6.2 Defining Classes - Exercise/04. Opinion Poll/Program.cs
+++ 03. C# - Advanced/6.2 Defining Classes - Exercise/04. Opinion Poll/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace _04._Opinion_Poll
+﻿namespace DefiningClasses
 {
-    internal class Program
+    public class StartUp
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Person> people = new List<Person>();
+
+            int numCycles = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numCycles; i++)
+            {
+                string[] currPerson = Console.ReadLine().Split();
+
+                Person person = new Person(currPerson[0], int.Parse(currPerson[1]));
+                people.Add(person);
+            }
+
+            foreach (var person in people.Where(x => x.Age > 30).OrderBy(x => x.Name))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
         }
     }
 }
