ba35b7e22223018b0bbf2c8aabffc45a7882d437
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-20 11:39:34 +0200

    Added 04. Car Engine And Tires

---
+++ 03. C# - Advanced/6.1 Defining Classes - Lab/04. Car Engine And Tires/Engine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Engine
+    {
+        public int HorsePower { get; set; }
+        public double CubicCapacity { get; set; }
+
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            HorsePower = horsePower;
+            CubicCapacity = cubicCapacity;
+        }
+    }
+}
---
+++ 03. C# - Advanced/6.1 Defining Classes - Lab/04. Car Engine And Tires/Tire.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Tire
+    {
+        public int Year { get; set; }
+        public double Pressure { get; set; }
+
+        public Tire(int year, double pressure)
+        {
+            Year = year;
+            Pressure = pressure;
+        }
+    }
+}
