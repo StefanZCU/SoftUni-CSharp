8f453a7c9231f5f57a13cf967567267c85ffcb6e
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-20 11:13:09 +0200

    Added 6.1 Defining Classes - Lab

---
+++ 03. C# - Advanced/6.1 Defining Classes - Lab/01. Car/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._Car
+{
+    public class Car
+    {
+		private string make;
+
+		public string Make
+		{
+			get { return make; }
+			set { make = value; }
+		}
+
+		private string model;
+
+		public string Model
+		{
+			get { return model; }
+			set { model = value; }
+		}
+
+		private int year;
+
+		public int Year
+		{
+			get { return year; }
+			set { year = value; }
+		}
+
+
+	}
+}
