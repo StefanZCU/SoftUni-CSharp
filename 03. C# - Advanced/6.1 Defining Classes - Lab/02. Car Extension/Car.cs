85153773a4731e3f792f0bc3f9e248897a86fdc9
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-20 11:29:28 +0200

    Added 02. Car Extension

---
+++ 03. C# - Advanced/6.1 Defining Classes - Lab/02. Car Extension/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        public string Make { get; set; } = default!;
+        public string Model { get; set; } = default!;
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+
+        public void Drive(double distance)
+        {
+            Console.WriteLine(FuelQuantity - (distance * FuelConsumption) > 0 
+                ? FuelQuantity -= (distance * FuelConsumption)
+                : "Not enough fuel to perform this trip!");
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\n" +
+                   $"Model: {this.Model}\n" +
+                   $"Year: {this.Year}\n" +
+                   $"Fuel: {this.FuelQuantity:F2}";
+        }
+	}
+}
--- 03. C# - Advanced/6.1 Defining Classes - Lab/02. Car Extension/Program.cs
+++ 03. C# - Advanced/6.1 Defining Classes - Lab/02. Car Extension/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CarManufacturer;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+
+    }
+}
