11fa4c9b428e23173341322d82e545fe1694b09a
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-21 10:35:02 +0200

    Added 01. Generic Box Of String

---
+++ 03. C# - Advanced/7.2 Generics - Exercise/01. Generic Box of String/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        private T element;
+
+        public Box(T element)
+        {
+            this.element = element;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.element.GetType()}: {this.element}";
+        }
+    }
+}
--- 03. C# - Advanced/7.2 Generics - Exercise/01. Generic Box of String/Program.cs
+++ 03. C# - Advanced/7.2 Generics - Exercise/01. Generic Box of String/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace GenericBoxOfString
+﻿using System.Linq.Expressions;
+
+namespace GenericBoxOfString
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numBoxes = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numBoxes; i++)
+            {
+                string line = Console.ReadLine();
+
+                Box<string> box = new Box<string>(line);
+
+                Console.WriteLine(box);
+            }
         }
     }
 }
