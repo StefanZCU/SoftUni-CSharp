11668188ed0fa05b5c938094567f3ee76257e7b3
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-21 10:21:31 +0200

    Added 03. Generic Scale

---
+++ 03. C# - Advanced/7.1 Generics - Lab/03. Generic Scale/EqualityScale.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        private T left { get; set; }
+        private T right { get; set; }
+
+        public EqualityScale(T left, T right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public bool AreEqual()
+        {
+            return left.Equals(right);
+        }
+    }
+}
--- 03. C# - Advanced/7.1 Generics - Lab/03. Generic Scale/StartUp.cs
+++ 03. C# - Advanced/7.1 Generics - Lab/03. Generic Scale/StartUp.cs
@@ -4,7 +4,8 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var areEqual = new EqualityScale<string>("Pesho", "Pesho");
+            Console.WriteLine(areEqual.AreEqual());
         }
     }
 }
