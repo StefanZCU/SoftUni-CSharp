21e4489cec44c1def02a33c6d5ebba2e40da07f1
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-21 10:15:25 +0200

    Added 02. Array Creator

--- 03. C# - Advanced/7.1 Generics - Lab/02. Array Creator/StartUp.cs
+++ 03. C# - Advanced/7.1 Generics - Lab/02. Array Creator/StartUp.cs
@@ -4,7 +4,8 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] strings = ArrayCreator.Create(5, "Pesho");
+
         }
     }
 }
