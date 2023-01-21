00522cbccfd74980202846a9cc246eab8abc2e50
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-21 18:11:32 +0200

    Added 03. The Race

---
+++ 03. C# - Advanced/0.1 Final Exam - Prep/20-Feb-2021/03. The Race_Skeleton/The Race - skeleton/Race.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheRace
+{
+    public class Race
+    {
+        private List<Racer> data;
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count => data.Count;
+
+        public Race(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            data = new List<Racer>();
+        }
+
+        public void Add(Racer Racer)
+        {
+            if (data.Count != Capacity)
+            {
+                data.Add(Racer);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            foreach (var racer in data)
+            {
+                if (racer.Name == name)
+                {
+                    data.Remove(racer);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public Racer GetOldestRacer()
+        {
+            return data.OrderByDescending(x => x.Age).First();
+        }
+
+        public Racer GetRacer(string name)
+        {
+            return data.FirstOrDefault(x => x.Name == name);
+        }
+
+        public Racer GetFastestRacer()
+        {
+            return data.OrderByDescending(x => x.Car.Speed).First();
+        }
+
+        public string Report()
+        {
+            return $"Racers participating at {Name}:\n{string.Join("\n", data)}";
+        }
+
+    }
+}
---
+++ 03. C# - Advanced/0.1 Final Exam - Prep/20-Feb-2021/03. The Race_Skeleton/The Race - skeleton/Racer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace TheRace
+{
+    public class Racer
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+        public Car Car { get; set; }
+
+        public Racer(string name, int age, string country, Car car)
+        {
+            Name = name;
+            Age = age;
+            Country = country;
+            Car = car; 
+        }
+
+        public override string ToString()
+        {
+            return $"Racer: {Name}, {Age} ({Country})";
+        }
+    }
+}
