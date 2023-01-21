07cd7065426f69513d8eb25ac2367f048bacbbe0
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-21 15:44:38 +0200

    Added 09. Custom Linked List

---
+++ 03. C# - Advanced/7.2 Generics - Exercise/09. Custom Linked List/DoublyLinkedList.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomDoublyLinkedList
+{
+    public class DoublyLinkedList<T>
+    {
+        public class ListNode
+        {
+            public T Value { get; set; }
+            public ListNode NextNode { get; set; }
+            public ListNode PreviousNode { get; set; }
+
+            public ListNode(T value)
+            {
+                this.Value = value;
+            }
+        }
+
+        private ListNode head;
+        private ListNode tail;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(T element)
+        {
+            if (this.Count == 0)
+            {
+                this.head = this.tail = new ListNode(element);
+            }
+            else
+            {
+                var newHead = new ListNode(element)
+                {
+                    NextNode = this.head
+                };
+                this.head.PreviousNode = newHead;
+                this.head = newHead;
+            }
+
+            this.Count++;
+        }
+
+        public void AddLast(T element)
+        {
+            if (this.Count == 0)
+            {
+                this.head = this.tail = new ListNode(element);
+            }
+            else
+            {
+                var newTail = new ListNode(element)
+                {
+                    PreviousNode = this.tail
+                };
+                this.tail.NextNode = newTail;
+                this.tail = newTail;
+            }
+
+            this.Count++;
+        }
+
+        public T RemoveFirst()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("This list is empty");
+            }
+
+            var firstElement = this.head.Value;
+            this.head = this.head.NextNode;
+            if (this.head != null)
+            {
+                this.head.PreviousNode = null;
+            }
+            else
+            {
+                this.tail = null;
+            }
+
+            this.Count--;
+            return firstElement;
+        }
+
+        public T RemoveLast()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("This list is empty");
+            }
+
+            var lastElement = this.tail.Value;
+            this.tail = this.tail.PreviousNode;
+            if (this.tail != null)
+            {
+                this.tail.NextNode = null;
+            }
+            else
+            {
+                this.head = null;
+            }
+
+            this.Count--;
+            return lastElement;
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            var currentNode = this.head;
+            while (currentNode != null)
+            {
+                action(currentNode.Value);
+                currentNode = currentNode.NextNode;
+            }
+        }
+
+        public T[] ToArray()
+        {
+            T[] array = new T[this.Count];
+            int counter = 0;
+            var currentNode = this.head;
+            while (currentNode != null)
+            {
+                array[counter] = currentNode.Value;
+                currentNode = currentNode.NextNode;
+                counter++;
+            }
+
+            return array;
+        }
+    }
+}
---
+++ 03. C# - Advanced/7.2 Generics - Exercise/09. Custom Linked List/StartUp.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomDoublyLinkedList
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
