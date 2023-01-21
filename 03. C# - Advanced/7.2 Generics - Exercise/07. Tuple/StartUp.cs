6d1cedc29d332ee24edf485daecaea687c577817
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-21 12:41:52 +0200

    Added 07. Tuple

---
+++ 03. C# - Advanced/7.2 Generics - Exercise/07. Tuple/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace Tuple
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var personInfo = Console.ReadLine().Split();
+
+            string fullName = $"{personInfo[0]} {personInfo[1]}";
+            string address = personInfo[2];
+
+            var personData = new Tuple<string, string>(fullName, address);
+
+            Console.WriteLine(personData);
+
+            var personBeer = Console.ReadLine().Split();
+
+            string name = personBeer[0];
+            int beerQnt = int.Parse(personBeer[1]);
+
+            var beerData = new Tuple<string, int>(name, beerQnt);
+
+            Console.WriteLine(beerData);
+
+            var numbersLine = Console.ReadLine().Split();
+
+            int intNum = int.Parse(numbersLine[0]);
+            double doubleNum = double.Parse(numbersLine[1]);
+
+            var numberData = new Tuple<int, double>(intNum, doubleNum);
+            Console.WriteLine(numberData);
+        }
+    }
+}
---
+++ 03. C# - Advanced/7.2 Generics - Exercise/07. Tuple/Tuple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuple
+{
+    public class Tuple<T1, T2>
+    {
+        public T1 ItemOne { get; set; }
+        public T2 ItemTwo { get; set; }
+
+        public Tuple(T1 itemOne, T2 itemTwo)
+        {
+            ItemOne = itemOne;
+            ItemTwo = itemTwo;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.ItemOne} -> {this.ItemTwo}";
+        }
+    }
+}
