6cf0b8b4638dac5cd1d8969d7d34d74d92e34f90
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-21 12:00:38 +0200

    Added 04. Generic Swap Method Integer

---
+++ 03. C# - Advanced/7.2 Generics - Exercise/04. Generic Swap Method Integer/Box.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericSwapMethodInteger
+{
+    public class Box
+    {
+        public List<T> SwapMethod<T>(int indexOne, int indexTwo, List<T> elements)
+        {
+            (elements[indexOne], elements[indexTwo]) = (elements[indexTwo], elements[indexOne]);
+
+            return elements;
+        }
+    }
+}
---
+++ 03. C# - Advanced/7.2 Generics - Exercise/04. Generic Swap Method Integer/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace GenericSwapMethodInteger
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int numBoxes = int.Parse(Console.ReadLine());
+
+            List<int> boxes = new List<int>();
+
+            for (int i = 0; i < numBoxes; i++)
+            {
+                int element = int.Parse(Console.ReadLine());
+                boxes.Add(element);
+            }
+
+            int[] indexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int indexOne = indexes[0];
+            int indexTwo = indexes[1];
+
+            Box boxObject = new Box();
+
+            boxObject.SwapMethod(indexOne, indexTwo, boxes);
+
+            foreach (var box in boxes)
+            {
+                Console.WriteLine($"{box.GetType()}: {box}");
+            }
+        }
+    }
+}
