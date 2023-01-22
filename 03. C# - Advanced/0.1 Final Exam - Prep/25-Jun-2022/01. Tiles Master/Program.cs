7985d8fdc7094334369e1753dac77baca413abff
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-22 12:48:34 +0200

    Added 01. Tiles Master

--- 03. C# - Advanced/0.1 Final Exam - Prep/25-Jun-2022/01. Tiles Master/Program.cs
+++ 03. C# - Advanced/0.1 Final Exam - Prep/25-Jun-2022/01. Tiles Master/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _01._Tiles_Master
 {
@@ -6,7 +8,74 @@ namespace _01._Tiles_Master
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack<int> whiteTiles = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray());
+            Queue<int> greyTiles = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray());
+
+            Dictionary<string, int> locationCount = new Dictionary<string, int>()
+            {
+                { "Sink", 0 },
+                { "Oven", 0 },
+                { "Countertop", 0 },
+                { "Wall", 0 },
+                { "Floor", 0 }
+            };
+
+
+            while (whiteTiles.Any() && greyTiles.Any())
+            {
+                if (whiteTiles.Peek() == greyTiles.Peek())
+                {
+                    int newTile = whiteTiles.Peek() + greyTiles.Peek();
+
+                    if (newTile == 70)
+                    {
+                        locationCount["Wall"]++;
+                    }
+                    else if (newTile == 60)
+                    {
+                        locationCount["Countertop"]++;
+                    }
+                    else if (newTile == 50)
+                    {
+                        locationCount["Oven"]++;
+                    }
+                    else if (newTile == 40)
+                    {
+                        locationCount["Sink"]++;
+                    }
+                    else
+                    {
+                        locationCount["Floor"]++;
+                    }
+
+                    whiteTiles.Pop();
+                    greyTiles.Dequeue();
+                }
+                else
+                {
+                    int newWhiteTile = whiteTiles.Pop();
+                    whiteTiles.Push(newWhiteTile / 2);
+                    greyTiles.Enqueue(greyTiles.Dequeue());
+                }
+            }
+
+            Console.WriteLine(whiteTiles.Count == 0
+                ? "White tiles left: none"
+                : $"White tiles left: {string.Join(", ", whiteTiles)}");
+
+            Console.WriteLine(greyTiles.Count == 0
+                ? "Grey tiles left: none"
+                : $"Grey tiles left: {string.Join(", ", greyTiles)}");
+
+            foreach (var location in locationCount.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                if (location.Value != 0)
+                {
+                    Console.WriteLine($"{location.Key}: {location.Value}");
+                }
+            }
         }
     }
 }
