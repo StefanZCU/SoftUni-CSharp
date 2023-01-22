6007744a50513731440a966a8073cd7fe48a3193
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-22 09:59:37 +0200

    Added 03. ShoeStore
    There is a problem with the answer checker for this problem. It keeps giving me 77/100, even the author's answer gives 77/100

---
+++ 03. C# - Advanced/0.1 Final Exam - Prep/14-Dec-2022/03. Shoe Store_Skeleton_6.0/Shoe.cs
@@ -0,0 +1,23 @@
+﻿namespace ShoeStore
+{
+    public class Shoe
+    {
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public double Size { get; set; }
+        public string Material { get; set; }
+
+        public Shoe(string brand, string type, double size, string material)
+        {
+            Brand = brand;
+            Type = type;
+            Size = size;
+            Material = material;
+        }
+
+        public override string ToString()
+        {
+            return $"Size: {Size}, {Material} {Brand} {Type} shoe.";
+        }
+    }
+}
---
+++ 03. C# - Advanced/0.1 Final Exam - Prep/14-Dec-2022/03. Shoe Store_Skeleton_6.0/ShoeStore.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ShoeStore
+{
+    public class ShoeStore
+    {
+        public string Name { get; set; }
+        public int StorageCapacity { get; set; }
+        public List<Shoe> Shoes { get;}
+        public int Count => Shoes.Count;
+
+        public ShoeStore(string name, int storageCapacity)
+        {
+            Name = name;
+            StorageCapacity = storageCapacity;
+            Shoes = new List<Shoe>();
+        }
+
+        public string AddShoe(Shoe shoe)
+        {
+            if (Shoes.Count == StorageCapacity)
+            {
+                return "No more space in the storage room.";
+            }
+
+            Shoes.Add(shoe);
+
+            return $"Successfully added {shoe.Type} {shoe.Material} pair of shoes to the store.";
+        }
+
+        public int RemoveShoes(string material)
+        {
+            int shoesRemoved = 0;
+            List<Shoe> shoesToRemove = new List<Shoe>();
+
+            foreach (var shoe in Shoes)
+            {
+                if (shoe.Material.ToLower() == material.ToLower())
+                {
+                    shoesToRemove.Add(shoe);
+                    shoesRemoved++;
+                }
+            }
+
+            foreach (var shoe in shoesToRemove)
+            {
+                Shoes.Remove(shoe);
+            }
+
+            return shoesRemoved;
+        }
+
+        public List<Shoe> GetShoesByType(string type)
+        {
+            return Shoes.Where(x => String.Equals(x.Type, type, StringComparison.CurrentCultureIgnoreCase)).ToList();
+        }
+
+        public Shoe GetShoeBySize(double size)
+        {
+            return Shoes.FirstOrDefault(x => x.Size == size);
+        }
+
+        public string StockList(double size, string type)
+        {
+            List<Shoe> stockList = Shoes.Where(s => s.Size == size && s.Type == type).ToList();
+
+            StringBuilder sb = new StringBuilder();
+            if (stockList.Count == 0)
+            {
+                sb.AppendLine("No matches found!");
+            }
+            else
+            {
+                sb.AppendLine($"Stock list for size {size} - {type} shoes:");
+                foreach (Shoe shoe1 in stockList)
+                {
+                    sb.AppendLine(shoe1.ToString());
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
