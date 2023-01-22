11f6240c7a6d872cf9ae80889d86cb470f2e7b14
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-22 10:42:13 +0200

    Added 03. Computer Architecture

---
+++ 03. C# - Advanced/0.1 Final Exam - Prep/22-Oct-2022/03. Computer Architecture/CPU.cs
@@ -0,0 +1,22 @@
+﻿namespace ComputerArchitecture
+{
+    public class CPU
+    {
+        public string Brand { get; set; }
+        public int Cores { get; set; }
+        public double Frequency { get; set; }
+
+        public CPU(string brand, int cores, double frequency)
+        {
+            Brand = brand;
+            Cores = cores;
+            Frequency = frequency;
+        }
+
+        public override string ToString()
+        {
+            return $"{Brand} CPU:\nCores: {Cores}\nFrequency: {Frequency:F1} GHz"
+                ;
+        }
+    }
+}
---
+++ 03. C# - Advanced/0.1 Final Exam - Prep/22-Oct-2022/03. Computer Architecture/Computer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace ComputerArchitecture
+{
+    public class Computer
+    {
+        public string Model { get; set; }
+        public int Capacity { get; set; }
+        public List<CPU> Multiprocessor { get; set; }
+        public int Count => Multiprocessor.Count;
+
+        public Computer(string model, int capacity)
+        {
+            Model = model;
+            Capacity = capacity;
+            Multiprocessor = new List<CPU>();
+        }
+
+        public void Add(CPU cpu)
+        {
+            if (Multiprocessor.Count == Capacity)
+            {
+                return;
+            }
+
+            Multiprocessor.Add(cpu);
+        }
+
+        public bool Remove(string brand)
+        {
+            if (Multiprocessor.Any(x => x.Brand.ToLower() == brand.ToLower()))
+            {
+                Multiprocessor.Remove(Multiprocessor.Find(x => x.Brand.ToLower() == brand.ToLower()));
+                return true;
+            }
+
+            return false;
+        }
+
+        public CPU MostPowerful()
+        {
+            return Multiprocessor.OrderByDescending(x => x.Frequency).First();
+        }
+
+        public CPU GetCPU(string brand)
+        {
+            return Multiprocessor.Find(x => x.Brand.ToLower() == brand.ToLower());
+        }
+
+        public string Report()
+        {
+            return $"CPUs in the Computer {Model}:\n{string.Join("\n", Multiprocessor)}";
+        }
+    }
+}
