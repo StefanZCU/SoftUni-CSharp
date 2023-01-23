b65c598a1ddb5ae91644e237a4b7f6d6c596c213
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-23 13:46:48 +0200

    Added 01. Food Finder

--- 03. C# - Advanced/0.1 Final Exam - Prep/23-Oct-2021/01. Food Finder/Program.cs
+++ 03. C# - Advanced/0.1 Final Exam - Prep/23-Oct-2021/01. Food Finder/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace _01._Food_Finder
 {
@@ -6,7 +10,71 @@ namespace _01._Food_Finder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Queue<char> originalVowels = new Queue<char>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray());
+
+            Stack<char> originalConsonants = new Stack<char>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray());
+
+
+            List<string> wordsToFind = new List<string>
+            {
+                {"pear"}, {"flour"}, {"pork"}, {"olive"}
+            };
+
+            List<string> wordsFound = new List<string>();
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                string word = wordsToFind[i];
+
+                var currentQueueV = new Queue<char>();
+                foreach (var c in originalVowels)
+                {
+                    currentQueueV.Enqueue(c);
+                }
+
+                var currentStackC = new Stack<char>();
+                foreach (var c in originalConsonants)
+                {
+                    currentStackC.Push(c);
+                }
+
+                StringBuilder sb = new StringBuilder();
+
+                while (currentStackC.Any())
+                {
+                    char currentConsonant = currentStackC.Pop();
+                    char currentVowel = currentQueueV.Dequeue();
+
+                    sb.Append($"{currentVowel}{currentConsonant}");
+
+                    currentQueueV.Enqueue(currentVowel);
+
+                }
+
+                int count = 0;
+
+                for (int j = 0; j < word.Length; j++)
+                {
+                    if (sb.ToString().Contains(word[j]))
+                    {
+                        count++;
+                    }
+                }
+
+                if (count == word.Length)
+                {
+                    wordsFound.Add(word);
+                }
+            }
+
+            Console.WriteLine($"Words found: {wordsFound.Count}");
+            if (wordsFound.Count != 0)
+            {
+                Console.WriteLine($"{string.Join("\n", wordsFound)}");
+            }
         }
     }
 }
