fdd3279b18c67a15a289386dd170a991da747d30
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-23 09:12:20 +0200

    Added 01. Meal Plan

--- 03. C# - Advanced/0.1 Final Exam - Prep/13-Apr-2022/01. Meal Plan/Program.cs
+++ 03. C# - Advanced/0.1 Final Exam - Prep/13-Apr-2022/01. Meal Plan/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _01._Meal_Plan
 {
@@ -6,7 +9,68 @@ namespace _01._Meal_Plan
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Queue<string> meals = new Queue<string>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries));
+            Stack<int> caloriesPerDay = new Stack<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+
+            int mealsEaten = 0;
+
+            while (meals.Any() && caloriesPerDay.Any())
+            {
+                int caloriesPerMeal = 0;
+
+                switch (meals.Peek())
+                {
+                    case "salad":
+                        caloriesPerMeal = 350;
+                        break;
+                    case "soup":
+                        caloriesPerMeal = 490;
+                        break;
+                    case "pasta":
+                        caloriesPerMeal = 680;
+                        break;
+                    case "steak":
+                        caloriesPerMeal = 790;
+                        break;
+                }
+
+                if (caloriesPerDay.Peek() - caloriesPerMeal > 0)
+                {
+                    var totalCaloriesForDay = caloriesPerDay.Pop();
+                    caloriesPerDay.Push(totalCaloriesForDay - caloriesPerMeal);
+                    meals.Dequeue();
+                }
+                else if (caloriesPerDay.Peek() - caloriesPerMeal == 0)
+                {
+                    caloriesPerDay.Pop();
+                    meals.Dequeue();
+                }
+                else
+                {
+                    var caloriesToTransfer = caloriesPerMeal - caloriesPerDay.Pop();
+                    meals.Dequeue();
+                    if (caloriesPerDay.Any())
+                    {
+                        var newCalories = caloriesPerDay.Pop() - caloriesToTransfer;
+                        caloriesPerDay.Push(newCalories);
+                    }
+                }
+
+                mealsEaten++;
+            }
+
+            if (meals.Any())
+            {
+                Console.WriteLine($"John ate enough, he had {mealsEaten} meals.");
+                Console.WriteLine($"Meals left: {string.Join(", ", meals)}.");
+            }
+            else
+            {
+                Console.WriteLine($"John had {mealsEaten} meals.");
+                Console.WriteLine($"For the next few days, he can eat {string.Join(", ", caloriesPerDay)} calories.");
+            }
+
         }
     }
 }
