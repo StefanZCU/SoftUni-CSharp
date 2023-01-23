5cb4115bce8df2b53954c5aab4ff6155a9336e7e
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-23 13:33:06 +0200

    Added 23-Oct-2021

---
+++ 03. C# - Advanced/0.1 Final Exam - Prep/23-Oct-2021/02. Pawn Wars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02._Pawn_Wars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
