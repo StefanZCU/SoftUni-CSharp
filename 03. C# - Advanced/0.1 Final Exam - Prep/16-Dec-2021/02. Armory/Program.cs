c60d8790ec6b9cb0af4b3da8a243ead94fc7777a
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-23 12:58:57 +0200

    Added 02. Armory

--- 03. C# - Advanced/0.1 Final Exam - Prep/16-Dec-2021/02. Armory/Program.cs
+++ 03. C# - Advanced/0.1 Final Exam - Prep/16-Dec-2021/02. Armory/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography;
 
 namespace _02._Armory
 {
@@ -6,7 +7,151 @@ namespace _02._Armory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int sizeMatrix = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[sizeMatrix, sizeMatrix];
+
+            int[] officerPosition = new int[2];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                string line = Console.ReadLine();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (line[j] == 'A')
+                    {
+                        officerPosition[0] = i;
+                        officerPosition[1] = j;
+                        matrix[i, j] = '-';
+                        continue;
+                    }
+
+                    matrix[i, j] = line[j];
+                }
+            }
+
+            int boughtSwordsValue = 0;
+            bool escaped = false;
+            string command;
+            while (boughtSwordsValue <= 65)
+            {
+                command = Console.ReadLine();
+
+                int row = officerPosition[0];
+                int col = officerPosition[1];
+
+                switch (command)
+                {
+                    case "up":
+                        if (IndexChecker(--row, col, matrix))
+                        {
+                            officerPosition[0]--;
+                        }
+                        else
+                        {
+                            escaped = true;
+                        }
+                        break;
+                    case "down":
+                        if (IndexChecker(++row, col, matrix))
+                        {
+                            officerPosition[0]++;
+                        }
+                        else
+                        {
+                            escaped = true;
+                        }
+                        break;
+                    case "right":
+                        if (IndexChecker(row, ++col, matrix))
+                        {
+                            officerPosition[1]++;
+                        }
+                        else
+                        {
+                            escaped = true;
+                        }
+                        break;
+                    case "left":
+                        if (IndexChecker(row, --col, matrix))
+                        {
+                            officerPosition[1]--;
+                        }
+                        else
+                        {
+                            escaped = true;
+                        }
+                        break;
+                }
+
+                row = officerPosition[0];
+                col = officerPosition[1];
+
+                if (escaped)
+                {
+                    break;
+                }
+
+                if (char.IsDigit(matrix[row, col]))
+                {
+                    boughtSwordsValue += int.Parse(matrix[row, col].ToString());
+                    matrix[row, col] = '-';
+                }
+
+                if (matrix[row, col] == 'M')
+                {
+                    bool flag = false;
+                    matrix[row, col] = '-';
+
+                    for (int i = 0; i < matrix.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < matrix.GetLength(1); j++)
+                        {
+                            if (matrix[i, j] == 'M')
+                            {
+                                officerPosition[0] = i;
+                                officerPosition[1] = j;
+                                matrix[i, j] = '-';
+                                flag = true;
+                                break;
+                            }
+                        }
+
+                        if (flag)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+
+            Console.WriteLine(escaped 
+                ? "I do not need more swords!" 
+                : "Very nice swords, I will come back for more!");
+
+            Console.WriteLine($"The king paid {boughtSwordsValue} gold coins.");
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (!escaped && officerPosition[0] == i && officerPosition[1] == j)
+                    {
+                        matrix[i, j] = 'A';
+                    }
+
+                    Console.Write(matrix[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static bool IndexChecker(int row, int col, char[,] matrix)
+        {
+            return row >= 0 && row < matrix.GetLength(0) && 
+                   col >= 0 && col < matrix.GetLength(1);
         }
     }
 }
