26b787a97990b70506c2e6aa9b76c9a42b1d206b
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-23 15:31:51 +0200

    Added 03. Stock Market

--- 03. C# - Advanced/0.1 Final Exam - Prep/23-Oct-2021/03. Stock Market/StockMarket/Investor.cs
+++ 03. C# - Advanced/0.1 Final Exam - Prep/23-Oct-2021/03. Stock Market/StockMarket/Investor.cs
@@ -1,7 +1,75 @@
-﻿namespace StockMarket
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockMarket
 {
     public class Investor
     {
-     
+        public List<Stock> Portfolio { get; set; }
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public decimal MoneyToInvest { get; set; }
+        public string BrokerName { get; set; }
+        public int Count => Portfolio.Count;
+
+        public Investor(string fullName, string emailAddress, decimal moneyToInvest, string brokerName)
+        {
+            Portfolio = new List<Stock>();
+            FullName = fullName;
+            EmailAddress = emailAddress;
+            MoneyToInvest = moneyToInvest;
+            BrokerName = brokerName;
+        }
+
+        public void BuyStock(Stock stock)
+        {
+            if (stock.MarketCapitalization > 10000 && MoneyToInvest >= stock.PricePerShare)
+            {
+                Portfolio.Add(stock);
+                MoneyToInvest -= stock.PricePerShare;
+            }
+        }
+
+        public string SellStock(string companyName, decimal sellPrice)
+        {
+            var stockToSell = Portfolio.FirstOrDefault(x => x.CompanyName == companyName);
+
+            if (stockToSell == null)
+            {
+                return $"{companyName} does not exist.";
+            }
+
+            if (stockToSell.PricePerShare > sellPrice)
+            {
+                return $"Cannot sell {companyName}.";
+            }
+
+            Portfolio.Remove(stockToSell);
+            MoneyToInvest += sellPrice;
+            return $"{companyName} was sold.";
+        }
+
+        public Stock FindStock(string companyName)
+        {
+            return Portfolio.FirstOrDefault(x => x.CompanyName == companyName);
+        }
+
+        public Stock FindBiggestCompany()
+        {
+            return Portfolio.OrderByDescending(x => x.MarketCapitalization).FirstOrDefault();
+        }
+
+        public string InvestorInformation()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The investor {FullName} with a broker {BrokerName} has stocks:");
+
+            foreach (var stock in Portfolio)
+            {
+                sb.AppendLine(stock.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
     }
 }
