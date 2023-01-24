d0ef8520451df87777fa03c606dd1a42d0ae45df
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-24 15:31:21 +0200

    Added 01. Lootbox

--- 03. C# - Advanced/0.1 Final Exam - Prep/22-Feb-2020/01. Lootbox/Program.cs
+++ 03. C# - Advanced/0.1 Final Exam - Prep/22-Feb-2020/01. Lootbox/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _01._Lootbox
 {
@@ -6,7 +8,32 @@ namespace _01._Lootbox
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Queue<int> firstBox = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray()); 
+            Stack<int> secondBox = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray());
+
+            int claimedItems = 0;
+
+            while (firstBox.Any() && secondBox.Any())
+            {
+                if ((firstBox.Peek() + secondBox.Peek()) % 2 == 0)
+                {
+                    claimedItems += (firstBox.Dequeue() + secondBox.Pop());
+                }
+                else
+                {
+                    firstBox.Enqueue(secondBox.Pop());
+                }
+            }
+
+            Console.WriteLine(firstBox.Any() ? "Second lootbox is empty" : "First lootbox is empty");
+
+            Console.WriteLine(claimedItems >= 100
+                ? $"Your loot was epic! Value: {claimedItems}"
+                : $"Your loot was poor... Value: {claimedItems}");
+
+
         }
     }
 }
