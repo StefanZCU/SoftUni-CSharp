621f9f9d2e1bd4de3ca517f484242a5f8cd6a252
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-24 12:12:28 +0200

    Added 01. Masterchef

--- 03. C# - Advanced/0.1 Final Exam - Prep/26-Jun-2021/01. Masterchef/Program.cs
+++ 03. C# - Advanced/0.1 Final Exam - Prep/26-Jun-2021/01. Masterchef/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _01._Masterchef
 {
@@ -6,7 +8,69 @@ namespace _01._Masterchef
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Queue<int> ingredients = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray());
+            Stack<int> freshness = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray());
+
+            Dictionary<string, int> cookedDishes = new Dictionary<string, int>
+            {
+                { "Dipping sauce", 0 },
+                { "Green salad", 0 },
+                { "Chocolate cake", 0 },
+                { "Lobster", 0 }
+            };
+
+            while (ingredients.Any() && freshness.Any())
+            {
+                if (ingredients.Peek() == 0)
+                {
+                    ingredients.Dequeue();
+                    continue;
+                }
+
+                var freshnessLevel = ingredients.Peek() * freshness.Peek();
+
+                switch (freshnessLevel)
+                {
+                    case 150:
+                        cookedDishes["Dipping sauce"]++;
+                        ingredients.Dequeue();
+                        break;
+                    case 250:
+                        cookedDishes["Green salad"]++;
+                        ingredients.Dequeue();
+                        break;
+                    case 300:
+                        cookedDishes["Chocolate cake"]++;
+                        ingredients.Dequeue();
+                        break;
+                    case 400:
+                        cookedDishes["Lobster"]++;
+                        ingredients.Dequeue();
+                        break;
+                    default:
+                        ingredients.Enqueue(ingredients.Dequeue() + 5);
+                        break;
+                }
+
+                freshness.Pop();
+            }
+
+            Console.WriteLine(cookedDishes.All(x => x.Value >= 1)
+                ? "Applause! The judges are fascinated by your dishes!"
+                : "You were voted off. Better luck next year.");
+
+            if (ingredients.Any())
+            {
+                Console.WriteLine($"Ingredients left: {ingredients.Sum()}");
+            }
+
+            foreach (var cookedDish in cookedDishes.Where(x => x.Value >= 1).OrderBy(x => x.Key))
+            {
+                Console.WriteLine($" # {cookedDish.Key} --> {cookedDish.Value}");
+            }
+
         }
     }
 }
