10984c6913b5076d2bd8f4d9c20d6137ddeaba84
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-24 11:53:02 +0200

    Added 03. Street Racing

---
+++ 03. C# - Advanced/0.1 Final Exam - Prep/18-Aug-2021/03. Street Racing/StreetRacing/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StreetRacing
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string LicensePlate { get; set; }
+        public int HorsePower { get; set; }
+        public double Weight { get; set; }
+
+        public Car(string make, string model, string licensePlate, int horsePower, double weight)
+        {
+            Make = make;
+            Model = model;
+            LicensePlate = licensePlate;
+            HorsePower = horsePower;
+            Weight = weight;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Make: {Make}");
+            sb.AppendLine($"Model: {Model}");
+            sb.AppendLine($"License Plate: {LicensePlate}");
+            sb.AppendLine($"Horse Power: {HorsePower}");
+            sb.AppendLine($"Weight: {Weight}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 03. C# - Advanced/0.1 Final Exam - Prep/18-Aug-2021/03. Street Racing/StreetRacing/Race.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StreetRacing
+{
+    public class Race
+    {
+        public List<Car> Participants { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Laps { get; set; }
+        public int Capacity { get; set; }
+        public int MaxHorsePower { get; set; }
+        public int Count => this.Participants.Count;
+
+        public Race(string name, string type, int laps, int capacity, int maxHorsePower)
+        {
+            Participants = new List<Car>();
+            Name = name;
+            Type = type;
+            Laps = laps;
+            Capacity = capacity;
+            MaxHorsePower = maxHorsePower;
+        }
+
+        public void Add(Car car)
+        {
+            if (Participants.FirstOrDefault(x => x.LicensePlate == car.LicensePlate) != null)
+            {
+                return;
+            }
+
+            if (car.HorsePower > MaxHorsePower)
+            {
+                return;
+            }
+
+            if (Capacity == Participants.Count)
+            {
+                return;
+            }
+
+            Participants.Add(car);
+        }
+
+        public bool Remove(string licensePlate)
+        {
+            var carToFind = Participants.FirstOrDefault(x => x.LicensePlate == licensePlate);
+
+            if (carToFind == null)
+            {
+                return false;
+            }
+
+            Participants.Remove(carToFind);
+            return true;
+        }
+
+        public Car FindParticipant(string licensePlate)
+        {
+            var carToFind = Participants.FirstOrDefault(x => x.LicensePlate == licensePlate);
+            return carToFind;
+        }
+
+        public Car GetMostPowerfulCar()
+        {
+            var carToFind = Participants.OrderByDescending(x => x.HorsePower).FirstOrDefault();
+            return carToFind;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Race: {Name} - Type: {Type} (Laps: {Laps})");
+
+            foreach (var participant in Participants)
+            {
+                sb.AppendLine(participant.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
