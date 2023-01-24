bee185fe8709ec19320ad29b9a87fcd512e0411f
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-24 12:42:24 +0200

    Added 02. Survivor

--- 03. C# - Advanced/0.1 Final Exam - Prep/26-Jun-2021/02. Survivor/Program.cs
+++ 03. C# - Advanced/0.1 Final Exam - Prep/26-Jun-2021/02. Survivor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _02._Survivor
 {
@@ -6,7 +7,99 @@ namespace _02._Survivor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int matrixRowLength = int.Parse(Console.ReadLine());
+
+            char[][] matrix = new char[matrixRowLength][];
+
+            int collectedTokens = 0;
+            int opponentTokens = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                char[] line = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(char.Parse)
+                    .ToArray();
+                matrix[i] = new char[line.Length];
+
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    matrix[i][j] = line[j];
+                }
+            }
+
+            string command;
+            while ((command = Console.ReadLine()) != "Gong")
+            {
+                string[] input = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                int row = int.Parse(input[1]);
+                int col = int.Parse(input[2]);
+
+                if (IndexChecker(row, col, matrix))
+                {
+                    if (input[0] == "Find")
+                    {
+                        if (matrix[row][col] == 'T')
+                        {
+                            collectedTokens++;
+                            matrix[row][col] = '-';
+                        }
+                    }
+                    else if (input[0] == "Opponent")
+                    {
+                        string direction = input[3];
+                        
+                        for (int i = 0; i < 4; i++)
+                        {
+                            if (IndexChecker(row, col, matrix))
+                            {
+                                if (matrix[row][col] == 'T')
+                                {
+                                    opponentTokens++;
+                                    matrix[row][col] = '-';
+                                }
+                            }
+                            else
+                            {
+                                break;
+                            }
+
+                            switch (direction)
+                            {
+                                case "up":
+                                    --row;
+                                    break;
+                                case "down":
+                                    ++row;
+                                    break;
+                                case "left":
+                                    --col;
+                                    break;
+                                case "right":
+                                    ++col;
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    Console.Write($"{matrix[i][j]} ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Collected tokens: {collectedTokens}");
+            Console.WriteLine($"Opponent's tokens: {opponentTokens}");
+        }
+
+        static bool IndexChecker(int row, int col, char[][] matrix)
+        {
+            return row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix[row].Length;
         }
     }
 }
