4c9b7c3dba2e35bed96e6d7353ecd6d85991ba5b
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-24 11:06:52 +0200

    Added 01. Birthday Celebration

--- 03. C# - Advanced/0.1 Final Exam - Prep/18-Aug-2021/01. Birthday Celebration/Program.cs
+++ 03. C# - Advanced/0.1 Final Exam - Prep/18-Aug-2021/01. Birthday Celebration/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _01._Birthday_Celebration
 {
@@ -6,7 +8,44 @@ namespace _01._Birthday_Celebration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Queue<int> guests = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+            Stack<int> plates = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+
+            int wastedFood = 0;
+
+            while (guests.Any() && plates.Any())
+            {
+                if (plates.Peek() > guests.Peek())
+                {
+                    var foodToThrow = plates.Peek() - guests.Peek();
+                    guests.Dequeue();
+                    wastedFood += foodToThrow;
+                }
+                else if (plates.Peek() == guests.Peek())
+                {
+                    guests.Dequeue();
+                }
+                else
+                {
+                    var guestsList = guests.ToList();
+                    guests.Clear();
+                    guestsList[0] -= plates.Peek();
+
+                    foreach (var guest in guestsList)
+                    {
+                        guests.Enqueue(guest);
+                    }
+                }
+
+                plates.Pop();
+
+            }
+
+            Console.WriteLine(plates.Any()
+                ? $"Plates: {string.Join(" ", plates)}"
+                : $"Guests: {string.Join(" ", guests)}");
+
+            Console.WriteLine($"Wasted grams of food: {wastedFood}");
         }
     }
 }
