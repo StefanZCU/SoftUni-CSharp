0760b6700b9dde885bc1ff34ae93dba43fcd90ae
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-24 10:53:50 +0200

    Added 18-Aug-2021

---
+++ 03. C# - Advanced/0.1 Final Exam - Prep/18-Aug-2021/01. Birthday Celebration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Birthday_Celebration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
