0b84cc0d137eb97819973a9c5017a150d38a823a
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-25 10:10:59 +0200

    Added 03. Cocktail Party

---
+++ 03. C# - Advanced/0.1 Final Exam - Prep/14-Apr-2021/03. Cocktail Party/Cocktail.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CocktailParty
+{
+    public class Cocktail
+    {
+        public List<Ingredient> Ingredients { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int MaxAlcoholLevel { get; set; }
+
+        public int CurrentAlcoholLevel => this.Ingredients.Sum(x => x.Alcohol);
+
+        public Cocktail(string name, int capacity, int maxAlcoholLevel)
+        {
+            Ingredients = new List<Ingredient>();
+            Name = name;
+            Capacity = capacity;
+            MaxAlcoholLevel = maxAlcoholLevel;
+        }
+
+        public void Add(Ingredient ingredient)
+        {
+            if (Ingredients.FirstOrDefault(x => x.Name == ingredient.Name) != null)
+            {
+                return;
+            }
+            if (Ingredients.Count >= Capacity)
+            {
+                return;
+            }
+
+            if (MaxAlcoholLevel - ingredient.Alcohol < 0)
+            {
+                return;
+            }
+
+            Ingredients.Add(ingredient);
+        }
+
+        public bool Remove(string name)
+        {
+            var ingredientToRemove = Ingredients.FirstOrDefault(x => x.Name == name);
+            if (ingredientToRemove != null)
+            {
+                Ingredients.Remove(ingredientToRemove);
+                return true;
+            }
+            return false;
+        }
+
+        public Ingredient FindIngredient(string name)
+        {
+            var ingredientToFind = Ingredients.FirstOrDefault(x => x.Name == name);
+            return ingredientToFind;
+        }
+
+        public Ingredient GetMostAlcoholicIngredient()
+        {
+            return Ingredients.OrderByDescending(x => x.Alcohol).FirstOrDefault();
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Cocktail: {Name} - Current Alcohol Level: {CurrentAlcoholLevel}");
+
+            foreach (var ingredient in Ingredients)
+            {
+                sb.AppendLine(ingredient.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 03. C# - Advanced/0.1 Final Exam - Prep/14-Apr-2021/03. Cocktail Party/Ingredient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CocktailParty
+{
+    public class Ingredient
+    {
+        public string Name { get; set; }
+        public int Alcohol { get; set; }
+        public int Quantity { get; set; }
+
+        public Ingredient(string name, int alcohol, int quantity)
+        {
+            Name = name;
+            Alcohol = alcohol;
+            Quantity = quantity;    
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Ingredient: {Name}");
+            sb.AppendLine($"Quantity: {Quantity}");
+            sb.AppendLine($"Alcohol: {Alcohol}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
