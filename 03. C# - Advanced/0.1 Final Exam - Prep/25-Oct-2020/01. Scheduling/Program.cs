81d749094091623181bf5fb794756c4eb5e83013
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-25 12:04:08 +0200

    Added 01. Scheduling

--- 03. C# - Advanced/0.1 Final Exam - Prep/25-Oct-2020/01. Scheduling/Program.cs
+++ 03. C# - Advanced/0.1 Final Exam - Prep/25-Oct-2020/01. Scheduling/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _01._Scheduling
 {
@@ -6,7 +8,36 @@ namespace _01._Scheduling
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack<int> tasks = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray());
+            Queue<int> threads = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray());
+
+            int taskToKill = int.Parse(Console.ReadLine());
+
+            int winningThread = 0;
+
+            while (true)
+            {
+                if (tasks.Peek() == taskToKill)
+                {
+                    winningThread = threads.Peek();
+                    break;
+                }
+
+                if (threads.Peek() >= tasks.Peek())
+                {
+                    threads.Dequeue();
+                    tasks.Pop();
+                }
+                else if (threads.Peek() < tasks.Peek())
+                {
+                    threads.Dequeue();
+                }
+            }
+
+            Console.WriteLine($"Thread with value {winningThread} killed task {taskToKill}");
+            Console.WriteLine(string.Join(" ", threads));
         }
     }
 }
