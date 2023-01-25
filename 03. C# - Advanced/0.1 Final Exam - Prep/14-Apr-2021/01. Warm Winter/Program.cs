7c10a9b45545482394565f2387fd8cf989940164
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-25 09:20:51 +0200

    Added 01. Warm Winter

--- 03. C# - Advanced/0.1 Final Exam - Prep/14-Apr-2021/01. Warm Winter/Program.cs
+++ 03. C# - Advanced/0.1 Final Exam - Prep/14-Apr-2021/01. Warm Winter/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _01._Warm_Winter
 {
@@ -6,7 +8,33 @@ namespace _01._Warm_Winter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack<int> hats = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray());
+            Queue<int> scarfs = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray());
+
+            List<int> sets = new List<int>();
+
+            while (hats.Any() && scarfs.Any())
+            {
+                if (hats.Peek() > scarfs.Peek())
+                {
+                    sets.Add(hats.Pop() + scarfs.Dequeue());
+                }
+                else if (hats.Peek() < scarfs.Peek())
+                {
+                    hats.Pop();
+                }
+                else if (hats.Peek() == scarfs.Peek())
+                {
+                    scarfs.Dequeue();
+                    var newHat = hats.Pop() + 1;
+                    hats.Push(newHat);
+                }
+            }
+
+            Console.WriteLine($"The most expensive set is: {sets.Max()}");
+            Console.WriteLine(string.Join(" ", sets));
         }
     }
 }
