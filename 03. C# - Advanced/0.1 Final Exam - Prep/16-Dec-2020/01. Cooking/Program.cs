72a81723fb235e8aa01cbe9cb5a5ffe9767fb44a
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-25 10:37:13 +0200

    Added 01. Cooking

--- 03. C# - Advanced/0.1 Final Exam - Prep/16-Dec-2020/01. Cooking/Program.cs
+++ 03. C# - Advanced/0.1 Final Exam - Prep/16-Dec-2020/01. Cooking/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _01._Cooking
 {
@@ -6,7 +8,65 @@ namespace _01._Cooking
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Queue<int> liquids = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray());
+            Stack<int> ingredients = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray());
+
+            Dictionary<string, int> cookedFood = new Dictionary<string, int>
+            {
+                { "Bread", 0 },
+                { "Cake", 0 },
+                { "Pastry", 0 },
+                { "Fruit Pie", 0 }
+            };
+
+            while (liquids.Any() && ingredients.Any())
+            {
+                var foodValue = liquids.Peek() + ingredients.Peek();
+
+                switch (foodValue)
+                {
+                    case 25:
+                        cookedFood["Bread"]++;
+                        ingredients.Pop();
+                        break;
+                    case 50:
+                        cookedFood["Cake"]++;
+                        ingredients.Pop();
+                        break;
+                    case 75:
+                        cookedFood["Pastry"]++;
+                        ingredients.Pop();
+                        break;
+                    case 100:
+                        cookedFood["Fruit Pie"]++;
+                        ingredients.Pop();
+                        break;
+                    default:
+                        ingredients.Push(ingredients.Pop() + 3);
+                        break;
+                }
+
+                liquids.Dequeue();
+            }
+
+            Console.WriteLine(cookedFood.All(x => x.Value >= 1)
+                ? "Wohoo! You succeeded in cooking all the food!"
+                : "Ugh, what a pity! You didn't have enough materials to cook everything.");
+
+            Console.WriteLine(liquids.Any() 
+                ? $"Liquids left: {string.Join(", ", liquids)}" 
+                : "Liquids left: none");
+
+            Console.WriteLine(ingredients.Any()
+                ? $"Ingredients left: {string.Join(", ", ingredients)}"
+                : "Ingredients left: none");
+
+            foreach (var food in cookedFood.OrderBy( x=> x.Key))
+            {
+                Console.WriteLine($"{food.Key}: {food.Value}");
+            }
         }
     }
 }
