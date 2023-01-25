3ba2377210651ac52c24297d497f2bbbc3439d22
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-25 14:07:37 +0200

    Added 03. Parking

---
+++ 03. C# - Advanced/0.1 Final Exam - Prep/28-Jun-2020/03. Parking/Parking/Parking.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Parking
+{
+    public class Parking
+    {
+        private List<Car> data;
+
+        public string Type { get; set; }
+        public int Capacity { get; set; }
+        public int Count => data.Count;
+
+        public Parking(string type, int capacity)
+        {
+            Type = type;
+            Capacity = capacity;
+            data = new List<Car>();
+        }
+
+        public void Add(Car car)
+        {
+            if (data.Count != Capacity)
+            {
+                data.Add(car);
+            }
+        }
+
+        public bool Remove(string manufacturer, string model)
+        {
+            Car carToRemove = null;
+
+            foreach (var car in data)
+            {
+                if (car.Manufacturer == manufacturer && car.Model == model)
+                {
+                    carToRemove = car;
+                    break;
+                }
+            }
+
+            if (carToRemove != null)
+            {
+                data.Remove(carToRemove);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Car GetLatestCar()
+        {
+            return data.OrderByDescending(x => x.Year).FirstOrDefault();
+        }
+
+        public Car GetCar(string manufacturer, string model)
+        {
+            Car carToGet = null;
+
+            foreach (var car in data)
+            {
+                if (car.Manufacturer == manufacturer && car.Model == model)
+                {
+                    carToGet = car;
+                    break;
+                }
+            }
+
+            return carToGet;
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The cars are parked in {Type}:");
+
+            foreach (var car in data)
+            {
+                sb.AppendLine(car.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
