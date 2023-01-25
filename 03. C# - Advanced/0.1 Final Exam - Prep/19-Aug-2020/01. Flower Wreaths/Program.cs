8017b66d3d25443350e62e1e8dfcdfc19a248c58
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-25 12:36:18 +0200

    Added 19-Aug-2020

---
+++ 03. C# - Advanced/0.1 Final Exam - Prep/19-Aug-2020/01. Flower Wreaths/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Flower_Wreaths
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
