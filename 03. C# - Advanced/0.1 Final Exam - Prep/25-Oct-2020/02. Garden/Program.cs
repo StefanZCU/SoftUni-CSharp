cc748376af87c042be4258416137c6265f477f08
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-25 12:20:47 +0200

    Added 02. Garden

--- 03. C# - Advanced/0.1 Final Exam - Prep/25-Oct-2020/02. Garden/Program.cs
+++ 03. C# - Advanced/0.1 Final Exam - Prep/25-Oct-2020/02. Garden/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace _02._Garden
 {
@@ -6,7 +9,95 @@ namespace _02._Garden
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] sizeMatrix = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[,] matrix = new int[sizeMatrix[0], sizeMatrix[1]];
+
+            List<string> plantedFlowersCoords = new List<string>();
+
+            StringBuilder sb = new StringBuilder();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Bloom Bloom Plow")
+            {
+                int[] coords = command.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                if (IndexChecker(coords[0], coords[1], matrix))
+                {
+                    matrix[coords[0], coords[1]] = 1;
+                    sb.Append($"{coords[0]} {coords[1]}");
+                    plantedFlowersCoords.Add(sb.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("Invalid coordinates.");
+                }
+
+                sb.Clear();
+            }
+
+
+            foreach (var flowerCoord in plantedFlowersCoords)
+            {
+                int[] coords = flowerCoord.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse).ToArray();
+
+                int row = coords[0];
+                int col = coords[1];
+
+                row--;
+                while (row >= 0)
+                {
+                    matrix[row, col] += 1;
+                    --row;
+                }
+
+                row = coords[0];
+                col = coords[1];
+
+                row++;
+                while (row <= matrix.GetLength(0) - 1)
+                {
+                    matrix[row, col] += 1;
+                    ++row;
+                }
+
+                row = coords[0];
+                col = coords[1];
+
+                col--;
+                while (col >= 0)
+                {
+                    matrix[row, col] += 1;
+                    --col;
+                }
+
+                row = coords[0];
+                col = coords[1];
+
+                col++;
+                while (col <= matrix.GetLength(1) - 1)
+                {
+                    matrix[row, col] += 1;
+                    ++col;
+                }
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j]} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static bool IndexChecker(int row, int col, int[,] matrix)
+        {
+            return row >= 0 && row < matrix.GetLength(0) && 
+                   col >= 0 && col < matrix.GetLength(1);
         }
     }
 }
