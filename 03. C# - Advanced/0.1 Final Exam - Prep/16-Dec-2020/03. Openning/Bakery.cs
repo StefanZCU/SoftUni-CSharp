b81cc77730f348ee01314adf8c2a1ba1ade8c709
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-25 11:51:10 +0200

    Added 03. Openning

---
+++ 03. C# - Advanced/0.1 Final Exam - Prep/16-Dec-2020/03. Openning/Bakery.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Bakery
+    {
+        private List<Employee> data;
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count => data.Count;
+
+        public Bakery(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            data = new List<Employee>();
+        }
+
+        public void Add(Employee employee)
+        {
+            if (data.Count != Capacity)
+            {
+                data.Add(employee);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            var personToRemove = data.FirstOrDefault(x => x.Name == name);
+            if (personToRemove != null)
+            {
+                data.Remove(personToRemove);
+                return true;
+            }
+            return false;
+        }
+
+        public Employee GetOldestEmployee()
+        {
+            return data.OrderByDescending(x => x.Age).FirstOrDefault();
+        }
+
+        public Employee GetEmployee(string name)
+        {
+            return data.FirstOrDefault(x => x.Name == name);
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Employees working at Bakery {Name}:");
+
+            foreach (var employee in data)
+            {
+                sb.AppendLine(employee.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 03. C# - Advanced/0.1 Final Exam - Prep/16-Dec-2020/03. Openning/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BakeryOpenning
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+
+        public Employee(string name, int age, string country)
+        {
+            Name = name;
+            Age = age;
+            Country = country;
+        }
+
+        public override string ToString()
+        {
+            return $"Employee: {Name}, {Age}, ({Country})";
+        }
+    }
+}
