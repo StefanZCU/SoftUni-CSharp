602589b411bd2d63c09ae76e7ab986b4cbddd764
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-25 12:35:05 +0200

    Added 03. Classroom

---
+++ 03. C# - Advanced/0.1 Final Exam - Prep/25-Oct-2020/03. Classroom/Classroom.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassroomProject
+{
+    public class Classroom
+    {
+        private List<Student> students;
+
+        public int Capacity { get; set; }
+        public int Count => students.Count;
+
+        public Classroom(int capacity)
+        {
+            Capacity = capacity;
+            students = new List<Student>();
+        }
+
+        public string RegisterStudent(Student student)
+        {
+            if (students.Count == Capacity)
+            {
+                return "No seats in the classroom";
+            }
+            
+            students.Add(student);
+            return $"Added student {student.FirstName} {student.LastName}";
+        }
+
+        public string DismissStudent(string firstName, string lastName)
+        {
+            Student studentToDismiss = null;
+
+            foreach (var student in students)
+            {
+                if (student.FirstName == firstName && student.LastName == lastName)
+                {
+                    studentToDismiss = student;
+                    break;
+                }
+            }
+
+            if (studentToDismiss != null)
+            {
+                students.Remove(studentToDismiss);
+                return $"Dismissed student {firstName} {lastName}";
+            }
+
+            return "Student not found";
+        }
+
+        public string GetSubjectInfo(string subject)
+        {
+            if (students.Any(x => x.Subject == subject))
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine($"Subject: {subject}");
+                sb.AppendLine($"Students:");
+
+                foreach (var student in students.Where(x => x.Subject == subject))
+                {
+                    sb.AppendLine($"{student.FirstName} {student.LastName}");
+                }
+
+                return sb.ToString().TrimEnd();
+            }
+
+            return "No students enrolled for the subject";
+        }
+
+        public int GetStudentsCount()
+        {
+            return students.Count;
+        }
+
+        public Student GetStudent(string firstName, string lastName)
+        {
+            Student studentToGet = null;
+
+            foreach (var student in students)
+            {
+                if (student.FirstName == firstName && student.LastName == lastName)
+                {
+                    studentToGet = student;
+                    return studentToGet;
+                }
+            }
+
+            return null;
+
+        }
+    }
+}
