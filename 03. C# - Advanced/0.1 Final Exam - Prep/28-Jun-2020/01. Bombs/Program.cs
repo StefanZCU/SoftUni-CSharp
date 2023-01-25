819857d85cc34cb09760127b1a9b49c9cba14eda
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-25 13:45:07 +0200

    Added 01. Bombs

--- 03. C# - Advanced/0.1 Final Exam - Prep/28-Jun-2020/01. Bombs/Program.cs
+++ 03. C# - Advanced/0.1 Final Exam - Prep/28-Jun-2020/01. Bombs/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _01._Bombs
 {
@@ -6,7 +8,67 @@ namespace _01._Bombs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Queue<int> bombEffect = new Queue<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray()); 
+            Stack<int> bombCasings = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray());
+
+            Dictionary<string, int> bombPouch = new Dictionary<string, int>
+            {
+                { "Datura Bombs", 0 },
+                { "Cherry Bombs", 0 },
+                { "Smoke Decoy Bombs", 0 }
+            };
+            bool filledPouch = false;
+            while (bombEffect.Any() && bombCasings.Any())
+            {
+                var value = bombEffect.Peek() + bombCasings.Peek();
+
+                switch (value)
+                {
+                    case 40:
+                        bombPouch["Datura Bombs"]++;
+                        bombEffect.Dequeue();
+                        bombCasings.Pop();
+                        break;
+                    case 60:
+                        bombPouch["Cherry Bombs"]++;
+                        bombEffect.Dequeue();
+                        bombCasings.Pop();
+                        break;
+                    case 120:
+                        bombPouch["Smoke Decoy Bombs"]++;
+                        bombEffect.Dequeue();
+                        bombCasings.Pop();
+                        break;
+                    default:
+                        bombCasings.Push(bombCasings.Pop() - 5);
+                        break;
+                }
+
+                if (bombPouch.All(x => x.Value >= 3))
+                {
+                    filledPouch = true;
+                    break;
+                }
+            }
+
+            Console.WriteLine(filledPouch 
+                ? "Bene! You have successfully filled the bomb pouch!" 
+                :"You don't have enough materials to fill the bomb pouch.");
+
+            Console.WriteLine(bombEffect.Any()
+                ? $"Bomb Effects: {string.Join(", ", bombEffect)}"
+                : "Bomb Effects: empty");
+
+            Console.WriteLine(bombCasings.Any()
+                ? $"Bomb Casings: {string.Join(", ", bombCasings)}"
+                : "Bomb Casings: empty");
+
+            foreach (var bomb in bombPouch.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{bomb.Key}: {bomb.Value}");
+            }
         }
     }
 }
