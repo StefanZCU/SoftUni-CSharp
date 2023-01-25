9a71e68901591e36b024086d419de6526631b197
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-25 12:46:23 +0200

    Added 01. Flower Wreaths

--- 03. C# - Advanced/0.1 Final Exam - Prep/19-Aug-2020/01. Flower Wreaths/Program.cs
+++ 03. C# - Advanced/0.1 Final Exam - Prep/19-Aug-2020/01. Flower Wreaths/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _01._Flower_Wreaths
 {
@@ -6,7 +8,45 @@ namespace _01._Flower_Wreaths
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack<int> lilies = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray());
+            Queue<int> roses = new Queue<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray());
+
+            int leftOverFlowers = 0;
+            int wreathesMade = 0;
+
+            while (lilies.Any() && roses.Any())
+            {
+                if (lilies.Peek() + roses.Peek() == 15)
+                {
+                    wreathesMade++;
+                    lilies.Pop();
+                    roses.Dequeue();
+                    continue;
+                }
+
+                if (lilies.Peek() + roses.Peek() > 15)
+                {
+                    lilies.Push(lilies.Pop() - 2);
+                    continue;
+                }
+
+                if (lilies.Peek() + roses.Peek() < 15)
+                {
+                    leftOverFlowers += (lilies.Pop() + roses.Dequeue());
+                }
+            }
+
+            while (leftOverFlowers > 15)
+            {
+                leftOverFlowers -= 15;
+                wreathesMade++;
+            }
+
+            Console.WriteLine(wreathesMade >= 5 
+                ? $"You made it, you are going to the competition with {wreathesMade} wreaths!" 
+                : $"You didn't make it, you need {5 - wreathesMade} wreaths more!");
         }
     }
 }
