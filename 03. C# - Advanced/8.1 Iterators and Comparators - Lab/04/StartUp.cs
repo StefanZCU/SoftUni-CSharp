57ec114d60cfe9c7c642e86b63949dba5a5b34e6
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-27 12:11:13 +0200

    Added 8.1 Iterators and Comparators - Lab

---
+++ 03. C# - Advanced/8.1 Iterators and Comparators - Lab/04/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _04_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
