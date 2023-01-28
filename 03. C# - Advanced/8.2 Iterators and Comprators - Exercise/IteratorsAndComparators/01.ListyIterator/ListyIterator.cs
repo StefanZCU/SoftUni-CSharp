4a9345ab9d64449a27251072a277b77d2ae472a4
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-28 14:59:19 +0200

    Added 01. Listy Iterator

---
+++ 03. C# - Advanced/8.2 Iterators and Comprators - Exercise/IteratorsAndComparators/01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private int index;
+        private List<T> items;
+
+        public ListyIterator(List<T> items)
+        {
+            this.items = items;
+        }
+
+        public bool Move()
+        {
+            if (index < items.Count - 1)
+            {
+                index++;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return index < items.Count - 1;
+        }
+
+        public void Print()
+        {
+            if (items.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine(items[index]);
+        }
+    }
+}
