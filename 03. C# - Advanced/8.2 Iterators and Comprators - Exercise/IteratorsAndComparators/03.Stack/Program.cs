a5ed47d300de0f151eae4debe6987d964ec5b01f
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-28 15:26:30 +0200

    Added 03. Stack

---
+++ 03. C# - Advanced/8.2 Iterators and Comprators - Exercise/IteratorsAndComparators/03.Stack/Program.cs
@@ -0,0 +1,45 @@
+﻿using _03.Stack;
+
+var stack = new _03.Stack.Stack<string>();
+
+string command;
+while ((command = Console.ReadLine()) != "END")
+{
+    string[] tokens = command.Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+    string action = tokens[0];
+
+    switch (action)
+    {
+        case "Push":
+
+            string[] itemsToPush = tokens.Skip(1).ToArray();
+
+            foreach (var item in itemsToPush)
+            {
+                stack.Push(item);
+            }
+
+            break;
+        case "Pop":
+            try
+            {
+                stack.Pop();
+            }
+            catch (InvalidOperationException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            break;
+    }
+}
+
+foreach (var item in stack)
+{
+    Console.WriteLine(item);
+}
+
+foreach (var item in stack)
+{
+    Console.WriteLine(item);
+}
---
+++ 03. C# - Advanced/8.2 Iterators and Comprators - Exercise/IteratorsAndComparators/03.Stack/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Stack
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private List<T> elements;
+
+        public Stack()
+        {
+            elements = new List<T>();
+        }
+
+        public int Count { get; private set; }
+
+        public void Push(T item)
+        {
+            elements.Add(item);
+        }
+
+        public T Pop()
+        {
+            if (elements.Count == 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+
+            T removedElement = elements[^1];
+            elements.Remove(elements[^1]);
+
+            return removedElement;
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = elements.Count - 1; i >= 0; i--)
+            {
+                yield return elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
