e78e9dc1829acdad941e53648ea2835838e359dc
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-28 10:20:52 +0200

    Added Classes

---
+++ 03. C# - Advanced/8.1 Iterators and Comparators - Lab/IteratorsAndComparators/IteratorsAndComparators/Library.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
