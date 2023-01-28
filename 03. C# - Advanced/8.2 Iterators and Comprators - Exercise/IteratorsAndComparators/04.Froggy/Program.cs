8875d3237629b8d83e9373369abb92680ef5547c
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-28 15:34:40 +0200

    Added 04. Froggy

---
+++ 03. C# - Advanced/8.2 Iterators and Comprators - Exercise/IteratorsAndComparators/04.Froggy/Program.cs
@@ -0,0 +1,10 @@
+﻿using _04.Froggy;
+
+List<int> stones = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+Lake lake = new Lake(stones);
+
+Console.WriteLine(string.Join(", ", lake));
