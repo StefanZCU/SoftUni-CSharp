c4ba77da8bfd0d41b26b7d6bcc3f716be7cb4476
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-29 10:47:34 +0200

    Added 05. Comparing Objects

---
+++ 03. C# - Advanced/8.2 Iterators and Comprators - Exercise/IteratorsAndComparators/05.ComparingObjects/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.ComparingObjects
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Town { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            int result = Name.CompareTo(other.Name);
+
+            if (result != 0)
+            {
+                return result;
+            }
+
+            result = Age.CompareTo(other.Age);
+
+            if (result != 0)
+            {
+                return result;
+            }
+
+            return Town.CompareTo(other.Town);
+        }
+    }
+}
--- 03. C# - Advanced/8.2 Iterators and Comprators - Exercise/IteratorsAndComparators/05.ComparingObjects/Program.cs
+++ 03. C# - Advanced/8.2 Iterators and Comprators - Exercise/IteratorsAndComparators/05.ComparingObjects/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _05.ComparingObjects;
+
+List<Person> people = new List<Person>();
+
+string command;
+while ((command = Console.ReadLine()) != "END")
+{
+    string[] personProps = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    Person person = new Person
+    {
+        Name = personProps[0],
+        Age = int.Parse(personProps[1]),
+        Town = personProps[2]
+    };
+
+    people.Add(person);
+}
+
+int compareIndex = int.Parse(Console.ReadLine()) - 1;
+
+Person personToCompare = people[compareIndex];
+
+int equalCount = 0;
+int diffCount = 0;
+
+foreach (var person in people)
+{
+    if (person.CompareTo(personToCompare) == 0)
+    {
+        equalCount++;
+    }
+    else
+    {
+        diffCount++;
+    }
+}
+
+if (equalCount == 1)
+{
+    Console.WriteLine("No matches");
+}
+else
+{
+    Console.WriteLine($"{equalCount} {diffCount} {people.Count}");
+}
