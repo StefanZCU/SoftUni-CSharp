12bc95b079ae64f7000637f6126cccda22d55818
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-29 12:19:40 +0200

    Added 02. Multiple Inheritance

---
+++ 04. C# - OOP/1.1 Inheritance - Lab/02_Multiple_Inheritance/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace Farm
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Puppy puppy = new Puppy();
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+        }
+    }
+}
