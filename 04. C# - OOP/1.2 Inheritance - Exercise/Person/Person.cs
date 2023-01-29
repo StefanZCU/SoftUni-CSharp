91a63e211667cd8478e781a2ad0b5f8f82acf8c4
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-29 13:00:10 +0200

    Added 01. Person

---
+++ 04. C# - OOP/1.2 Inheritance - Exercise/Person/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+
+            age = age switch
+            {
+                > 15 => 15,
+                < 0 => 0,
+                _ => age
+            };
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append($"Name: {this.Name}, Age: {this.Age}");
+            return stringBuilder.ToString().TrimEnd();
+        }
+
+    }
+}
