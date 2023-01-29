78a81ae4f5e1dfa55fa8f9326f8a5a2ab69a5159
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-29 13:19:43 +0200

    Added 04. Need for Speed

---
+++ 04. C# - OOP/1.2 Inheritance - Exercise/NeedForSpeed/SportCar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedForSpeed
+{
+    public class SportCar : Car
+    {
+        public SportCar(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+        }
+
+        public override double FuelConsumption => 10;
+
+        public override void Drive(double kilometers)
+        {
+            Fuel -= kilometers * FuelConsumption;
+        }
+    }
+}
