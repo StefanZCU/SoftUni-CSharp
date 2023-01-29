460fff80f4357ed9c6f961a3dfe76932c09a022e
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-29 13:48:22 +0200

    Added 05. Restaurant

---
+++ 04. C# - OOP/1.2 Inheritance - Exercise/Restaurant/Fish.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public class Fish : MainDish
+    {
+        private const double fishGrams = 22;
+
+        public Fish(string name, decimal price) : base(name, price, fishGrams)
+        {
+        }
+    }
+}
