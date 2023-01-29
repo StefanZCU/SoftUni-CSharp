6447e1014b0e0492bed7947c0eafb28611c6c303
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-29 11:12:07 +0200

    Added 02. Line Numbers

--- 03. C# - Advanced/4.2 Streams, Files and Directories - Exercises/Skeleton-Exercise/LineNumbers/LineNumbers.cs
+++ 03. C# - Advanced/4.2 Streams, Files and Directories - Exercises/Skeleton-Exercise/LineNumbers/LineNumbers.cs
@@ -1,19 +1,33 @@
-﻿namespace LineNumbers
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LineNumbers;
+
+public class LineNumbers
 {
-    using System;
-    public class LineNumbers
+    static void Main()
     {
-        static void Main()
-        {
-            string inputFilePath = @"..\..\..\text.txt";
-            string outputFilePath = @"..\..\..\output.txt";
+        string inputFilePath = @"..\..\..\text.txt";
+        string outputFilePath = @"..\..\..\output.txt";
 
-            ProcessLines(inputFilePath, outputFilePath);
+        ProcessLines(inputFilePath, outputFilePath);
+    }
+
+    public static void ProcessLines(string inputFilePath, string outputFilePath)
+    {
+        StringBuilder sb = new();
+
+        string[] lines = File.ReadAllLines(inputFilePath);
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            int lettersCount = lines[i].Count(char.IsLetter);
+            int symbolsCount = lines[i].Count(char.IsPunctuation);
+
+            sb.AppendLine($"Line {i + 1}: {lines[i]} ({lettersCount})({symbolsCount})");
         }
 
-        public static void ProcessLines(string inputFilePath, string outputFilePath)
-        {
-            throw new NotImplementedException();
-        }
+        File.WriteAllText(outputFilePath, sb.ToString());
     }
 }
