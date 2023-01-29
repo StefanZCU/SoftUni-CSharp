a400e95b717c3cc23a8679b2ba729a80afaead5f
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-29 13:55:03 +0200

    Added temp 06.

---
+++ 04. C# - OOP/1.2 Inheritance - Exercise/Animals/Dog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Dog : Animals
+    {
+        public Dog(string name, int age, string gender) : base(name, age, gender)
+        {
+
+        }
+
+        public void ProduceSound()
+        {
+            Console.WriteLine("Woof!");
+        }
+    }
+}
