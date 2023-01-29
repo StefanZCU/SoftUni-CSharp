0a777d5ddf59cee7b6036eb34c4b1fc6c1cd9f52
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-29 12:31:35 +0200

    Added 04. Random List

---
+++ 04. C# - OOP/1.1 Inheritance - Lab/04_Random_List/RandomList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public Random random = new Random();
+        
+
+        public string RandomString()
+        {
+            var index = random.Next(0, this.Count);
+            string element = this[index];
+            this.RemoveAt(index);
+            return element;
+        }
+    }
+}
