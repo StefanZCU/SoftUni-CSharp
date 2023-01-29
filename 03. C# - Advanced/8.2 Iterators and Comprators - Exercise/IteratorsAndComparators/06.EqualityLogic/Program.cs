c3b589e2938b489da82431490fe83a89db15ba5e
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-29 10:59:02 +0200

    Added 06. Equality Logic

---
+++ 03. C# - Advanced/8.2 Iterators and Comprators - Exercise/IteratorsAndComparators/06.EqualityLogic/Program.cs
@@ -0,0 +1,24 @@
+﻿using _06.EqualityLogic;
+
+HashSet<Person> hashSet = new HashSet<Person>();
+SortedSet<Person> sortedSet = new SortedSet<Person>();
+
+int count = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < count; i++)
+{
+    string[] personProps = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    Person person = new Person
+    {
+        Name = personProps[0],
+        Age = int.Parse(personProps[1])
+    };
+
+    hashSet.Add(person);
+    sortedSet.Add(person);
+}
+
+Console.WriteLine(hashSet.Count);
+Console.WriteLine(sortedSet.Count);
