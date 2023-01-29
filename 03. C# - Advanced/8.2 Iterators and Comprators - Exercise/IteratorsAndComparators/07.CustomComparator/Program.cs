643b4e6b9cba026c2973d98b19564b687e9a2882
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-29 11:00:48 +0200

    Added 07. Custom Comparator

---
+++ 03. C# - Advanced/8.2 Iterators and Comprators - Exercise/IteratorsAndComparators/07.CustomComparator/Program.cs
@@ -0,0 +1,41 @@
+﻿Func<int, int, int> customComparator = (x, y) =>
+{
+    if (x % 2 == 0 && y % 2 != 0)
+    {
+        return -1;
+    }
+
+    if (x % 2 != 0 && y % 2 == 0)
+    {
+        return 1;
+    }
+
+    return x.CompareTo(y);
+};
+
+int[] numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+Array.Sort(numbers, new Comparison<int>(customComparator));
+
+Console.WriteLine(string.Join(" ", numbers));
+
+public class CustomComparator : IComparer<int>
+{
+    public int Compare(int x, int y)
+    {
+        if (x % 2 == 0 && y % 2 != 0)
+        {
+            return -1;
+        }
+
+        if (x % 2 != 0 && y % 2 == 0)
+        {
+            return 1;
+        }
+
+        return x.CompareTo(y);
+    }
+}
