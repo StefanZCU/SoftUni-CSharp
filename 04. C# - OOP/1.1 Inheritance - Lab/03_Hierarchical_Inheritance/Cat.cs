4538564d61ecb80ae83ae088c06bc90dcb0ec370
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-29 12:22:37 +0200

    Added 03. Hierarchical Inheritance

---
+++ 04. C# - OOP/1.1 Inheritance - Lab/03_Hierarchical_Inheritance/Cat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farm
+{
+    public class Cat : Animal
+    {
+        public void Meow()
+        {
+            Console.WriteLine("meowing...");
+        }
+
+    }
+}
