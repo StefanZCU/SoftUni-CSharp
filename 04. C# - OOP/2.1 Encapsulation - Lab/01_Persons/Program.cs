e59aea0862da2a64c0b02e0f8cd3cdcf680ba832
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-31 08:56:05 +0200

    Added 2.1 Encapsulation - Lab

---
+++ 04. C# - OOP/2.1 Encapsulation - Lab/01_Persons/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_Persons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
