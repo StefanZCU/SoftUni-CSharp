ff65f91bb4122f849deca5fd3906c91f063c575b
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-31 09:54:52 +0200

    Added 01. Class Box Data

---
+++ 04. C# - OOP/2.2 Encapsulation - Exercise/01_Class_Box_Data/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace _01_Class_Box_Data
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(length, width, height);
+                Console.WriteLine(box);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+        }
+    }
+}
