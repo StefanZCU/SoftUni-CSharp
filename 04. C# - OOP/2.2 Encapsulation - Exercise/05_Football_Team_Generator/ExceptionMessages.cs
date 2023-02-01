80b4f712f5857291c2f46c4fffc10c8a372ee63f
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-01 10:28:57 +0200

    Added 05. Football Team Generator

---
+++ 04. C# - OOP/2.2 Encapsulation - Exercise/05_Football_Team_Generator/ExceptionMessages.cs
@@ -0,0 +1,13 @@
+﻿namespace _05_Football_Team_Generator
+{
+    public static class ExceptionMessages
+    {
+        public const string InvalidName = "A name should not be emepty.";
+        public const string InvalidStatRange = "{0} should be between 0 and 100.";
+        public const string PlayerDoesntExist = "Player {0} is not in {1} team.";
+        public const string TeamDoesntExist = "Team {0} does not exist.";
+
+
+    }
+
+}
