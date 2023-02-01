9603e5a52f0add97822c10196926ba1eefc59a9e
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-01 13:26:30 +0200

    Added 3.2 Interfaces and Abstraction

---
+++ 04. C# - OOP/3.1 Interfaces and Abstraction/01_Shapes/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_Shapes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ 04. C# - OOP/3.1 Interfaces and Abstraction/02_Cars/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02_Cars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
