5b501c9d8ba15dc6683ec0b7b51871d3efa9aba6
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-06 17:52:18 +0200

    Added 02. Enter Numbers

--- 04. C# - OOP/5.1 Exceptions and Error Handling - Lab/P02/Program.cs
+++ 04. C# - OOP/5.1 Exceptions and Error Handling - Lab/P02/Program.cs
@@ -4,7 +4,54 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] array = new int[10];
+            int index = 0;
+            while (index < 10)
+            {
+                try
+                {
+                    if (index == 0)
+                    {
+                        array[index] = ReadNumber(1, 100);
+                    }
+                    else
+                    {
+                        array[index] = ReadNumber(array.Max(), 100);
+                    }
+
+                    index++;
+                }
+                catch (ArgumentOutOfRangeException aoore)
+                {
+                    Console.WriteLine(aoore.Message);
+                }
+                catch (Exception ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", array));
+        }
+
+        static int ReadNumber(int start, int end)
+        {
+            int num;
+            try
+            {
+                num = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                throw new Exception("Invalid Number!");
+            }
+
+            if (num <= start || num >= end)
+            {
+                throw new ArgumentOutOfRangeException($"Your number is not in range {start} - {end}!");
+            }
+
+            return num;
         }
     }
 }
