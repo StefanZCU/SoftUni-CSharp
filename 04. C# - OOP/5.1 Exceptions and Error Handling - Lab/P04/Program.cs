27f1e67ab0085ff3fa61ddd42e7f21d690a1b2c3
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-07 12:37:30 +0200

    Added 04. Sum of Integers

--- 04. C# - OOP/5.1 Exceptions and Error Handling - Lab/P04/Program.cs
+++ 04. C# - OOP/5.1 Exceptions and Error Handling - Lab/P04/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int sum = 0;
+            string[] elements = Console.ReadLine().Split();
+
+            foreach (string element in elements)
+            {
+                try
+                {
+                    int currentNum = int.Parse(element);
+                    sum += currentNum;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"The element '{element}' is in wrong format!");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"The element '{element}' is out of range!");
+                }
+                finally
+                {
+                    Console.WriteLine($"Element '{element}' processed - current sum: {sum}");
+                }
+            }
+
+            Console.WriteLine($"The total sum of all integers is: {sum}");
         }
     }
 }
