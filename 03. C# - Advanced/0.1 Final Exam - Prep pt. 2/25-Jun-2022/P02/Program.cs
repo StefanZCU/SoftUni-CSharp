4328b6a45f0671035c1f809dc5d1cf4fe6bfa7bc
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-15 13:10:36 +0200

    Added 02. Wall Destroyer

--- 03. C# - Advanced/0.1 Final Exam - Prep pt. 2/25-Jun-2022/P02/Program.cs
+++ 03. C# - Advanced/0.1 Final Exam - Prep pt. 2/25-Jun-2022/P02/Program.cs
@@ -1,71 +1,118 @@
 ﻿namespace P02
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Stack<int> whiteTiles = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            int matrixSize = int.Parse(Console.ReadLine());
 
-            Queue<int> grayTiles = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            char[,] matrix = new char[matrixSize, matrixSize];
+            int[] position = new int[2];
 
-            Dictionary<string, int> tilesMade = new Dictionary<string, int>
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                { "Sink", 0 },
-                { "Oven", 0 },
-                { "Countertop", 0 },
-                { "Wall", 0 },
-                { "Floor", 0 }
-            };
+                string line = Console.ReadLine();
 
-            while (whiteTiles.Any() && grayTiles.Any())
-            {
-                if (whiteTiles.Peek() == grayTiles.Peek())
+                for (int j = 0; j < matrix.GetLength(1); j++)
                 {
-                    switch (whiteTiles.Peek() + grayTiles.Peek())
+                    if (line[j] == 'V')
                     {
-                        case 40:
-                            tilesMade["Sink"]++;
-                            break;
-                        case 50:
-                            tilesMade["Oven"]++;
-                            break;
-                        case 60:
-                            tilesMade["Countertop"]++;
-                            break;
-                        case 70:
-                            tilesMade["Wall"]++;
-                            break;
-                        default:
-                            tilesMade["Floor"]++;
-                            break;
+                        position[0] = i;
+                        position[1] = j;
+                        matrix[i, j] = '*';
+                        continue;
                     }
 
-                    whiteTiles.Pop();
-                    grayTiles.Dequeue();
-                }
-                else
-                {
-                    whiteTiles.Push(whiteTiles.Pop() / 2);
-                    grayTiles.Enqueue(grayTiles.Dequeue());
+                    matrix[i, j] = line[j];
                 }
             }
 
-            Console.WriteLine(whiteTiles.Any()
-                ? $"White tiles left: {string.Join(", ", whiteTiles)}"
-                : "White tiles left: none");
 
-            Console.WriteLine(grayTiles.Any()
-                ? $"Grey tiles left: {string.Join(", ", grayTiles)}"
-                : "Grey tiles left: none");
+            int holesMade = 1;
+            int rodsHit = 0;
+            bool electrocuted = false;
 
-            foreach (var tile in tilesMade.Where(x => x.Value >= 1).OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            string command;
+            while ((command = Console.ReadLine()) != "End")
             {
-                Console.WriteLine($"{tile.Key}: {tile.Value}");
+                int row = position[0];
+                int col = position[1];
+                int previousRow = position[0];
+                int previousCol = position[1];
+
+                switch (command)
+                {
+                    case "up" when IndexChecker(--row, col, matrix):
+                        position[0]--;
+                        break;
+                    case "down" when IndexChecker(++row, col, matrix):
+                        position[0]++;
+                        break;
+                    case "right" when IndexChecker(row, ++col, matrix):
+                        position[1]++;
+                        break;
+                    case "left" when IndexChecker(row, --col, matrix):
+                        position[1]--;
+                        break;
+                    default:
+                        continue;
+                }
+
+                row = position[0];
+                col = position[1];
+
+                switch (matrix[row, col])
+                {
+                    case 'C':
+                        matrix[row, col] = 'E';
+                        holesMade++;
+                        electrocuted = true;
+                        break;
+                    case '*':
+                        Console.WriteLine($"The wall is already destroyed at position [{row}, {col}]!");
+                        break;
+                    case 'R':
+                        Console.WriteLine("Vanko hit a rod!");
+                        rodsHit++;
+                        position[0] = previousRow;
+                        position[1] = previousCol;
+                        break;
+                    default:
+                        matrix[row, col] = '*';
+                        holesMade++;
+                        break;
+                }
+
+                if (electrocuted)
+                {
+                    break;
+                }
             }
+
+            Console.WriteLine(electrocuted
+                ? $"Vanko got electrocuted, but he managed to make {holesMade} hole(s)."
+                : $"Vanko managed to make {holesMade} hole(s) and he hit only {rodsHit} rod(s).");
+
+            matrix[position[0], position[1]] = !electrocuted ? 'V' : 'E';
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+
+        private static bool IndexChecker(int row, int col, char[,] matrix)
+        {
+            return row >= 0 && row < matrix.GetLength(0) &&
+                   col >= 0 && col < matrix.GetLength(1);
         }
     }
 }
