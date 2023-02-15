552ba999cbd226df3f891f3fb5b698ec22ea26de
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-15 12:58:07 +0200

    Added 01. Energy Drinks

--- 03. C# - Advanced/0.1 Final Exam - Prep pt. 2/22-Oct-2022/P01/Program.cs
+++ 03. C# - Advanced/0.1 Final Exam - Prep pt. 2/22-Oct-2022/P01/Program.cs
@@ -1,12 +1,44 @@
 ﻿namespace P01
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack<int> caffeine = new Stack<int>(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+            Queue<int> energyDrink = new Queue<int>(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+
+            int caffeineDrank = 0;
+
+            while (caffeine.Any() && energyDrink.Any())
+            {
+                if (caffeine.Peek() * energyDrink.Peek() + caffeineDrank <= 300)
+                {
+                    caffeineDrank += caffeine.Pop() * energyDrink.Dequeue();
+                }
+                else
+                {
+                    caffeine.Pop();
+                    if (caffeineDrank - 30 >= 0)
+                    {
+                        caffeineDrank -= 30;
+                    }
+                    else
+                    {
+                        caffeineDrank = 0;
+                    }
+                    energyDrink.Enqueue(energyDrink.Dequeue());
+                }
+            }
+
+            Console.WriteLine(energyDrink.Any()
+                ? $"Drinks left: {string.Join(", ", energyDrink)}"
+                : "At least Stamat wasn't exceeding the maximum caffeine.");
+
+            Console.WriteLine($"Stamat is going to sleep with {caffeineDrank} mg caffeine.");
         }
     }
 }
