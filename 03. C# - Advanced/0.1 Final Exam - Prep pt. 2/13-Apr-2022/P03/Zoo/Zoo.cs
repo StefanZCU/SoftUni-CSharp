6c469965ef2e3b36726d9a4b367ed93da84c90e1
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-15 15:52:49 +0200

    Added 03. Zoo

---
+++ 03. C# - Advanced/0.1 Final Exam - Prep pt. 2/13-Apr-2022/P03/Zoo/Zoo.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Zoo
+{
+    public class Zoo
+    {
+        private List<Animal> animals;
+
+        public Zoo(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            animals = new List<Animal>();
+        }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public IReadOnlyCollection<Animal> Animals => animals;
+
+        public string AddAnimal(Animal animal)
+        {
+            if (string.IsNullOrWhiteSpace(animal.Species))
+            {
+                return "Invalid animal species.";
+            }
+
+            if (animal.Diet != "herbivore" && animal.Diet != "carnivore")
+            {
+                return "Invalid animal diet.";
+            }
+
+            if (animals.Count == Capacity)
+            {
+                return "The zoo is full.";
+            }
+
+            animals.Add(animal);
+            return $"Successfully added {animal.Species} to the zoo.";
+        }
+
+        public int RemoveAnimals(string species)
+        {
+            int removedAnimals = 0;
+
+            while (animals.FirstOrDefault(x => x.Species == species) != null)
+            {
+                animals.Remove(animals.FirstOrDefault(y => y.Species == species));
+                removedAnimals++;
+            }
+
+            return removedAnimals;
+        }
+
+        public List<Animal> GetAnimalsByDiet(string diet)
+        {
+            return animals.Where(x => x.Diet == diet).ToList();
+        }
+
+        public Animal GetAnimalByWeight(double weight)
+        {
+            return animals.FirstOrDefault(x => x.Weight == weight);
+        }
+
+        public string GetAnimalCountByLength(double minimumLength, double maximumLength)
+        {
+            var count = animals.Count(x => x.Length >= minimumLength && x.Length <= maximumLength);
+
+            return $"There are {count} animals with a length between {minimumLength} and {maximumLength} meters.";
+        }
+    }
+}
