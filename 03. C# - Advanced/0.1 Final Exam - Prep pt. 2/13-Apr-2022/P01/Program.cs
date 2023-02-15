3dcdb635b8fdfed518f5f70bf46d0e8e0bd1e1a7
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-15 13:50:53 +0200

    Added 01. Meal Plan

--- 03. C# - Advanced/0.1 Final Exam - Prep pt. 2/13-Apr-2022/P01/Program.cs
+++ 03. C# - Advanced/0.1 Final Exam - Prep pt. 2/13-Apr-2022/P01/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace P01
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01
 {
     using System;
 
@@ -6,7 +10,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Queue<string> meals = new Queue<string>(Console.ReadLine().Split());
+            Stack<int> caloriesPerDay = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+
+            int mealsEaten = 0;
+
+            while (meals.Any() && caloriesPerDay.Any())
+            {
+                var calories = meals.Peek() switch
+                {
+                    "salad" => 350,
+                    "soup" => 490,
+                    "pasta" => 680,
+                    "steak" => 790,
+                    _ => 0
+                };
+
+                if (caloriesPerDay.Peek() > calories)
+                {
+                    caloriesPerDay.Push(caloriesPerDay.Pop() - calories);
+                    meals.Dequeue();
+                }
+                else if (caloriesPerDay.Peek() == calories)
+                {
+                    caloriesPerDay.Pop();
+                    meals.Dequeue();
+                }
+                else
+                {
+                    int caloriesLeftOver = calories - caloriesPerDay.Peek();
+                    meals.Dequeue();
+                    caloriesPerDay.Pop();
+
+                    if (caloriesPerDay.Any())
+                    {
+                        caloriesPerDay.Push(caloriesPerDay.Pop() - caloriesLeftOver);
+                    }
+                }
+
+                mealsEaten++;
+            }
+
+            Console.WriteLine(meals.Any()
+                ? $"John ate enough, he had {mealsEaten} meals.\nMeals left: {string.Join(", ", meals)}."
+                : $"John had {mealsEaten} meals.\nFor the next few days, he can eat {string.Join(", ", caloriesPerDay)} calories.");
         }
     }
 }
