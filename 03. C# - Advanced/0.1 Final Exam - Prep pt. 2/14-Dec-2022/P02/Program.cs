3bdf63f110e65a65e1eab97df06c7138fd57859e
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-15 12:50:47 +0200

    Added 02. Navy Battle

--- 03. C# - Advanced/0.1 Final Exam - Prep pt. 2/14-Dec-2022/P02/Program.cs
+++ 03. C# - Advanced/0.1 Final Exam - Prep pt. 2/14-Dec-2022/P02/Program.cs
@@ -6,7 +6,91 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int matrixSize = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[matrixSize, matrixSize];
+            int[] position = new int[2];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                string line = Console.ReadLine();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (line[j] == 'S')
+                    {
+                        position[0] = i;
+                        position[1] = j;
+                        matrix[i, j] = '-';
+                        continue;
+                    }
+
+                    matrix[i, j] = line[j];
+                }
+            }
+
+            int destroyedShips = 0;
+            int minesHit = 0;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "up":
+                        position[0]--;
+                        break;
+                    case "down":
+                        position[0]++;
+                        break;
+                    case "left":
+                        position[1]--;
+                        break;
+                    case "right":
+                        position[1]++;
+                        break;
+                }
+
+                int row = position[0];
+                int col = position[1];
+
+                switch (matrix[row, col])
+                {
+                    case '-':
+                        continue;
+                    case '*':
+                        matrix[row, col] = '-';
+                        minesHit++;
+                        break;
+                    case 'C':
+                        matrix[row, col] = '-';
+                        destroyedShips++;
+                        break;
+                }
+
+                if (minesHit == 3)
+                {
+                    Console.WriteLine($"Mission failed, U-9 disappeared! Last known coordinates [{row}, {col}]!");
+                    break;
+                }
+
+                if (destroyedShips != 3) continue;
+                Console.WriteLine("Mission accomplished, U-9 has destroyed all battle cruisers of the enemy!");
+                break;
+            }
+
+            matrix[position[0], position[1]] = 'S';
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j]);
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
