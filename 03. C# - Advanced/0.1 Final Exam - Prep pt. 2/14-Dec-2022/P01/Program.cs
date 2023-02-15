9e486184feeb85f76e168a07afcb2788778c70d5
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-15 12:50:04 +0200

    Added 01. Climb The Peaks

--- 03. C# - Advanced/0.1 Final Exam - Prep pt. 2/14-Dec-2022/P01/Program.cs
+++ 03. C# - Advanced/0.1 Final Exam - Prep pt. 2/14-Dec-2022/P01/Program.cs
@@ -1,12 +1,57 @@
 ﻿namespace P01
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Queue<string> conqueredPeaks = new Queue<string>();
+
+            Stack<int> foodPortions = new Stack<int>(Console.ReadLine()
+                .Split(", ").Select(int.Parse)
+                .ToArray());
+            Queue<int> stamina = new Queue<int>(Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray());
+
+            Dictionary<string, int> peaks = new Dictionary<string, int>()
+            {
+                { "Vihren", 80},
+                { "Kutelo", 90},
+                { "Banski Suhodol", 100},
+                { "Polezhan", 60},
+                { "Kamenitza", 70}
+            };
+
+            Queue<string> peakNames = new Queue<string>();
+
+            foreach (var peak in peaks)
+            {
+                peakNames.Enqueue(peak.Key);
+            }
+
+            while (foodPortions.Any() && stamina.Any() && peakNames.Any())
+            {
+                if (foodPortions.Peek() + stamina.Peek() >= peaks[peakNames.Peek()])
+                {
+                    conqueredPeaks.Enqueue(peakNames.Dequeue());
+                }
+
+                foodPortions.Pop();
+                stamina.Dequeue();
+            }
+
+            Console.WriteLine(peakNames.Any()
+                ? "Alex failed! He has to organize his journey better next time -> @PIRINWINS"
+                : "Alex did it! He climbed all top five Pirin peaks in one week -> @FIVEinAWEEK");
+
+            if (!conqueredPeaks.Any()) return;
+            Console.WriteLine("Conquered peaks:");
+            Console.WriteLine(string.Join("\n", conqueredPeaks));
         }
     }
 }
