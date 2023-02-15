5c28f4d064fa9aba3c481fbacb1e27308775d9b0
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-15 13:09:06 +0200

    Added 01. Tiles Master

--- 03. C# - Advanced/0.1 Final Exam - Prep pt. 2/25-Jun-2022/P02/Program.cs
+++ 03. C# - Advanced/0.1 Final Exam - Prep pt. 2/25-Jun-2022/P02/Program.cs
@@ -1,12 +1,71 @@
 ﻿namespace P02
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack<int> whiteTiles = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+
+            Queue<int> grayTiles = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+
+            Dictionary<string, int> tilesMade = new Dictionary<string, int>
+            {
+                { "Sink", 0 },
+                { "Oven", 0 },
+                { "Countertop", 0 },
+                { "Wall", 0 },
+                { "Floor", 0 }
+            };
+
+            while (whiteTiles.Any() && grayTiles.Any())
+            {
+                if (whiteTiles.Peek() == grayTiles.Peek())
+                {
+                    switch (whiteTiles.Peek() + grayTiles.Peek())
+                    {
+                        case 40:
+                            tilesMade["Sink"]++;
+                            break;
+                        case 50:
+                            tilesMade["Oven"]++;
+                            break;
+                        case 60:
+                            tilesMade["Countertop"]++;
+                            break;
+                        case 70:
+                            tilesMade["Wall"]++;
+                            break;
+                        default:
+                            tilesMade["Floor"]++;
+                            break;
+                    }
+
+                    whiteTiles.Pop();
+                    grayTiles.Dequeue();
+                }
+                else
+                {
+                    whiteTiles.Push(whiteTiles.Pop() / 2);
+                    grayTiles.Enqueue(grayTiles.Dequeue());
+                }
+            }
+
+            Console.WriteLine(whiteTiles.Any()
+                ? $"White tiles left: {string.Join(", ", whiteTiles)}"
+                : "White tiles left: none");
+
+            Console.WriteLine(grayTiles.Any()
+                ? $"Grey tiles left: {string.Join(", ", grayTiles)}"
+                : "Grey tiles left: none");
+
+            foreach (var tile in tilesMade.Where(x => x.Value >= 1).OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{tile.Key}: {tile.Value}");
+            }
         }
     }
 }
