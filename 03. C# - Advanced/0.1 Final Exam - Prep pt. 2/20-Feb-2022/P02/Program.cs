b8f3c1e37774e5667af29bbdaa0c0e9d5f3556ce
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-16 10:50:13 +0200

    Added 20-Feb-2022

---
+++ 03. C# - Advanced/0.1 Final Exam - Prep pt. 2/20-Feb-2022/P02/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace P02
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
