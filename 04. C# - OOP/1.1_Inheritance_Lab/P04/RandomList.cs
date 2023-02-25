469a58ca9c6a32b314eb19167c0bc055600562a4
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-25 10:28:16 +0200

    Added 04. Random List

---
+++ 04. C# - OOP/1.1_Inheritance_Lab/P04/RandomList.cs
@@ -0,0 +1,22 @@
+﻿namespace CustomRandomList
+{
+    using System;
+
+    public class RandomList : List<string>
+    {
+        private readonly Random _random;
+
+        public RandomList()
+        {
+            _random = new Random();
+        }
+
+        public string RandomString()
+        {
+            var index = _random.Next(0, Count);
+            var element = this[index];
+            RemoveAt(index);
+            return element;
+        }
+    }
+}
