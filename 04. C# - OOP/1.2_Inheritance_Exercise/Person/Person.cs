c651ed709e02f6ec4aab27126026b3289c714d4a
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-25 11:37:11 +0200

    Added 02. Zoo

--- 04. C# - OOP/1.2_Inheritance_Exercise/Person/Person.cs
+++ 04. C# - OOP/1.2_Inheritance_Exercise/Person/Person.cs
@@ -7,7 +7,6 @@
         public string Name { get; set; }
         public int Age { get; set; }
 
-
         public Person(string name, int age)
         {
             Name = name;
---
+++ 04. C# - OOP/1.2_Inheritance_Exercise/Zoo/Animal.cs
@@ -0,0 +1,13 @@
+﻿namespace Zoo
+{
+    public class Animal
+    {
+        public Animal(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+    }
+}
