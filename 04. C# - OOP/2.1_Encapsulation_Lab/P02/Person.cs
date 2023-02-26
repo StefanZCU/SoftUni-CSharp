cdeff4de2de30047dbd29f8c6adaca09105d3860
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-26 10:40:22 +0200

    Added 02. Salary

---
+++ 04. C# - OOP/2.1_Encapsulation_Lab/P02/Person.cs
@@ -0,0 +1,33 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName { get; }
+        public string LastName { get; }
+        public int Age { get; }
+        public decimal Salary { get; private set; }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age < 30)
+            {
+                percentage /= 2;
+            }
+
+            Salary += Salary * percentage / 100;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:F2} leva.";
+        }
+    }
+}
