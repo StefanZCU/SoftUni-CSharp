ec20daa1595fc039d068cf8edae97f02480660fa
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-26 11:57:51 +0200

    Added 03. Shopping Spree

---
+++ 04. C# - OOP/2.2_Encapsulation_Exercise/P03/Person.cs
@@ -0,0 +1,59 @@
+﻿namespace P03
+{
+    public class Person
+    {
+        private string name;
+        private int money;
+
+        public Person(string name, int money)
+        {
+            Name = name;
+            Money = money;
+            Products = new List<Product>();
+        }
+
+        public List<Product> Products { get; }
+
+        public string Name
+        {
+            get => name;
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                name = value;
+            }
+        }
+
+        public int Money
+        {
+            get => money;
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                money = value;
+            }
+        }
+
+        public string AddProduct(string productName, List<Product> allProducts)
+        {
+            if (Money - allProducts.First(x => x.Name == productName).Cost >= 0)
+            {
+                Money -= allProducts.First(x => x.Name == productName).Cost;
+                Products.Add(allProducts.First(x => x.Name == productName));
+                return $"{Name} bought {productName}";
+            }
+
+            return $"{Name} can't afford {productName}";
+        }
+    }
+}
