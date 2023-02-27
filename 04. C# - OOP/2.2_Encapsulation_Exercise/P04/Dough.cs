a60256cc052ba62ad671cfd38b73060e79014a9b
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-27 11:03:46 +0200

    Added 04. Pizza Calories

---
+++ 04. C# - OOP/2.2_Encapsulation_Exercise/P04/Dough.cs
@@ -0,0 +1,87 @@
+﻿namespace P04
+{
+    public class Dough
+    {
+        private string flourType;
+        private string bakingTechnique;
+        private double weight;
+
+        public Dough(string flourType, string bakingTechnique, double weight)
+        {
+            FlourType = flourType;
+            BakingTechnique = bakingTechnique;
+            Weight = weight;
+        }
+
+        private string FlourType
+        {
+            get => flourType;
+
+            set
+            {
+                if (value.ToLower() is not ("white" or "wholegrain"))
+                {
+                    throw new Exception(ExceptionMessages.InvalidDough);
+                }
+
+                flourType = value;
+            }
+        }
+
+        private string BakingTechnique
+        {
+            get => bakingTechnique;
+
+            set
+            {
+                if (value.ToLower() is not ("crispy" or "chewy" or "homemade"))
+                {
+                    throw new Exception(ExceptionMessages.InvalidDough);
+                }
+
+                bakingTechnique = value;
+            }
+        }
+
+        private double Weight
+        {
+            get => weight;
+
+            set
+            {
+                if (value is > 200 or < 1)
+                {
+                    throw new Exception(ExceptionMessages.WrongDoughWeight);
+                }
+
+                weight = value;
+            }
+        }
+
+        public double CaloriesPerGram => GetCaloriesPerGram();
+
+        private double GetCaloriesPerGram()
+        {
+            Dictionary<string, double> modifiers = new Dictionary<string, double>
+            {
+                { "white", 1.5 },
+                { "wholegrain", 1.0 },
+                { "crispy", 0.9 },
+                { "chewy", 1.1 },
+                { "homemade", 1.0 }
+            };
+
+            var totalCalories = 2 * Weight;
+
+            foreach (var modifier in modifiers)
+            {
+                if (modifier.Key == FlourType.ToLower() || modifier.Key == BakingTechnique.ToLower())
+                {
+                    totalCalories *= modifier.Value;
+                }
+            }
+
+            return totalCalories;
+        }
+    }
+}
---
+++ 04. C# - OOP/2.2_Encapsulation_Exercise/P05/ExceptionMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace P05
+{
+    public static class ExceptionMessages
+    {
+        public const string NameIsNull = "A name should not be empty.";
+        public const string StatIsInvalid = "{0} should be between 0 and 100.";
+        public const string PlayerDoesNotExist = "Player {0} is not in {1} team.";
+        public const string TeamDoesNotExist = "Team {0} does not exist.";
+    }
+}
