13d13b430f010d28ac411c7553f7b21afbfb3bf3
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-28 12:22:48 +0200

    Added 01. Shapes

---
+++ 04. C# - OOP/3.1_Interfaces_and_Abstraction_Lab/P01/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public void Draw()
+        {
+            DrawLine(width, '*', '*');
+            for (int i = 1; i < height - 1; ++i)
+            {
+                DrawLine(width, '*', ' ');
+            }
+            DrawLine(width, '*', '*');
+        }
+
+        private void DrawLine(int width, char end, char mid)
+        {
+            Console.Write(end);
+
+            for (int i = 1; i < width - 1; ++i)
+            {
+                Console.Write(mid);
+            }
+
+            Console.WriteLine(end);
+        }
+    }
+}
