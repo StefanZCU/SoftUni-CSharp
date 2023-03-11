d3a0618e87abe1be33e5e6dfd9d10adfbc24e156
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-11 15:01:23 +0200

    Create Weapon.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/14-Aug-2022/Models/Weapons/Weapon.cs
@@ -0,0 +1,40 @@
+﻿namespace PlanetWars.Models.Weapons
+{
+    using System;
+
+    using Contracts;
+    using Utilities.Messages;
+
+    public abstract class Weapon : IWeapon
+    {
+        private int destructionLevel;
+
+        protected Weapon(int destructionLevel, double price)
+        {
+            DestructionLevel = destructionLevel;
+            Price = price;
+        }
+
+        public double Price { get; private set; }
+
+        public int DestructionLevel
+        {
+            get => destructionLevel;
+
+            private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException(ExceptionMessages.TooLowDestructionLevel);
+                }
+                
+                if (value > 10)
+                {
+                    throw new ArgumentException(ExceptionMessages.TooHighDestructionLevel);
+                }
+
+                destructionLevel = value;
+            }
+        }
+    }
+}
