1ae206b6222b8ad3978a8166d4b7fef3544ee24e
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-11 15:01:31 +0200

    Create PlanetRepository.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/14-Aug-2022/Repositories/PlanetRepository.cs
@@ -0,0 +1,37 @@
+﻿namespace PlanetWars.Repositories
+{
+    using System.Linq;
+    using System.Collections.Generic;
+
+    using Contracts;
+    using PlanetWars.Models.Planets.Contracts;
+
+    public class PlanetRepository : IRepository<IPlanet>
+    {
+        private List<IPlanet> models;
+
+        public PlanetRepository()
+        {
+            models = new List<IPlanet>();
+        }
+
+        public IReadOnlyCollection<IPlanet> Models => models.AsReadOnly();
+        public IPlanet FindByName(string name)
+        {
+            return models.FirstOrDefault(x => x.Name == name);
+        }
+        public void AddItem(IPlanet model)
+        {
+            models.Add(model);
+        }
+
+        public bool RemoveItem(string name)
+        {
+            var planetToRemove = models.FirstOrDefault(x => x.Name == name);
+            if (planetToRemove == null) return false;
+
+            models.Remove(planetToRemove);
+            return true;
+        }
+    }
+}
