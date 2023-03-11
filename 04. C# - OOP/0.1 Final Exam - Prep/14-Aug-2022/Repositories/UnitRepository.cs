e38cd4f260d7670987648ae97afcb669679ad425
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-11 15:01:33 +0200

    Create UnitRepository.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/14-Aug-2022/Repositories/UnitRepository.cs
@@ -0,0 +1,37 @@
+﻿namespace PlanetWars.Repositories
+{
+    using System.Linq;
+    using System.Collections.Generic;
+
+    using Contracts;
+    using PlanetWars.Models.MilitaryUnits.Contracts;
+
+    public class UnitRepository : IRepository<IMilitaryUnit>
+    {
+        private List<IMilitaryUnit> models;
+
+        public UnitRepository()
+        {
+            models = new List<IMilitaryUnit>();
+        }
+
+        public IReadOnlyCollection<IMilitaryUnit> Models => models.AsReadOnly();
+        public IMilitaryUnit FindByName(string name)
+        {
+            return models.FirstOrDefault(x => x.GetType().Name == name);
+        }
+        public void AddItem(IMilitaryUnit model)
+        {
+            models.Add(model);
+        }
+        public bool RemoveItem(string name)
+        {
+            var unitToRemove = models.FirstOrDefault(x => x.GetType().Name == name);
+
+            if (unitToRemove == null) return false;
+
+            models.Remove(unitToRemove);
+            return true;
+        }
+    }
+}
