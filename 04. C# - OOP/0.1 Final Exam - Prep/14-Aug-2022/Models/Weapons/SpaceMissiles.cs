e90756a684523fffc3fd0590da75ee6f930a12e8
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-11 15:01:22 +0200

    Create SpaceMissiles.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/14-Aug-2022/Models/Weapons/SpaceMissiles.cs
@@ -0,0 +1,11 @@
+﻿namespace PlanetWars.Models.Weapons
+{
+    public class SpaceMissiles : Weapon
+    {
+        private const double PRICE = 8.75;
+
+        public SpaceMissiles(int destructionLevel) : base(destructionLevel, PRICE)
+        {
+        }
+    }
+}
