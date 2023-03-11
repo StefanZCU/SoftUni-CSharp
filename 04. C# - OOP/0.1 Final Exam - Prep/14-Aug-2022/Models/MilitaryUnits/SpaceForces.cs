91bb519a08e99ddab485ac6b8214a8a8f0856fec
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-11 15:01:09 +0200

    Create SpaceForces.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/14-Aug-2022/Models/MilitaryUnits/SpaceForces.cs
@@ -0,0 +1,10 @@
+﻿namespace PlanetWars.Models.MilitaryUnits
+{
+    public class SpaceForces : MilitaryUnit
+    {
+        private const double COST = 11;
+        public SpaceForces() : base(COST)
+        {
+        }
+    }
+}
