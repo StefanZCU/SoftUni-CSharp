5d105bbe30c126659b3e63a6638e3aeed76f672f
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-11 15:01:07 +0200

    Create MilitaryUnit.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/14-Aug-2022/Models/MilitaryUnits/MilitaryUnit.cs
@@ -0,0 +1,28 @@
+﻿namespace PlanetWars.Models.MilitaryUnits
+{
+    using System;
+
+    using Contracts;
+    using Utilities.Messages;
+
+    public abstract class MilitaryUnit : IMilitaryUnit
+    {
+        protected MilitaryUnit(double cost)
+        {
+            Cost = cost;
+            EnduranceLevel = 1;
+        }
+
+        public double Cost { get; private set; }
+        public int EnduranceLevel { get; private set; }
+
+        public void IncreaseEndurance()
+        {
+            EnduranceLevel++;
+            if (EnduranceLevel <= 20) return;
+
+            EnduranceLevel = 20;
+            throw new ArgumentException(ExceptionMessages.EnduranceLevelExceeded);
+        }
+    }
+}
