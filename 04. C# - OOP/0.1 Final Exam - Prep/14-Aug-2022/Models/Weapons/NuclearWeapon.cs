deb527f82098654dd1e98ca82514efee761b1deb
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-11 15:01:20 +0200

    Create NuclearWeapon.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/14-Aug-2022/Models/Weapons/NuclearWeapon.cs
@@ -0,0 +1,10 @@
+﻿namespace PlanetWars.Models.Weapons
+{
+    public class NuclearWeapon : Weapon
+    {
+        private const double PRICE = 15;
+        public NuclearWeapon(int destructionLevel) : base(destructionLevel, PRICE)
+        {
+        }
+    }
+}
