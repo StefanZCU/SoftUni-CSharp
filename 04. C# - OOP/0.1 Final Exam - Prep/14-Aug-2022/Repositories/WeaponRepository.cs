51fdbf23faa43b57fa472750f48e6132dde67164
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-11 15:01:35 +0200

    Create WeaponRepository.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/14-Aug-2022/Repositories/WeaponRepository.cs
@@ -0,0 +1,40 @@
+﻿namespace PlanetWars.Repositories
+{
+    using System.Linq;
+    using System.Collections.Generic;
+
+    using Contracts;
+    using PlanetWars.Models.Weapons.Contracts;
+
+    public class WeaponRepository : IRepository<IWeapon>
+    {
+        private List<IWeapon> models;
+
+        public WeaponRepository()
+        {
+            models = new List<IWeapon>();
+        }
+
+        public IReadOnlyCollection<IWeapon> Models => models.AsReadOnly();
+
+        public IWeapon FindByName(string name)
+        {
+            return models.FirstOrDefault(x => x.GetType().Name == name);
+        }
+
+        public void AddItem(IWeapon model)
+        {
+            models.Add(model);
+        }
+
+        public bool RemoveItem(string name)
+        {
+            var weaponToRemove = models.FirstOrDefault(x => x.GetType().Name == name);
+
+            if (weaponToRemove == null) return false;
+
+            models.Remove(weaponToRemove);
+            return true;
+        }
+    }
+}
