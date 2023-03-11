1faf8bfefc4313569e305bffe55f7fb71a36ad0a
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-11 15:01:17 +0200

    Create BioChemicalWeapon.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/14-Aug-2022/Models/Weapons/BioChemicalWeapon.cs
@@ -0,0 +1,10 @@
+﻿namespace PlanetWars.Models.Weapons
+{
+    public class BioChemicalWeapon : Weapon
+    {
+        private const double PRICE = 3.2;
+        public BioChemicalWeapon(int destructionLevel) : base(destructionLevel, PRICE)
+        {
+        }
+    }
+}
