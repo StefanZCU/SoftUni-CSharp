7202d5e936585b372dabc8b7e266a572dbfa732b
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-12 12:53:49 +0200

    Update OutputMessages.cs

--- 04. C# - OOP/0.1 Final Exam - Prep/18-Apr-2022/Heroes/Utilities/Messages/OutputMessages.cs
+++ 04. C# - OOP/0.1 Final Exam - Prep/18-Apr-2022/Heroes/Utilities/Messages/OutputMessages.cs
@@ -4,5 +4,9 @@
     {
         public const string KnightsWon = "The knights took {0} casualties but won the battle.";
         public const string BarbariansWon = "The barbarians took {0} casualties but won the battle.";
+        public const string SuccessfullyCreatedKnight = "Successfully added Sir {0} to the collection.";
+        public const string SuccessfullyCreatedBarbarian = "Successfully added Barbarian {0} to the collection.";
+        public const string SuccessfullyAddedWeaponToCollection = "A {0} {1} is added to the collection.";
+        public const string HeroCanParticipateInBattle = "Hero {0} can participate in battle using a {1}.";
     }
 }
