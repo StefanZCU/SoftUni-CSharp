17d735a3ecb49677dbeedf6d68e076ba59c8bfd1
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-12 11:27:31 +0200

    Create HeroRepository.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/18-Apr-2022/Heroes/Repositories/HeroRepository.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Heroes.Models.Contracts;
+using Heroes.Repositories.Contracts;
+
+namespace Heroes.Repositories
+{
+    public class HeroRepository : IRepository<IHero>
+    {
+        private List<IHero> models;
+
+        public HeroRepository()
+        {
+            models = new List<IHero>();
+        }
+
+        public IReadOnlyCollection<IHero> Models => models.AsReadOnly();
+        public void Add(IHero model)
+        {
+            models.Add(model);
+        }
+
+        public bool Remove(IHero model)
+        {
+            var heroToRemove = models.FirstOrDefault(x => x.Name == model.Name);
+            if (heroToRemove == null) return false; 
+            
+            models.Remove(heroToRemove);
+            return true;
+        }
+
+        public IHero FindByName(string name)
+        {
+            return models.FirstOrDefault(x => x.Name == name);
+        }
+    }
+}
