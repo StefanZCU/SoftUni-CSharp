c5351de18e15dab0a58b6ae0a75f120779d4db82
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-12 11:27:26 +0200

    Create OutputMessages.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/18-Apr-2022/Heroes/Utilities/Messages/OutputMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace Heroes.Utilities.Messages
+{
+    public static class OutputMessages
+    {
+        public const string KnightsWon = "The knights took {0} casualties but won the battle.";
+        public const string BarbariansWon = "The barbarians took {0} casualties but won the battle.";
+    }
+}
