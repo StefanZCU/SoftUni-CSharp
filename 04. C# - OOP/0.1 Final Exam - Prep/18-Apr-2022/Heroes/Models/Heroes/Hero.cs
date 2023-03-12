ae3f6e34b70541b1953657fd6919fcdad3eafa01
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-12 11:27:41 +0200

    Create Hero.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/18-Apr-2022/Heroes/Models/Heroes/Hero.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Heroes.Models.Contracts;
+using Heroes.Utilities.Messages;
+
+namespace Heroes.Models.Heroes
+{
+    public abstract class Hero : IHero
+    {
+        private string name;
+        private int health;
+        private int armour;
+        private IWeapon weapon;
+
+        public Hero(string name, int health, int armour)
+        {
+            Name = name;
+            Health = health;
+            Armour = armour;
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.HeroNameCannotBeNull);
+                }
+
+                name = value;
+            }
+        }
+        public int Health
+        {
+            get => health;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.HeroHealthCannotBeNegative);
+                }
+
+                health = value;
+            }
+        }
+        public int Armour
+        {
+            get => armour;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.HeroArmourCannotBeNegative);
+                }
+
+                armour = value;
+            }
+        }
+        public bool IsAlive => Health > 0;
+        public IWeapon Weapon
+        {
+            get => weapon;
+            private set => weapon = value ?? throw new ArgumentException(ExceptionMessages.WeaponIsNull);
+        }
+        public void AddWeapon(IWeapon weapon)
+        {
+            Weapon ??= weapon;
+        }
+        public void TakeDamage(int points)
+        {
+            if (!IsAlive) return;
+            if (Armour - points <= 0)
+            {
+                points -= Armour;
+                Armour = 0;
+                Health -= points;
+            }
+            else
+            {
+                Armour -= points;
+                points = 0;
+            }
+
+            if (Health - points <= 0)
+            {
+                Health = 0;
+            }
+        }
+    }
+}
