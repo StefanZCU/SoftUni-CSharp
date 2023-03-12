83c2f728f02c910fe929dccd9bcde9ff240f0902
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-12 11:27:29 +0200

    Create WeaponRepository.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/18-Apr-2022/Heroes/Repositories/WeaponRepository.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Heroes.Models.Contracts;
+using Heroes.Repositories.Contracts;
+
+namespace Heroes.Repositories
+{
+    public class WeaponRepository : IRepository<IWeapon>
+    {
+        private List<IWeapon> models;
+
+        public WeaponRepository()
+        {
+            models = new List<IWeapon>();
+        }
+
+        public IReadOnlyCollection<IWeapon> Models => models.AsReadOnly();
+        public void Add(IWeapon model)
+        {
+            models.Add(model);
+        }
+
+        public bool Remove(IWeapon model)
+        {
+            var weaponToRemove = models.FirstOrDefault(x => x.Name == model.Name);
+            if (weaponToRemove == null) return false;
+
+            models.Remove(weaponToRemove);
+            return true;
+        }
+
+        public IWeapon FindByName(string name)
+        {
+            return models.FirstOrDefault(x => x.Name == name);
+        }
+    }
+}
