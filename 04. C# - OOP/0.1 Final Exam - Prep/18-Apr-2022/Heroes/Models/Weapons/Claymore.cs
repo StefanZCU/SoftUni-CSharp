b9e27a8a214ba875eb1424852288274f1ad14573
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-12 11:27:36 +0200

    Create Claymore.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/18-Apr-2022/Heroes/Models/Weapons/Claymore.cs
@@ -0,0 +1,15 @@
+﻿namespace Heroes.Models.Weapons
+{
+    public class Claymore : Weapon
+    {
+        public Claymore(string name, int durability) : base(name, durability)
+        {
+        }
+
+        public override int DoDamage()
+        {
+            Durability--;
+            return Durability == 0 ? 0 : 20;
+        }
+    }
+}
