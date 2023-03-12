e6c54d7175bce0a658bd506c9d211482fbc3eb58
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-12 11:27:32 +0200

    Create Weapon.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/18-Apr-2022/Heroes/Models/Weapons/Weapon.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Heroes.Utilities.Messages;
+using Heroes.Models.Contracts;
+
+namespace Heroes.Models.Weapons
+{
+    public abstract class Weapon : IWeapon
+    {
+        private string name;
+        private int durability;
+
+        public Weapon(string name, int durability)
+        {
+            Name = name;
+            Durability = durability;
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.WeaponNameCannotBeNull);
+                }
+
+                name = value;
+            }
+        }
+
+        public int Durability
+        {
+            get => durability;
+            protected set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.WeaponDurabilityCannotBeNegative);
+                }
+
+                durability = value;
+            }
+        }
+
+        public abstract int DoDamage();
+    }
+}
