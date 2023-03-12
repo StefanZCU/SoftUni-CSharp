e745976e07783b8750d99b21ead5fcc0207bb55f
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-12 11:27:28 +0200

    Create ExceptionMessages.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/18-Apr-2022/Heroes/Utilities/Messages/ExceptionMessages.cs
@@ -0,0 +1,12 @@
+﻿namespace Heroes.Utilities.Messages
+{
+    public static class ExceptionMessages
+    {
+        public const string WeaponNameCannotBeNull = "Weapon type cannot be null or empty.";
+        public const string WeaponDurabilityCannotBeNegative = "Durability cannot be below 0.";
+        public const string HeroNameCannotBeNull = "Hero name cannot be null or empty.";
+        public const string HeroHealthCannotBeNegative = "Hero health cannot be below 0.";
+        public const string HeroArmourCannotBeNegative = "Hero armour cannot be below 0.";
+        public const string WeaponIsNull = "Weapon cannot be null.";
+    }
+}
