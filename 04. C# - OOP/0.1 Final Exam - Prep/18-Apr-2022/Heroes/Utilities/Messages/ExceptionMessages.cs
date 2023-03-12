5ff75ca7f284976f03e0fe80fcf50d3b808faa64
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-12 12:53:50 +0200

    Update ExceptionMessages.cs

--- 04. C# - OOP/0.1 Final Exam - Prep/18-Apr-2022/Heroes/Utilities/Messages/ExceptionMessages.cs
+++ 04. C# - OOP/0.1 Final Exam - Prep/18-Apr-2022/Heroes/Utilities/Messages/ExceptionMessages.cs
@@ -8,5 +8,12 @@
         public const string HeroHealthCannotBeNegative = "Hero health cannot be below 0.";
         public const string HeroArmourCannotBeNegative = "Hero armour cannot be below 0.";
         public const string WeaponIsNull = "Weapon cannot be null.";
+        public const string HeroAlreadyExists = "The hero {0} already exists.";
+        public const string InvalidHeroType = "Invalid hero type.";
+        public const string WeaponAlreadyExists = "The weapon {0} already exists.";
+        public const string InvalidWeaponType = "Invalid weapon type.";
+        public const string HeroDoesNotExist = "Hero {0} does not exist.";
+        public const string WeaponDoesNotExist = "Weapon {0} does not exist.";
+        public const string HeroAlreadyHasWeapon = "Hero {0} is well-armed.";
     }
 }
