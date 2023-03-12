0d261a9fa6d71e3c802c59b44588ffb5e0f99b10
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-12 11:27:34 +0200

    Create Mace.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/18-Apr-2022/Heroes/Models/Weapons/Mace.cs
@@ -0,0 +1,15 @@
+﻿namespace Heroes.Models.Weapons
+{
+    public class Mace : Weapon
+    {
+        public Mace(string name, int durability) : base(name, durability)
+        {
+        }
+
+        public override int DoDamage()
+        {
+            Durability--;
+            return Durability == 0 ? 0 : 25;
+        }
+    }
+}
