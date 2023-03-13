2b028a571cc2362f08ea3e8b9f5e56911c4fe3dc
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-13 10:14:21 +0200

    Create RaceRepository.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/09-Apr-2022/Formula1/Repositories/RaceRepository.cs
@@ -0,0 +1,36 @@
+﻿namespace Formula1.Repositories
+{
+    using System.Linq;
+    using System.Collections.Generic;
+
+    using Contracts;
+    using Formula1.Models.Contracts;
+
+    public class RaceRepository: IRepository<IRace>
+    {
+        private List<IRace> models;
+        public RaceRepository()
+        {
+            models = new List<IRace>();
+        }
+
+        public IReadOnlyCollection<IRace> Models => models.AsReadOnly();
+        public void Add(IRace model)
+        {
+            models.Add(model);
+        }
+
+        public bool Remove(IRace model)
+        {
+            var raceToRemove = models.FirstOrDefault(x => x.RaceName == model.RaceName);
+            if (raceToRemove == null) return false;
+            models.Remove(raceToRemove);
+            return true;
+        }
+
+        public IRace FindByName(string name)
+        {
+            return models.FirstOrDefault(x => x.RaceName == name);
+        }
+    }
+}
