91ce181607df4e92b68fadba6e8b43de71a20ca1
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-13 10:13:53 +0200

    Create FormulaOneCar.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/09-Apr-2022/Formula1/Models/FormulaOneCars/FormulaOneCar.cs
@@ -0,0 +1,63 @@
+﻿namespace Formula1.Models.FormulaOneCars
+{
+    using System;
+
+    using Contracts;
+    using Utilities;
+
+    public abstract class FormulaOneCar : IFormulaOneCar
+    {
+        private string model;
+        private int horsePower;
+        private double engineDisplacement;
+
+        public FormulaOneCar(string model, int horsepower, double engineDisplacement)
+        {
+            Model = model;
+            Horsepower = horsepower;
+            EngineDisplacement = engineDisplacement;
+        }
+
+        public string Model
+        {
+            get => model;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 3)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidF1CarModel, value));
+                }
+
+                model = value;
+            }
+        }
+        public int Horsepower
+        {
+            get => horsePower;
+            private set
+            {
+                if (value < 900 || value > 1050)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidF1HorsePower, value));
+                }
+
+                horsePower = value;
+            }
+        }
+        public double EngineDisplacement
+        {
+            get => engineDisplacement;
+            private set
+            {
+                if (value < 1.6 || value > 2.00)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidF1EngineDisplacement, value));
+                }
+
+                engineDisplacement = value;
+            }
+        }
+
+        public double RaceScoreCalculator(int laps) => engineDisplacement / horsePower * laps;
+    }
+}
