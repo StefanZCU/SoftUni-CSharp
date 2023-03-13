d2b1548daf05edc47f882167ceb3dc8e7e67ffee
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-13 10:13:59 +0200

    Create FormulaOneCarRepository.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/09-Apr-2022/Formula1/Repositories/FormulaOneCarRepository.cs
@@ -0,0 +1,36 @@
+﻿namespace Formula1.Repositories
+{
+    using System.Linq;
+    using System.Collections.Generic;
+
+    using Contracts;
+    using Formula1.Models.Contracts;
+
+    public class FormulaOneCarRepository : IRepository<IFormulaOneCar>
+    {
+        private List<IFormulaOneCar> models;
+        public FormulaOneCarRepository()
+        {
+            models = new List<IFormulaOneCar>();
+        }
+        public IReadOnlyCollection<IFormulaOneCar> Models => models.AsReadOnly();
+        public void Add(IFormulaOneCar model)
+        {
+            models.Add(model);
+        }
+
+        public bool Remove(IFormulaOneCar model)
+        {
+            var carToRemove = models.FirstOrDefault(x => x.Model == model.Model);
+            if (carToRemove == null) return false;
+
+            models.Remove(carToRemove);
+            return true;
+        }
+
+        public IFormulaOneCar FindByName(string name)
+        {
+            return models.FirstOrDefault(x => x.Model == name);
+        }
+    }
+}
