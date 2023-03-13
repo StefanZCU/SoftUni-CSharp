4cbbdf6c021b08b680f2e49bbf6d6bc3560fea39
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-13 10:14:01 +0200

    Create PilotRepository.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/09-Apr-2022/Formula1/Repositories/PilotRepository.cs
@@ -0,0 +1,36 @@
+﻿namespace Formula1.Repositories
+{
+    using System.Linq;
+    using System.Collections.Generic;
+
+    using Contracts;
+    using Formula1.Models.Contracts;
+
+    public class PilotRepository : IRepository<IPilot>
+    {
+        private List<IPilot> models;
+        public PilotRepository()
+        {
+            models = new List<IPilot>();
+        }
+
+        public IReadOnlyCollection<IPilot> Models => models.AsReadOnly();
+        public void Add(IPilot model)
+        {
+            models.Add(model);
+        }
+
+        public bool Remove(IPilot model)
+        {
+            var pilotToRemove = models.FirstOrDefault(x => x.FullName == model.FullName);
+            if (pilotToRemove == null) return false;
+            models.Remove(pilotToRemove);
+            return true;
+        }
+
+        public IPilot FindByName(string name)
+        {
+            return models.FirstOrDefault(x => x.FullName == name);
+        }
+    }
+}
