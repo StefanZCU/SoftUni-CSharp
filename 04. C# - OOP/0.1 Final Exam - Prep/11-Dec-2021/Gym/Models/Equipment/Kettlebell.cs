9b4254aaeeb764bf07f600784a22b6d9c752b586
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-14 10:45:26 +0200

    Create Kettlebell.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/11-Dec-2021/Gym/Models/Equipment/Kettlebell.cs
@@ -0,0 +1,12 @@
+﻿namespace Gym.Models.Equipment
+{
+    public class Kettlebell : Equipment
+    {
+        private const double WEIGHT = 10000.0;
+        private const decimal PRICE = 80.0m;
+
+        public Kettlebell() : base(WEIGHT, PRICE)
+        {
+        }
+    }
+}
