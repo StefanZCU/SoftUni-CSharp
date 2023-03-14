c81ca8752412d3ef310901c0ad1391cf8fac2853
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-14 10:45:28 +0200

    Create Equipment.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/11-Dec-2021/Gym/Models/Equipment/Equipment.cs
@@ -0,0 +1,16 @@
+﻿namespace Gym.Models.Equipment
+{
+    using Contracts;
+
+    public abstract class Equipment : IEquipment
+    {
+        protected Equipment(double weight, decimal price)
+        {
+            Weight = weight;
+            Price = price;
+        }
+
+        public double Weight { get; }
+        public decimal Price { get; }
+    }
+}
