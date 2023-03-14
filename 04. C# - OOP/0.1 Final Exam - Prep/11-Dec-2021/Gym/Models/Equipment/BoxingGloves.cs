ef8d0747753192522feb252409fd37a9c7473def
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-14 10:45:31 +0200

    Create BoxingGloves.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/11-Dec-2021/Gym/Models/Equipment/BoxingGloves.cs
@@ -0,0 +1,12 @@
+﻿namespace Gym.Models.Equipment
+{
+    public class BoxingGloves : Equipment
+    {
+        private const double WEIGHT = 227.0;
+        private const decimal PRICE = 120.0m;
+
+        public BoxingGloves() : base(WEIGHT, PRICE)
+        {
+        }
+    }
+}
