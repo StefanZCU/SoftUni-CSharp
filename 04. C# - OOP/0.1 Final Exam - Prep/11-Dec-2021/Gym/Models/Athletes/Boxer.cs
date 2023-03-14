27a3ad5e9d6ca7cd8c931cc302aee3dd0b1f5412
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-14 10:45:35 +0200

    Create Boxer.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/11-Dec-2021/Gym/Models/Athletes/Boxer.cs
@@ -0,0 +1,22 @@
+﻿namespace Gym.Models.Athletes
+{
+    using System;
+
+    using Utilities.Messages;
+
+    public class Boxer : Athlete
+    {
+        private const int INITIAL_STAMINA = 60;
+        public Boxer(string fullName, string motivation, int numberOfMedals) : base(fullName, motivation, numberOfMedals, INITIAL_STAMINA)
+        {
+        }
+
+        public override void Exercise()
+        {
+            Stamina += 15;
+            if (Stamina <= 100) return;
+            Stamina = 100;
+            throw new ArgumentException(ExceptionMessages.InvalidStamina);
+        }
+    }
+}
