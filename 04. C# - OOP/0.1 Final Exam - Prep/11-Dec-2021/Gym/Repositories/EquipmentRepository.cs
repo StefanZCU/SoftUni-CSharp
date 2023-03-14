d5df18c22b1d95111abbaff1a72e95716a5ace11
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-14 10:45:18 +0200

    Create EquipmentRepository.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/11-Dec-2021/Gym/Repositories/EquipmentRepository.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+
+namespace Gym.Repositories
+{
+    using System.Collections.Generic;
+
+    using Contracts;
+    using Gym.Models.Equipment.Contracts;
+
+    public class EquipmentRepository : IRepository<IEquipment>
+    {
+        private List<IEquipment> models;
+
+        public EquipmentRepository()
+        {
+            models = new List<IEquipment>();
+        }
+
+        public IReadOnlyCollection<IEquipment> Models => models.AsReadOnly();
+        public void Add(IEquipment model)
+        {
+            models.Add(model);
+        }
+
+        public bool Remove(IEquipment model)
+        {
+            var equipmentToRemove = models.FirstOrDefault(x => x.GetType().Name == model.GetType().Name);
+            if (equipmentToRemove == null) return false;
+            models.Remove(equipmentToRemove);
+            return true;
+        }
+
+        public IEquipment FindByType(string type)
+        {
+            return models.FirstOrDefault(x => x.GetType().Name == type);
+        }
+    }
+}
