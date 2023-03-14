c20787e13187c503f0992d1b099d38828d916930
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-14 10:45:33 +0200

    Create Weightlifter.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/11-Dec-2021/Gym/Models/Athletes/Weightlifter.cs
@@ -0,0 +1,23 @@
+﻿namespace Gym.Models.Athletes
+{
+    using System;
+
+    using Utilities.Messages;
+
+    public class Weightlifter : Athlete
+    {
+        private const int INITIAL_STAMINA = 50;
+
+        public Weightlifter(string fullName, string motivation, int numberOfMedals) : base(fullName, motivation, numberOfMedals, INITIAL_STAMINA)
+        {
+        }
+
+        public override void Exercise()
+        {
+            Stamina += 15;
+            if (Stamina <= 100) return;
+            Stamina = 100;
+            throw new ArgumentException(ExceptionMessages.InvalidStamina);
+        }
+    }
+}
