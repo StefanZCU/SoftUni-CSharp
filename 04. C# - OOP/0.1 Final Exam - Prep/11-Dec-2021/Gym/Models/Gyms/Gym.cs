564968b7a3bcea39187e9cd54e08d838fdb5255e
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-14 10:45:22 +0200

    Create Gym.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/11-Dec-2021/Gym/Models/Gyms/Gym.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gym.Models.Athletes.Contracts;
+using Gym.Models.Equipment.Contracts;
+using Gym.Models.Gyms.Contracts;
+using Gym.Utilities.Messages;
+
+namespace Gym.Models.Gyms
+{
+    public abstract class Gym : IGym
+    {
+        private string name;
+
+        public Gym(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            Equipment = new List<IEquipment>();
+            Athletes = new List<IAthlete>();
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidGymName);
+                }
+
+                name = value;
+            }
+        }
+        public int Capacity { get; }
+        public ICollection<IEquipment> Equipment { get; }
+        public ICollection<IAthlete> Athletes { get; }
+        public double EquipmentWeight => Equipment.Sum(x => x.Weight);
+        public void AddAthlete(IAthlete athlete)
+        {
+            if (Athletes.Count == Capacity)
+            {
+                throw new InvalidOperationException(ExceptionMessages.NotEnoughSize);
+            }
+            Athletes.Add(athlete);
+        }
+
+        public bool RemoveAthlete(IAthlete athlete)
+        {
+            var athleteToRemove = Athletes.FirstOrDefault(x => x.FullName == athlete.FullName);
+            if (athleteToRemove == null) return false;
+            Athletes.Remove(athleteToRemove);
+            return true;
+        }
+
+        public void AddEquipment(IEquipment equipment)
+        {
+            Equipment.Add(equipment);
+        }
+
+        public void Exercise()
+        {
+            foreach (var athlete in Athletes)
+            {
+                athlete.Exercise();
+            }
+        }
+
+        public string GymInfo()
+        {
+            var sb = new StringBuilder();
+            sb
+                .AppendLine($"{Name} is a {GetType().Name}:")
+                .AppendLine(Athletes.Count > 0
+                    ? $"Athletes: {string.Join(", ", Athletes.Select(x => x.FullName))}"
+                    : "Athletes: No athletes")
+                .AppendLine($"Equipment total count: {Equipment.Count}")
+                .AppendLine($"Equipment total weight: {EquipmentWeight} grams");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
