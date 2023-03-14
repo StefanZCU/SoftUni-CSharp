c0b952aa9e67b08c88fb6c19284770a65d07c38a
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-14 10:45:37 +0200

    Create Athlete.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/11-Dec-2021/Gym/Models/Athletes/Athlete.cs
@@ -0,0 +1,67 @@
+﻿namespace Gym.Models.Athletes
+{
+    using System;
+
+    using Contracts;
+    using Utilities.Messages;
+
+    public abstract class Athlete : IAthlete
+    {
+        private string fullName;
+        private string motivation;
+        private int numberOfMedals;
+
+        public Athlete(string fullName, string motivation, int numberOfMedals, int stamina)
+        {
+            FullName = fullName;
+            Motivation = motivation;
+            NumberOfMedals = numberOfMedals;
+            Stamina = stamina;
+        }
+
+        public string FullName
+        {
+            get => fullName;
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidAthleteName);
+                }
+
+                fullName = value;
+            }
+        }
+
+        public string Motivation
+        {
+            get => motivation;
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidAthleteMotivation);
+                }
+
+                motivation = value;
+            }
+        }
+        public int Stamina { get; protected set; }
+
+        public int NumberOfMedals
+        {
+            get => numberOfMedals;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidAthleteMedals);
+                }
+
+                numberOfMedals = value;
+            }
+        }
+
+        public abstract void Exercise();
+    }
+}
