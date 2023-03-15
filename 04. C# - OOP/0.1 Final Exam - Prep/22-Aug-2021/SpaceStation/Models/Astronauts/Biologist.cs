584932f14c086b12072cf0e5ba6266a4a810455e
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-15 12:32:23 +0200

    Create Biologist.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/22-Aug-2021/SpaceStation/Models/Astronauts/Biologist.cs
@@ -0,0 +1,18 @@
+﻿namespace SpaceStation.Models.Astronauts
+{
+    public class Biologist : Astronaut
+    {
+        private const double INITIAL_OXYGEN = 70;
+
+        public Biologist(string name) : base(name, INITIAL_OXYGEN)
+        {
+        }
+
+        public override void Breath()
+        {
+            Oxygen = Oxygen - 5 < 0 
+                ? 0 
+                : Oxygen - 5;
+        }
+    }
+}
