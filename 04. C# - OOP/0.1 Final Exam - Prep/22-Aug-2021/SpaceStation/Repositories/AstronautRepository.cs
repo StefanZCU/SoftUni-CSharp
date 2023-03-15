eb8aed83fde45c612f99b93422cea13d6223f302
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-15 12:32:10 +0200

    Create AstronautRepository.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/22-Aug-2021/SpaceStation/Repositories/AstronautRepository.cs
@@ -0,0 +1,35 @@
+﻿namespace SpaceStation.Repositories
+{
+    using System.Linq;
+    using System.Collections.Generic;
+
+    using Contracts;
+    using SpaceStation.Models.Astronauts.Contracts;
+
+    public class AstronautRepository : IRepository<IAstronaut>
+    {
+        private List<IAstronaut> models;
+        public AstronautRepository()
+        {
+            models = new List<IAstronaut>();
+        }
+        public IReadOnlyCollection<IAstronaut> Models => models.AsReadOnly();
+        public void Add(IAstronaut model)
+        {
+            models.Add(model);
+        }
+
+        public bool Remove(IAstronaut model)
+        {
+            var astronautToRemove = models.FirstOrDefault(x => x.Name == model.Name);
+            if (astronautToRemove == null) return false;
+            models.Remove(astronautToRemove);
+            return true;
+        }
+
+        public IAstronaut FindByName(string name)
+        {
+            return models.FirstOrDefault(x => x.Name == name);
+        }
+    }
+}
