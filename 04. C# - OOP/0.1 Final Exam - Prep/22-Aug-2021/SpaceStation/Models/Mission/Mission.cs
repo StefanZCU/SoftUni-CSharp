a025f483feb97ea3f752b15d4da6a60e2b6821d5
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-15 13:28:58 +0200

    Update Mission.cs

--- 04. C# - OOP/0.1 Final Exam - Prep/22-Aug-2021/SpaceStation/Models/Mission/Mission.cs
+++ 04. C# - OOP/0.1 Final Exam - Prep/22-Aug-2021/SpaceStation/Models/Mission/Mission.cs
@@ -1,17 +1,21 @@
 ﻿namespace SpaceStation.Models.Mission
 {
-    using System.Linq;
     using System.Collections.Generic;
 
     using Contracts;
-    using SpaceStation.Models.Astronauts.Contracts;
     using SpaceStation.Models.Planets.Contracts;
+    using SpaceStation.Models.Astronauts.Contracts;
 
     public class Mission : IMission
     {
+        public Mission()
+        {
+            
+        }
+
         public void Explore(IPlanet planet, ICollection<IAstronaut> astronauts)
         {
-            foreach (var astronaut in astronauts.Where(x => x.Oxygen > 0))
+            foreach (var astronaut in astronauts)
             {
                 List<string> itemsToRemove = new List<string>();
 
