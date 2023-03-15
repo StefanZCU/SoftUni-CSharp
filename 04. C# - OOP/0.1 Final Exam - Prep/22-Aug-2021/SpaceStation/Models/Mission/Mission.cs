107a4cf3f817888ef078c596166a042b6f8a9a34
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-15 12:32:15 +0200

    Create Mission.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/22-Aug-2021/SpaceStation/Models/Mission/Mission.cs
@@ -0,0 +1,37 @@
+﻿namespace SpaceStation.Models.Mission
+{
+    using System.Linq;
+    using System.Collections.Generic;
+
+    using Contracts;
+    using SpaceStation.Models.Astronauts.Contracts;
+    using SpaceStation.Models.Planets.Contracts;
+
+    public class Mission : IMission
+    {
+        public void Explore(IPlanet planet, ICollection<IAstronaut> astronauts)
+        {
+            foreach (var astronaut in astronauts.Where(x => x.Oxygen > 0))
+            {
+                List<string> itemsToRemove = new List<string>();
+
+                foreach (var planetItem in planet.Items)
+                {
+                    astronaut.Bag.Items.Add(planetItem);
+                    itemsToRemove.Add(planetItem);
+                    astronaut.Breath();
+
+                    if (!astronaut.CanBreath)
+                    {
+                        break;
+                    }
+                }
+
+                foreach (var item in itemsToRemove)
+                {
+                    planet.Items.Remove(item);
+                }
+            }
+        }
+    }
+}
