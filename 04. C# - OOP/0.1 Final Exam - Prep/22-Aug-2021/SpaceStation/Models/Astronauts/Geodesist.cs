059957fe01d75f06c460ac39c5e1d33e434bd344
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-15 12:32:21 +0200

    Create Geodesist.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/22-Aug-2021/SpaceStation/Models/Astronauts/Geodesist.cs
@@ -0,0 +1,11 @@
+﻿namespace SpaceStation.Models.Astronauts
+{
+    public class Geodesist : Astronaut
+    {
+        private const double INITIAL_OXYGEN = 50;
+
+        public Geodesist(string name) : base(name, INITIAL_OXYGEN)
+        {
+        }
+    }
+}
