db669100abd1ab0dcd70f8b403cb234beb73832a
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-15 12:32:17 +0200

    Create Backpack.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/22-Aug-2021/SpaceStation/Models/Bags/Backpack.cs
@@ -0,0 +1,16 @@
+﻿namespace SpaceStation.Models.Bags
+{
+    using System.Collections.Generic;
+
+    using Contracts;
+
+    public class Backpack : IBag
+    {
+        public Backpack()
+        {
+            Items = new List<string>();
+        }
+
+        public ICollection<string> Items { get; }
+    }
+}
