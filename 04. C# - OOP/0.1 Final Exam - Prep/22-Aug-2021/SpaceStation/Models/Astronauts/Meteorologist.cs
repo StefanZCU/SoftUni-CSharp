23d5e7a65a792eb7dafababd7c7dc3acbdeb8ff0
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-15 12:32:19 +0200

    Create Meteorologist.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/22-Aug-2021/SpaceStation/Models/Astronauts/Meteorologist.cs
@@ -0,0 +1,10 @@
+﻿namespace SpaceStation.Models.Astronauts
+{
+    public class Meteorologist : Astronaut
+    {
+        private const double INITIAL_OXYGEN = 90;
+        public Meteorologist(string name) : base(name, INITIAL_OXYGEN)
+        {
+        }
+    }
+}
