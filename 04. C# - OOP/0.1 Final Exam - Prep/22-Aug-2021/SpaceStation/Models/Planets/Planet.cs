81ec68bd765ec0c0a6c29751c8582b92b08942be
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-15 12:32:12 +0200

    Create Planet.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/22-Aug-2021/SpaceStation/Models/Planets/Planet.cs
@@ -0,0 +1,34 @@
+﻿namespace SpaceStation.Models.Planets
+{
+    using System.Collections.Generic;
+    using System;
+
+    using Utilities.Messages;
+    using Contracts;
+
+    public class Planet : IPlanet
+    {
+        private string name;
+
+        public Planet(string name)
+        {
+            Name = name;
+            Items = new List<string>();
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(ExceptionMessages.InvalidPlanetName);
+                }
+                name = value;
+            }
+        }
+
+        public ICollection<string> Items { get; }
+    }
+}
