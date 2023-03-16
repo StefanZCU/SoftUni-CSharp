f662a4eb43050c2a208996325a2a37b5cfa41943
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-16 15:17:33 +0200

    Create Bunny.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/18-Apr-2021/Easter/Models/Bunnies/Bunny.cs
@@ -0,0 +1,44 @@
+﻿namespace Easter.Models.Bunnies
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Contracts;
+    using Utilities.Messages;
+    using Easter.Models.Dyes.Contracts;
+
+    public abstract class Bunny : IBunny
+    {
+        private string name;
+        private List<IDye> dyes;
+
+        public Bunny(string name, int energy)
+        {
+            Name = name;
+            Energy = energy;
+            dyes = new List<IDye>();
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidBunnyName);
+                }
+                name = value;
+            }
+        }
+        public int Energy { get; protected set; }
+        public ICollection<IDye> Dyes => dyes;
+
+        public abstract void Work();
+
+        public void AddDye(IDye dye)
+        {
+            dyes.Add(dye);
+        }
+    }
+}
