3e60a2464f027abc81d524e0ffd0a0f350f439b0
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-16 15:17:21 +0200

    Create Workshop.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/18-Apr-2021/Easter/Models/Workshops/Workshop.cs
@@ -0,0 +1,31 @@
+﻿namespace Easter.Models.Workshops
+{
+    using System.Linq;
+
+    using Contracts;
+    using Easter.Models.Bunnies.Contracts;
+    using Easter.Models.Eggs.Contracts;
+
+    public class Workshop : IWorkshop
+    {
+        public Workshop()
+        {
+            
+        }
+        public void Color(IEgg egg, IBunny bunny)
+        {
+            while (bunny.Energy != 0 && bunny.Dyes.Any(x => !x.IsFinished()))
+            {
+                var currentDye = bunny.Dyes.First(x => !x.IsFinished());
+                bunny.Work();
+                currentDye.Use();
+                egg.GetColored();
+
+                if (egg.IsDone())
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
