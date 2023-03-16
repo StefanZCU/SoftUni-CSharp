c85e84010a0f73a1780e5e7cd29744c6199be36e
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-16 14:03:32 +0200

    Create SuperCar.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/15-Aug-2020/CarRacing/Models/Cars/SuperCar.cs
@@ -0,0 +1,12 @@
+﻿namespace CarRacing.Models.Cars
+{
+    public class SuperCar : Car
+    {
+        private const double FUEL_AVAILABLE = 80;
+        private const double FUEL_CONSUMPTION_PER_RACE = 10;
+
+        public SuperCar(string make, string model, string VIN, int horsePower) : base(make, model, VIN, horsePower, FUEL_AVAILABLE, FUEL_CONSUMPTION_PER_RACE)
+        {
+        }
+    }
+}
