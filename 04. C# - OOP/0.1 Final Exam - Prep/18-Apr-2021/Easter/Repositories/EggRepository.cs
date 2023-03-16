f7e04425f6e10f38183cfd14bf24ddbf8e5e97fd
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-16 15:17:15 +0200

    Create EggRepository.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/18-Apr-2021/Easter/Repositories/EggRepository.cs
@@ -0,0 +1,37 @@
+﻿namespace Easter.Repositories
+{
+    using System.Linq;
+    using System.Collections.Generic;
+
+    using Contracts;
+    using Easter.Models.Eggs.Contracts;
+
+    public class EggRepository : IRepository<IEgg>
+    {
+        private List<IEgg> models;
+
+        public EggRepository()
+        {
+            models = new List<IEgg>();
+        }
+
+        public IReadOnlyCollection<IEgg> Models => models;
+        public void Add(IEgg model)
+        {
+            models.Add(model);
+        }
+
+        public bool Remove(IEgg model)
+        {
+            var eggToRemove = models.FirstOrDefault(x => x.Name == model.Name);
+            if (eggToRemove == null) return false;
+            models.Remove(eggToRemove);
+            return true;
+        }
+
+        public IEgg FindByName(string name)
+        {
+            return models.FirstOrDefault(x => x.Name == name);
+        }
+    }
+}
