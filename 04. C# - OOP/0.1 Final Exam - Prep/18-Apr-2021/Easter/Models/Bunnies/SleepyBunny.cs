79bc5624563ab1074edcb01157de9c4fb06ec157
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-16 15:17:29 +0200

    Create SleepyBunny.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/18-Apr-2021/Easter/Models/Bunnies/SleepyBunny.cs
@@ -0,0 +1,17 @@
+﻿namespace Easter.Models.Bunnies
+{
+    public class SleepyBunny : Bunny
+    {
+        private const int INITIAL_ENERGY = 50;
+        public SleepyBunny(string name) : base(name, INITIAL_ENERGY)
+        {
+        }
+
+        public override void Work()
+        {
+            Energy = Energy - 15 < 0
+                ? 0
+                : Energy - 15;
+        }
+    }
+}
