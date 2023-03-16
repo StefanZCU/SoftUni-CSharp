d7493dcfd82c42da4d5811d3c07655351d9e8a57
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-16 15:17:24 +0200

    Create Egg.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/18-Apr-2021/Easter/Models/Eggs/Egg.cs
@@ -0,0 +1,44 @@
+﻿namespace Easter.Models.Eggs
+{
+    using System;
+
+    using Contracts;
+    using Utilities.Messages;
+
+    public class Egg : IEgg
+    {
+        private string name;
+        private int energyRequired;
+
+        public Egg(string name, int energyRequired)
+        {
+            Name = name;
+            EnergyRequired = energyRequired;
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidEggName);
+                }
+                name = value;
+            }
+        }
+
+        public int EnergyRequired
+        {
+            get => energyRequired;
+            private set => energyRequired = value < 0 ? 0 : value;
+        }
+        public void GetColored()
+        {
+            EnergyRequired -= 10;
+        }
+
+        public bool IsDone() => energyRequired == 0;
+    }
+}
