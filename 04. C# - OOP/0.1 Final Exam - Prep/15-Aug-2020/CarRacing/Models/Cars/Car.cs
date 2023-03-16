932e0ed9fbbbdb6d5e98399cf3235184fce13f9b
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-16 14:03:37 +0200

    Create Car.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/15-Aug-2020/CarRacing/Models/Cars/Car.cs
@@ -0,0 +1,102 @@
+﻿namespace CarRacing.Models.Cars
+{
+    using System;
+
+    using Contracts;
+    using Utilities.Messages;
+
+    public abstract class Car : ICar
+    {
+        private string make;
+        private string model;
+        private string vin;
+        private int horsePower;
+        private double fuelConsumptionPerRace;
+
+        public Car(string make, string model, string VIN, int horsePower, double fuelAvailable, double fuelConsumptionPerRace)
+        {
+            Make = make;
+            Model = model;
+            this.VIN = VIN;
+            HorsePower = horsePower;
+            FuelAvailable = fuelAvailable;
+            FuelConsumptionPerRace = fuelConsumptionPerRace;
+        }
+
+        public string Make
+        {
+            get => make;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidCarMake);
+                }
+                make = value;
+            }
+        }
+
+        public string Model
+        {
+            get => model;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidCarModel);
+                }
+                model = value;
+            }
+        }
+
+        public string VIN
+        {
+            get => vin;
+            private set
+            {
+                if (value.Length != 17)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidCarVIN);
+                }
+                vin = value;
+            }
+        }
+
+        public int HorsePower
+        {
+            get => horsePower;
+            protected set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidCarHorsePower);
+                }
+                horsePower = value;
+            }
+        }
+
+        public double FuelAvailable { get; private set; }
+
+        public double FuelConsumptionPerRace
+        {
+            get => fuelConsumptionPerRace;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidCarFuelConsumption);
+                }
+                fuelConsumptionPerRace = value;
+            }
+        }
+        public virtual void Drive()
+        {
+            if (FuelAvailable - FuelConsumptionPerRace < 0)
+            {
+                FuelAvailable = 0;
+                return;
+            }
+            FuelAvailable -= FuelConsumptionPerRace;
+        }
+    }
+}
