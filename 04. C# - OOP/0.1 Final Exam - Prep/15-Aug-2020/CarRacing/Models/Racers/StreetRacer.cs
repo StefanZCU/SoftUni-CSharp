a0a042e4b419a254308626a6bb0dbceb5cc2b86c
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-16 14:03:22 +0200

    Create StreetRacer.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/15-Aug-2020/CarRacing/Models/Racers/StreetRacer.cs
@@ -0,0 +1,20 @@
+﻿namespace CarRacing.Models.Racers
+{
+    using CarRacing.Models.Cars.Contracts;
+
+    public class StreetRacer : Racer
+    {
+        private const string RACING_BEHAVIOR = "aggressive";
+        private const int DRIVING_EXPERIENCE = 10;
+
+        public StreetRacer(string username, ICar car) : base(username, RACING_BEHAVIOR, DRIVING_EXPERIENCE, car)
+        {
+        }
+
+        public override void Race()
+        {
+            base.Race();
+            DrivingExperience += 5;
+        }
+    }
+}
