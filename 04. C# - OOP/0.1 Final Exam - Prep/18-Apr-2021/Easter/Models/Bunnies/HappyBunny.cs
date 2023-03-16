4e61bb1c4ede9730c628780780bf1d709c82847d
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-16 15:17:31 +0200

    Create HappyBunny.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/18-Apr-2021/Easter/Models/Bunnies/HappyBunny.cs
@@ -0,0 +1,17 @@
+﻿namespace Easter.Models.Bunnies
+{
+    public class HappyBunny : Bunny
+    {
+        private const int INITIAL_ENERGY = 100;
+        public HappyBunny(string name) : base(name, INITIAL_ENERGY)
+        {
+        }
+
+        public override void Work()
+        {
+            Energy = Energy - 10 < 0
+                ? 0
+                : Energy - 10;
+        }
+    }
+}
