5be1f86c834ec3ff492aa7bc43efc9c62e8c06d0
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-16 14:03:25 +0200

    Create ProfessionalRacer.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/15-Aug-2020/CarRacing/Models/Racers/ProfessionalRacer.cs
@@ -0,0 +1,20 @@
+﻿namespace CarRacing.Models.Racers
+{
+    using CarRacing.Models.Cars.Contracts;
+
+    public class ProfessionalRacer : Racer
+    {
+        private const string RACING_BEHAVIOR = "strict";
+        private const int DRIVING_EXPERIENCE = 30;
+
+        public ProfessionalRacer(string username, ICar car) : base(username, RACING_BEHAVIOR, DRIVING_EXPERIENCE, car)
+        {
+        }
+
+        public override void Race()
+        {
+            base.Race();
+            DrivingExperience += 10;
+        }
+    }
+}
