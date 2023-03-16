4b0e27a5bce0d4e66fddd25369e9d6f051d60978
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-16 14:03:23 +0200

    Create Racer.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/15-Aug-2020/CarRacing/Models/Racers/Racer.cs
@@ -0,0 +1,75 @@
+﻿namespace CarRacing.Models.Racers
+{
+    using System;
+
+    using Contracts;
+    using Utilities.Messages;
+    using CarRacing.Models.Cars.Contracts;
+
+    public abstract class Racer : IRacer
+    {
+        private string username;
+        private string racingBehavior;
+        private int drivingExperience;
+        private ICar car;
+
+        public Racer(string username, string racingBehavior, int drivingExperience, ICar car)
+        {
+            Username = username;
+            RacingBehavior = racingBehavior;
+            DrivingExperience = drivingExperience;
+            Car = car;
+        }
+
+        public string Username
+        {
+            get => username;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidRacerName);
+                }
+                username = value;
+            }
+        }
+
+        public string RacingBehavior
+        {
+            get => racingBehavior;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidRacerBehavior);
+                }
+                racingBehavior = value;
+            }
+        }
+
+        public int DrivingExperience
+        {
+            get => drivingExperience;
+            protected set
+            {
+                if (value < 0 || value > 100)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidRacerDrivingExperience);
+                }
+                drivingExperience = value;
+            }
+        }
+
+        public ICar Car
+        {
+            get => car;
+            private set => car = value ?? throw new ArgumentException(ExceptionMessages.InvalidRacerCar);
+        }
+        public virtual void Race()
+        {
+            Car.Drive();
+        }
+
+        public bool IsAvailable() => Car.FuelAvailable >= Car.FuelConsumptionPerRace;
+    }
+}
