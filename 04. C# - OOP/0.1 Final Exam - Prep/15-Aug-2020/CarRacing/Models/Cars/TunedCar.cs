c9ffdc61acb85e213dc54988be921fa982a72799
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-16 14:03:31 +0200

    Create TunedCar.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/15-Aug-2020/CarRacing/Models/Cars/TunedCar.cs
@@ -0,0 +1,20 @@
+﻿namespace CarRacing.Models.Cars
+{
+    using System;
+
+    public class TunedCar : Car
+    {
+        private const double FUEL_AVAILABLE = 65;
+        private const double FUEL_CONSUMPTION_PER_RACE = 7.5;
+
+        public TunedCar(string make, string model, string VIN, int horsePower) : base(make, model, VIN, horsePower, FUEL_AVAILABLE, FUEL_CONSUMPTION_PER_RACE)
+        {
+        }
+
+        public override void Drive()
+        {
+            base.Drive();
+            HorsePower -= Convert.ToInt32(HorsePower * 0.03);
+        }
+    }
+}
