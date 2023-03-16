1f04de1bc57d64a308904f7d0f6d184d533327c4
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-16 15:17:20 +0200

    Create BunnyRepository.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/18-Apr-2021/Easter/Repositories/BunnyRepository.cs
@@ -0,0 +1,37 @@
+﻿namespace Easter.Repositories
+{
+    using System.Linq;
+    using System.Collections.Generic;
+
+    using Contracts;
+    using Easter.Models.Bunnies.Contracts;
+
+    public class BunnyRepository : IRepository<IBunny>
+    {
+        private List<IBunny> models;
+
+        public BunnyRepository()
+        {
+            models = new List<IBunny>();
+        }
+
+        public IReadOnlyCollection<IBunny> Models => models;
+        public void Add(IBunny model)
+        {
+            models.Add(model);
+        }
+
+        public bool Remove(IBunny model)
+        {
+            var bunnyToRemove = models.FirstOrDefault(x => x.Name == model.Name);
+            if (bunnyToRemove == null) return false;
+            models.Remove(bunnyToRemove);
+            return true;
+        }
+
+        public IBunny FindByName(string name)
+        {
+            return models.FirstOrDefault(x => x.Name == name);
+        }
+    }
+}
