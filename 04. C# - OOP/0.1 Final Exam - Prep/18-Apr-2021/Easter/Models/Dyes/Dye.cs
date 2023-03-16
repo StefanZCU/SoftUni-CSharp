d67d284c6985f22d0865ec977265122ceae50717
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-16 15:17:26 +0200

    Create Dye.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/18-Apr-2021/Easter/Models/Dyes/Dye.cs
@@ -0,0 +1,26 @@
+﻿namespace Easter.Models.Dyes
+{
+    using Contracts;
+
+    public class Dye : IDye
+    {
+        private int power;
+
+        public Dye(int power)
+        {
+            Power = power;
+        }
+
+        public int Power
+        {
+            get => power;
+            private set => power = value < 0 ? 0 : value;
+        }
+        public void Use()
+        {
+            Power -= 10;
+        }
+
+        public bool IsFinished() => Power == 0;
+    }
+}
