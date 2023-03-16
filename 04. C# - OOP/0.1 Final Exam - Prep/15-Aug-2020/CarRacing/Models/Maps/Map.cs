215a643b06bfdbc383d230bda407e5133a204813
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-16 14:03:28 +0200

    Create Map.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/15-Aug-2020/CarRacing/Models/Maps/Map.cs
@@ -0,0 +1,44 @@
+﻿namespace CarRacing.Models.Maps
+{
+    using Contracts;
+    using Utilities.Messages;
+    using CarRacing.Models.Racers.Contracts;
+
+    public class Map : IMap
+    {
+        public string StartRace(IRacer racerOne, IRacer racerTwo)
+        {
+            if (!racerOne.IsAvailable() && !racerTwo.IsAvailable())
+            {
+                return OutputMessages.RaceCannotBeCompleted;
+            }
+            if (!racerOne.IsAvailable())
+            {
+                return string.Format(OutputMessages.OneRacerIsNotAvailable, racerTwo.Username, racerOne.Username);
+            }
+            if (!racerTwo.IsAvailable())
+            {
+                return string.Format(OutputMessages.OneRacerIsNotAvailable, racerOne.Username, racerTwo.Username);
+            }
+
+            racerOne.Race();
+            racerTwo.Race();
+
+            var racerOneScore = racerOne.RacingBehavior switch
+            {
+                "strict" => racerOne.Car.HorsePower * racerOne.DrivingExperience * 1.2,
+                _ => racerOne.Car.HorsePower * racerOne.DrivingExperience * 1.1
+            };
+
+            var racerTwoScore = racerTwo.RacingBehavior switch
+            {
+                "strict" => racerTwo.Car.HorsePower * racerTwo.DrivingExperience * 1.2,
+                _ => racerTwo.Car.HorsePower * racerTwo.DrivingExperience * 1.1
+            };
+
+            return racerOneScore > racerTwoScore
+                ? string.Format(OutputMessages.RacerWinsRace, racerOne.Username, racerTwo.Username, racerOne.Username)
+                : string.Format(OutputMessages.RacerWinsRace, racerOne.Username, racerTwo.Username, racerTwo.Username);
+        }
+    }
+}
