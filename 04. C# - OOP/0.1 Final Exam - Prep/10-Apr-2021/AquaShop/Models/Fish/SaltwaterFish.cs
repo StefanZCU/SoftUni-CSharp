12be4556c07aec9277ef90ac2f238264a0f7df16
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-17 11:43:47 +0200

    Create SaltwaterFish.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/10-Apr-2021/AquaShop/Models/Fish/SaltwaterFish.cs
@@ -0,0 +1,15 @@
+﻿namespace AquaShop.Models.Fish
+{
+    public class SaltwaterFish : Fish
+    {
+        public SaltwaterFish(string name, string species, decimal price) : base(name, species, price)
+        {
+            Size = 5;
+        }
+
+        public override void Eat()
+        {
+            Size += 2;
+        }
+    }
+}
