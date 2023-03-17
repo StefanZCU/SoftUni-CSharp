e41ffb349e51a6db8183d60838d49da726297888
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-17 11:43:48 +0200

    Create FreshwaterFish.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/10-Apr-2021/AquaShop/Models/Fish/FreshwaterFish.cs
@@ -0,0 +1,15 @@
+﻿namespace AquaShop.Models.Fish
+{
+    public class FreshwaterFish : Fish
+    {
+        public FreshwaterFish(string name, string species, decimal price) : base(name, species, price)
+        {
+            Size = 3;
+        }
+
+        public override void Eat()
+        {
+            Size += 3;
+        }
+    }
+}
