8da99ba6b86e6e702ab010df50c4b5292b8d66bb
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-17 11:43:43 +0200

    Create DecorationRepository.cs

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/10-Apr-2021/AquaShop/Repositories/DecorationRepository.cs
@@ -0,0 +1,37 @@
+﻿namespace AquaShop.Repositories
+{
+    using System.Linq;
+    using System.Collections.Generic;
+
+    using Contracts;
+    using AquaShop.Models.Decorations.Contracts;
+
+    public class DecorationRepository : IRepository<IDecoration>
+    {
+        private List<IDecoration> models;
+
+        public DecorationRepository()
+        {
+            models = new List<IDecoration>();
+        }
+
+        public IReadOnlyCollection<IDecoration> Models => models;
+        public void Add(IDecoration model)
+        {
+            models.Add(model);
+        }
+
+        public bool Remove(IDecoration model)
+        {
+            var decorationToRemove = models.FirstOrDefault(x => x.GetType().Name == model.GetType().Name);
+            if (decorationToRemove == null) return false;
+            models.Remove(decorationToRemove);
+            return true;
+        }
+
+        public IDecoration FindByType(string type)
+        {
+            return models.FirstOrDefault(x => x.GetType().Name == type);
+        }
+    }
+}
