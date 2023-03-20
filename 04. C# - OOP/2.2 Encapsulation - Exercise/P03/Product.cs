93d2113b6ba6bcaa21cf687ed80ee33bccb1fa06
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-20 13:02:24 +0200

    Added 03. Shopping Spree

---
+++ 04. C# - OOP/2.2 Encapsulation - Exercise/P03/Product.cs
@@ -0,0 +1,44 @@
+﻿namespace P03
+{
+    public class Product
+    {
+        private string name;
+        private int cost;
+
+        public Product(string name, int cost)
+        {
+            Name = name;
+            Cost = cost;
+        }
+
+        public int Cost
+        {
+            get => cost;
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                cost = value;
+            }
+        }
+
+        public string Name
+        {
+            get => name;
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                name = value;
+            }
+        }
+    }
+}
