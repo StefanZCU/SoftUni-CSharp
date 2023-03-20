82c78269d40fe55037ee1570007769b30acb531b
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-20 12:12:22 +0200

    Added 01. Persons

---
+++ 04. C# - OOP/2.1 Encapsulation - Lab/P01/Person.cs
@@ -0,0 +1,39 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+		private string firstName;
+		private string lastName;
+        private int age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public string FirstName
+        {
+            get => firstName;
+			set => firstName = value;
+		}
+
+        public string LastName
+        {
+            get => lastName;
+            set => lastName = value;
+        }
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} is {Age} years old.";
+        }
+    }
+}
