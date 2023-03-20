2dd653f5bfcb97cc09e4b987642dc7c1601c770f
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-20 12:23:03 +0200

    Added 03. Validation

--- 04. C# - OOP/2.1 Encapsulation - Lab/P01/Person.cs
+++ 04. C# - OOP/2.1 Encapsulation - Lab/P01/Person.cs
@@ -18,25 +18,53 @@
         public string FirstName
         {
             get => firstName;
-			set => firstName = value;
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                firstName = value;
+            }
 		}
 
         public string LastName
         {
             get => lastName;
-            set => lastName = value;
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                lastName = value;
+            }
         }
 
         public int Age
         {
             get => age;
-            set => age = value;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                age = value;
+            }
         }
 
         public decimal Salary
         {
             get => salary;
-            set => salary = value;
+            private set
+            {
+                if (value < 650)
+                {
+                    throw new ArgumentException("Salary cannot be less than 650 leva!");
+                }
+                salary = value;
+            }
         }
 
         public void IncreaseSalary(decimal percentage)
--- 04. C# - OOP/2.1 Encapsulation - Lab/P01/StartUp.cs
+++ 04. C# - OOP/2.1 Encapsulation - Lab/P01/StartUp.cs
@@ -8,13 +8,20 @@
             var persons = new List<Person>();
             for (int i = 0; i < lines; i++)
             {
-                var cmdArgs = Console.ReadLine().Split();
-                var person = new Person(cmdArgs[0],
-                    cmdArgs[1],
-                    int.Parse(cmdArgs[2]),
-                    decimal.Parse(cmdArgs[3]));
+                try
+                {
+                    var cmdArgs = Console.ReadLine().Split();
+                    var person = new Person(cmdArgs[0],
+                        cmdArgs[1],
+                        int.Parse(cmdArgs[2]),
+                        decimal.Parse(cmdArgs[3]));
 
-                persons.Add(person);
+                    persons.Add(person);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
             var parcentage = decimal.Parse(Console.ReadLine());
             persons.ForEach(p => p.IncreaseSalary(parcentage));
