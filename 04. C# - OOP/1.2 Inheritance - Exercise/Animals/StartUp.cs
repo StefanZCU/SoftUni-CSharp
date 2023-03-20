5ff1fd3b1d4d248793d79a601251a83072f39c8c
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-20 12:07:03 +0200

    Added 06. Animals

--- 04. C# - OOP/1.2 Inheritance - Exercise/Animals/StartUp.cs
+++ 04. C# - OOP/1.2 Inheritance - Exercise/Animals/StartUp.cs
@@ -1,10 +1,54 @@
 ﻿namespace Animals
 {
+    using System;
+
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+            string animalType;
+            while ((animalType = Console.ReadLine()) != "Beast!")
+            {
+
+                string[] cmdArgs = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                try
+                {
+                    switch (animalType)
+                    {
+                        case "Dog":
+                            Dog dog = new(cmdArgs[0], int.Parse(cmdArgs[1]), cmdArgs[2]);
+                            PrintAnimal(animalType, dog);
+                            break;
+                        case "Frog":
+                            Frog frog = new(cmdArgs[0], int.Parse(cmdArgs[1]), cmdArgs[2]);
+                            PrintAnimal(animalType, frog);
+                            break;
+                        case "Cat":
+                            Cat cat = new(cmdArgs[0], int.Parse(cmdArgs[1]), cmdArgs[2]);
+                            PrintAnimal(animalType, cat);
+                            break;
+                        case "Tomcat":
+                            Tomcat tomcat = new(cmdArgs[0], int.Parse(cmdArgs[1]));
+                            PrintAnimal(animalType, tomcat);
+                            break;
+                        case "Kitten":
+                            Kitten kitten = new(cmdArgs[0], int.Parse(cmdArgs[1]));
+                            PrintAnimal(animalType, kitten);
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        private static void PrintAnimal<T>(string animalType, T animal) where T : Animal
+        {
+            Console.WriteLine(animalType);
+            Console.WriteLine(animal.ToString());
         }
     }
 }
---
+++ 04. C# - OOP/1.2 Inheritance - Exercise/Animals/Tomcat.cs
@@ -0,0 +1,13 @@
+﻿namespace Animals
+{
+    public class Tomcat : Cat
+    {
+        private const string TomcatGender = "Male";
+
+        public Tomcat(string name, int age) : base(name, age, TomcatGender)
+        {
+        }
+
+        public override string ProduceSound() => "MEOW";
+    }
+}
