c4a5af5381fdb9b1212a36fe43a25f87a1798971
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-20 13:36:12 +0200

    Added 02. Dummy Tests

--- 04. C# - OOP/7.1 Unit Testing - Lab/Skeleton.Tests/AxeTests.cs
+++ 04. C# - OOP/7.1 Unit Testing - Lab/Skeleton.Tests/AxeTests.cs
@@ -12,6 +12,7 @@ namespace Skeleton.Tests
             Axe axe = new Axe(10, 10);
             Dummy dummy = new Dummy(10, 10);
 
+
             axe.Attack(dummy);
             Assert.That(axe.DurabilityPoints, Is.EqualTo(9), "Axe Durability doesn't change after attack.");
         }
--- 04. C# - OOP/7.1 Unit Testing - Lab/Skeleton.Tests/DummyTests.cs
+++ 04. C# - OOP/7.1 Unit Testing - Lab/Skeleton.Tests/DummyTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace Skeleton.Tests
 {
@@ -6,9 +7,34 @@ namespace Skeleton.Tests
     public class DummyTests
     {
         [Test]
-        public void Test1()
+        public void DummyLosesHealthIfAttacked()
         {
+            Dummy dummy = new Dummy(15, 10);
+            dummy.TakeAttack(10);
+            Assert.AreEqual(5, dummy.Health);
+        }
 
+        [Test]
+        public void DummyThrowsExceptionIfDead()
+        {
+            Dummy dummy = new Dummy(15, 10);
+            dummy.TakeAttack(15);
+            Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(15));
+        }
+
+        [Test]
+        public void DeadDummyCanGiveXP()
+        {
+            Dummy dummy = new Dummy(15, 10);
+            dummy.TakeAttack(15);
+            Assert.AreEqual(10, dummy.GiveExperience());
+        }
+
+        [Test]
+        public void AliveDummyCannotGiveXP()
+        {
+            Dummy dummy = new Dummy(15, 10);
+            Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
         }
     }
 }
