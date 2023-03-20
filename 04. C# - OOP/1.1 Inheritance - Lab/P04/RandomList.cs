58943392e67346f5f992d19ad6f78c130a91a74a
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-20 11:04:58 +0200

    Added 04. Random List

---
+++ 04. C# - OOP/1.1 Inheritance - Lab/P04/RandomList.cs
@@ -0,0 +1,15 @@
+﻿namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random random;
+
+        public string RandomString()
+        {
+            random = new Random();
+            var stringToRemove = this[random.Next(0, Count)];
+            Remove(stringToRemove);
+            return stringToRemove;
+        }
+    }
+}
