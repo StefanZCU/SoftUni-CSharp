225e75cf64798bdd36b4de8af82accbf49597519
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-20 12:55:25 +0200

    Added 01. Class Box Data

---
+++ 04. C# - OOP/2.2 Encapsulation - Exercise/P01/Box.cs
@@ -0,0 +1,87 @@
+﻿using System.Security.AccessControl;
+using System.Text;
+
+namespace P01
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+        {
+            get => length;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(Length)} cannot be zero or negative.");
+                }
+
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => width;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(Width)} cannot be zero or negative.");
+                }
+
+                width = value;
+            }
+        }
+
+        public double Height
+        {
+            get => height;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"{nameof(Height)} cannot be zero or negative.");
+                }
+
+                height = value;
+            }
+        }
+
+        private double SurfaceArea()
+        {
+            return 2 * length * width + 2 * length * height + 2 * width * height;
+        }
+
+        private double LateralSurfaceArea()
+        {
+           return 2 * length * height + 2 * width * height;
+        }
+
+        private double Volume()
+        {
+            return length * width * height;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb
+                .AppendLine($"Surface Area - {SurfaceArea():F2}")
+                .AppendLine($"Lateral Surface Area - {LateralSurfaceArea():F2}")
+                .AppendLine($"Volume - {Volume():F2}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 04. C# - OOP/2.2 Encapsulation - Exercise/P01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace P01
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var length = double.Parse(Console.ReadLine());
+                var width = double.Parse(Console.ReadLine());
+                var height = double.Parse(Console.ReadLine());
+
+                Box box = new Box(length, width, height);
+                Console.WriteLine(box);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
