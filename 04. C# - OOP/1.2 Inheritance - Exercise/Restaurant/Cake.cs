869822624c8960fd9712195bb7fd0168de027be5
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-20 12:01:20 +0200

    Added 05. Restaurant

---
+++ 04. C# - OOP/1.2 Inheritance - Exercise/Restaurant/Cake.cs
@@ -0,0 +1,13 @@
+﻿namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const double GRAMS = 250;
+        private const double CALORIES = 1000;
+        private const decimal CAKEPRICE = 5m;
+
+        public Cake(string name) : base(name, CAKEPRICE, GRAMS, CALORIES)
+        {
+        }
+    }
+}
---
+++ 04. C# - OOP/1.2 Inheritance - Exercise/Restaurant/Coffee.cs
@@ -0,0 +1,14 @@
+﻿namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double COFFEE_MILLILITERS = 50;
+        private const decimal COFFEE_PRICE = 3.5m;
+        public Coffee(string name, double caffeine) : base(name, COFFEE_PRICE, COFFEE_MILLILITERS)
+        {
+            Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; set; }
+    }
+}
