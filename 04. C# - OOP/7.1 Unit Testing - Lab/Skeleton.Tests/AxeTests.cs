97872d4acc062f9294d22ef4aab30bba062363b9
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-20 13:17:31 +0200

    Added 01. Test Axe

--- 04. C# - OOP/7.1 Unit Testing - Lab/Skeleton.Tests/AxeTests.cs
+++ 04. C# - OOP/7.1 Unit Testing - Lab/Skeleton.Tests/AxeTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System;
 
 namespace Skeleton.Tests
 {
@@ -6,9 +7,21 @@ namespace Skeleton.Tests
     public class AxeTests
     {
         [Test]
-        public void Test1()
+        public void AxeLosesDurabilityAfterAttack()
         {
+            Axe axe = new Axe(10, 10);
+            Dummy dummy = new Dummy(10, 10);
 
+            axe.Attack(dummy);
+            Assert.That(axe.DurabilityPoints, Is.EqualTo(9), "Axe Durability doesn't change after attack.");
+        }
+
+        [Test]
+        public void ThrowsExceptionWithBrokenWeapon()
+        {
+            Axe axe = new Axe(10, 0);
+            Dummy dummy = new Dummy(10, 10);
+            Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
         }
     }
 }
