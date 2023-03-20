60c82db1972a35d40498ad031fb167ef525d5252
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-20 12:29:40 +0200

    Added 04. Team

--- 04. C# - OOP/2.1 Encapsulation - Lab/P01/StartUp.cs
+++ 04. C# - OOP/2.1 Encapsulation - Lab/P01/StartUp.cs
@@ -4,30 +4,7 @@
     {
         static void Main(string[] args)
         {
-            var lines = int.Parse(Console.ReadLine());
-            var persons = new List<Person>();
-            for (int i = 0; i < lines; i++)
-            {
-                try
-                {
-                    var cmdArgs = Console.ReadLine().Split();
-                    var person = new Person(cmdArgs[0],
-                        cmdArgs[1],
-                        int.Parse(cmdArgs[2]),
-                        decimal.Parse(cmdArgs[3]));
-
-                    persons.Add(person);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
-            }
-            var parcentage = decimal.Parse(Console.ReadLine());
-            persons.ForEach(p => p.IncreaseSalary(parcentage));
-            persons.ForEach(p => Console.WriteLine(p.ToString()));
-
-
+            
         }
     }
 }
---
+++ 04. C# - OOP/2.1 Encapsulation - Lab/P01/Team.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.name = name;
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+        }
+
+        public IReadOnlyCollection<Person> FirstTeam => firstTeam;
+        public IReadOnlyCollection<Person> ReserveTeam => reserveTeam;
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb
+                .AppendLine($"First team has {firstTeam.Count} players.")
+                .AppendLine($"Reserve team has {reserveTeam.Count} players.");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
