9c3c39543a5677f4e85c1a2db84049d5b6a92c84
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-20 11:38:14 +0200

    Added 01. Person

---
+++ 04. C# - OOP/1.2 Inheritance - Exercise/Person/Person.cs
@@ -0,0 +1,33 @@
+﻿namespace Person
+{
+    public class Person
+    {
+        private int age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public virtual int Age
+        {
+            get => age;
+
+            set
+            {
+                if (value > 0)
+                {
+                    age = value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
--- 04. C# - OOP/1.2 Inheritance - Exercise/Person/StartUp.cs
+++ 04. C# - OOP/1.2 Inheritance - Exercise/Person/StartUp.cs
@@ -1,10 +1,26 @@
 ﻿namespace Person
 {
+    using System;
+
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            if (age > 15)
+            {
+                Person person = new(name, age);
+
+                Console.WriteLine(person);
+            }
+            else
+            {
+                Child child = new(name, age);
+
+                Console.WriteLine(child);
+            }
         }
     }
 }
