f3259fdbde2dd35d4e6085903d2d6b3b0d315468
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-20 11:13:47 +0200

    Added 05. Stack of Strings

---
+++ 04. C# - OOP/1.1 Inheritance - Lab/P05/StackOfStrings.cs
@@ -0,0 +1,19 @@
+﻿namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty() => this.Count == 0;
+
+        public Stack<string> AddRange()
+        {
+            List<string> newList = new List<string>();
+            newList.AddRange(this);
+            Stack<string> newStack = new Stack<string>();
+            foreach (var VARIABLE in newList)
+            {
+                newStack.Push(VARIABLE);
+            }
+            return newStack;
+        }
+    }
+}
