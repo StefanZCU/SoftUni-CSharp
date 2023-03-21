b7cfd36abf4b3d9fe021601b90e963528cc99469
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-21 10:24:41 +0200

    Added 04. Pizza Calories

---
+++ 04. C# - OOP/2.2 Encapsulation - Exercise/P04/StartUp.cs
@@ -0,0 +1,56 @@
+﻿namespace P04
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var pizza = MakePizza();
+
+                var input = Console.ReadLine();
+
+                while (input != "END")
+                {
+                    var inputParts = input
+                        .Split()
+                        .ToArray();
+
+                    var command = inputParts[0];
+
+                    var toppingType = inputParts[1];
+                    var weight = double.Parse(inputParts[2]);
+
+                    var topping = new Topping(toppingType, weight);
+
+                    pizza.AddTopping(topping);
+
+                    input = Console.ReadLine();
+                }
+
+                Console.WriteLine(pizza);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static Pizza MakePizza()
+        {
+            var pizzaName = Console.ReadLine().Split()[1];
+
+            var doughInfo = Console.ReadLine()
+                .Split()
+                .ToArray();
+
+            var flourType = doughInfo[1];
+            var bakingTechnique = doughInfo[2];
+            var doughWeight = double.Parse(doughInfo[3]);
+
+            var dough = new Dough(flourType, bakingTechnique, doughWeight);
+
+            return new Pizza(pizzaName, dough);
+        }
+    }
+}
