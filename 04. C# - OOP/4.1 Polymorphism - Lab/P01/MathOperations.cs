5bff659c5ce1bdfe61c7790788b8f29157f0fd59
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-22 12:12:16 +0200

    Added 01. Math Operations

---
+++ 04. C# - OOP/4.1 Polymorphism - Lab/P01/MathOperations.cs
@@ -0,0 +1,20 @@
+﻿namespace Operations
+{
+    public class MathOperations
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public double Add(double x, double y, double z)
+        {
+            return x + y + z;
+        }
+
+        public decimal Add(decimal x, decimal y, decimal z)
+        {
+            return x + y + z;
+        }
+    }
+}
