c5231196a2349c33986ac7b8ae46e85dc5eb7e73
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-22 14:17:09 +0200

    Added 01. Stealer

---
+++ 04. C# - OOP/6.1 Reflection and Attributes - Lab/P01/Spy.cs
@@ -0,0 +1,30 @@
+﻿namespace Stealer
+{
+    using System.Text;
+    using System.Reflection;
+
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] fieldNames)
+        {
+            var classType = Type.GetType($"Stealer.{className}");
+
+            var fields = classType
+                .GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic)
+                .Where(x => fieldNames.Contains(x.Name));
+
+            var classInstance = Activator.CreateInstance(classType);
+
+            var fieldsInfo = new StringBuilder();
+
+            fieldsInfo.AppendLine($"Class under investigation: {className}");
+
+            foreach (var field in fields)
+            {
+                fieldsInfo.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return fieldsInfo.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 04. C# - OOP/6.1 Reflection and Attributes - Lab/P01/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace Stealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var spy = new Spy();
+            var result = spy.StealFieldInfo("Hacker", "username", "password");
+
+            Console.WriteLine(result);
+        }
+    }
+}
