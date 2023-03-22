37b5043ff28d546fb2465c1ba427ebdb1db07f5f
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-22 14:07:10 +0200

    Added 05. Play Catch

---
+++ 04. C# - OOP/5.1 Exceptions and Error Handling - Lab/P05/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace P05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] originalArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int exceptionsCaught = 0;
+            while (exceptionsCaught != 3)
+            {
+                string[] cmdArgs = Console.ReadLine().Split();
+
+                string command = cmdArgs[0];
+
+                try
+                {
+                    if (command == "Replace")
+                    {
+                        int index = int.Parse(cmdArgs[1]);
+                        int element = int.Parse(cmdArgs[2]);
+
+                        originalArray[index] = element;
+                    }
+                    else if (command == "Print")
+                    {
+                        int startIndex = int.Parse(cmdArgs[1]);
+                        int endIndex = int.Parse(cmdArgs[2]);
+
+                        int[] arrayCopy = new int[(endIndex - startIndex) + 1];
+                        Array.Copy(originalArray, startIndex, arrayCopy, 0, (endIndex - startIndex) + 1);
+
+                        Console.WriteLine(string.Join(", ", arrayCopy));
+                    }
+                    else if (command == "Show")
+                    {
+                        int index = int.Parse(cmdArgs[1]);
+
+                        Console.WriteLine(originalArray[index]);
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("The variable is not in the correct format!");
+                    exceptionsCaught++;
+                }
+                catch (Exception ex) when (ex is IndexOutOfRangeException or ArgumentOutOfRangeException or ArgumentException)
+                {
+                    Console.WriteLine("The index does not exist!");
+                    exceptionsCaught++;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", originalArray));
+        }
+    }
+}
