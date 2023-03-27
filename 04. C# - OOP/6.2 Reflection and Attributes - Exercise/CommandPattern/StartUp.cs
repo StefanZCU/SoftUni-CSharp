c707d152d5dc48ecc6131077a78e5d1ca6bd5090
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-27 12:05:47 +0300

    Added 6.2 Reflection and Attributes - Exercise skeleton

---
+++ 04. C# - OOP/6.2 Reflection and Attributes - Exercise/CommandPattern/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace CommandPattern
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //ICommandInterpreter command = new CommandInterpreter();
+            //IEngine engine = new Engine(command);
+            //engine.Run();
+        }
+    }
+}
