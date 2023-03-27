710a9eab08d5b9d47846a4a40af493f8ca650e07
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-27 11:46:44 +0300

    Added 04. Collector

---
+++ 04. C# - OOP/6.1 Reflection and Attributes - Lab/P04/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.CollectGettersAndSetters("Stealer.Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
