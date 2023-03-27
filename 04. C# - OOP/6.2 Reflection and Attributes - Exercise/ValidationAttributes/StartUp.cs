458aa92d40b70675f26c1ea53e80d8ad0b89cf95
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-27 13:22:29 +0300

    Added 02. Validation Attributes

--- 04. C# - OOP/6.2 Reflection and Attributes - Exercise/ValidationAttributes/StartUp.cs
+++ 04. C# - OOP/6.2 Reflection and Attributes - Exercise/ValidationAttributes/StartUp.cs
@@ -1,18 +1,17 @@
 ﻿namespace ValidationAttributes
 {
+    using System;
+
+    using Models;
+
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            //var person = new Person
-            // (
-            //     null,
-            //     -1
-            // );
+            Person person = new(null, -1);
 
-            //bool isValidEntity = Validator.IsValid(person);
-
-            //Console.WriteLine(isValidEntity);
+            Console.WriteLine(Validator.IsValid(person));
         }
     }
+
 }
---
+++ 04. C# - OOP/6.2 Reflection and Attributes - Exercise/ValidationAttributes/Validator.cs
@@ -0,0 +1,42 @@
+﻿namespace ValidationAttributes
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Collections.Generic;
+
+    using Attributes;
+
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            Type objType = obj.GetType();
+
+            PropertyInfo[] propertyInfos = objType
+                .GetProperties()
+                .Where(p => p.CustomAttributes
+                    .Any(ca => typeof(MyValidationAttribute)
+                        .IsAssignableFrom(ca.AttributeType)))
+                .ToArray();
+
+            foreach (PropertyInfo propertyInfo in propertyInfos)
+            {
+                IEnumerable<MyValidationAttribute> attributes = propertyInfo
+                    .GetCustomAttributes(true)
+                    .Where(ca => ca is MyValidationAttribute)
+                    .Cast<MyValidationAttribute>();
+
+                foreach (MyValidationAttribute attribute in attributes)
+                {
+                    if (!attribute.IsValid(propertyInfo.GetValue(obj)))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
