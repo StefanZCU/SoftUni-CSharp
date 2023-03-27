24c7091d4c2a43d48c59c6b05d16e092bee86c44
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-03-27 17:47:46 +0300

    Added 04. Fighting Arena

--- 04. C# - OOP/7.2 Unit Testing - Exercise/FightingArena.Tests/ArenaTests.cs
+++ 04. C# - OOP/7.2 Unit Testing - Exercise/FightingArena.Tests/ArenaTests.cs
@@ -1,10 +1,86 @@
-﻿namespace FightingArena.Tests
+﻿namespace Tests
 {
+    using System;
+
+    using FightingArena;
+
     using NUnit.Framework;
 
-    [TestFixture]
     public class ArenaTests
     {
+        private Arena arena;
 
+        [SetUp]
+        public void Setup()
+        {
+            this.arena = new Arena();
+        }
+
+        [Test]
+        public void Test_ArenaConstructorShouldWorkCorrectly()
+        {
+            Assert.IsNotNull(this.arena);
+        }
+
+        [Test]
+        public void Test_ArenaEnrollShouldWorkCorrectly()
+        {
+            var warrior = new Warrior("Stef", 5, 100);
+
+            this.arena.Enroll(warrior);
+
+            Assert.That(this.arena.Warriors, Has.Member(warrior));
+        }
+
+        [Test]
+        public void Test_ArenaEnrollShouldThrowExceptionIfWarriorIsAlreadyEnrolled()
+        {
+            var warrior = new Warrior("Kris", 10, 100);
+
+            this.arena.Enroll(warrior);
+
+            Assert.Throws<InvalidOperationException>(() => this.arena.Enroll(warrior));
+        }
+
+        [Test]
+        public void Test_ArenaCountShouldWorkCorrectly()
+        {
+            var expectedResult = 1;
+
+            var warrior = new Warrior("Pato", 10, 100);
+
+            this.arena.Enroll(warrior);
+
+            Assert.AreEqual(expectedResult, this.arena.Count);
+        }
+
+        [Test]
+        public void Test_ArenaFightMethodShouldWorkCorrectly()
+        {
+            var attacker = new Warrior("Pato", 10, 100);
+            var defender = new Warrior("Krischo", 5, 50);
+
+            this.arena.Enroll(attacker);
+            this.arena.Enroll(defender);
+
+            this.arena.Fight(attacker.Name, defender.Name);
+
+            var expectedAttackerHp = 95;
+            var expectedDefenderHp = 40;
+
+            Assert.AreEqual(expectedAttackerHp, attacker.HP);
+            Assert.AreEqual(expectedDefenderHp, defender.HP);
+        }
+
+        [Test]
+        public void Test_ArenaFightMethodShouldThrowExceptionIfWarriorsAreNotFound()
+        {
+            var attacker = new Warrior("Pato", 10, 100);
+            var defender = new Warrior("Krischo", 5, 5);
+
+            this.arena.Enroll(attacker);
+
+            Assert.Throws<InvalidOperationException>(() => this.arena.Fight(attacker.Name, defender.Name));
+        }
     }
 }
--- 04. C# - OOP/7.2 Unit Testing - Exercise/FightingArena.Tests/WarriorTests.cs
+++ 04. C# - OOP/7.2 Unit Testing - Exercise/FightingArena.Tests/WarriorTests.cs
@@ -1,10 +1,113 @@
-namespace FightingArena.Tests
+namespace Tests
 {
+    using System;
+
+    using FightingArena;
+
     using NUnit.Framework;
 
     [TestFixture]
     public class WarriorTests
     {
+        [Test]
+        public void Test_WarriorConstructorShouldWorksCorrectly()
+        {
+            var warrior = new Warrior("Pesho", 15, 100);
 
+            var expectedName = "Pesho";
+            var expectedDamage = 15;
+            var expectedHp = 100;
+
+            Assert.AreEqual(expectedName, warrior.Name);
+            Assert.AreEqual(expectedDamage, warrior.Damage);
+            Assert.AreEqual(expectedHp, warrior.HP);
+        }
+
+        [Test]
+        public void Test_WarriorNameShouldThrowExceptionIfIsEmpty()
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior("", 25, 50));
+        }
+
+        [Test]
+        public void Test_WarriorNameShouldThrowExceptionIfIsWhitespace()
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior("   ", 25, 50));
+        }
+
+        [Test]
+        public void Test_WarriorDamageShouldThrowExceptionIfIsNegative()
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior("Pato", -10, 100));
+        }
+
+        [Test]
+        public void Test_WarriorDamageShouldThrowExceptionIfIsZero()
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior("Pato", 0, 100));
+        }
+
+        [Test]
+        public void Test_WarriorHpShouldThrowExceptionIfIsNegative()
+        {
+            Assert.Throws<ArgumentException>(() => new Warrior("Pato", 15, -10));
+        }
+
+        [Test]
+        public void Test_AttackMethodShouldWorksCorrectly()
+        {
+            var attacker = new Warrior("Pato", 10, 100);
+            var defender = new Warrior("Stefcho", 5, 90);
+
+            attacker.Attack(defender);
+
+            var expectedAttackerHp = 95;
+            var expectedDefenderHp = 80;
+
+            Assert.AreEqual(expectedAttackerHp, attacker.HP);
+            Assert.AreEqual(expectedDefenderHp, defender.HP);
+        }
+
+        [Test]
+        public void Test_WarriorShouldNotAttackIfHisHpIsEqualOrLessThan30()
+        {
+            var attacker = new Warrior("Pato", 10, 25);
+            var defender = new Warrior("Stefcho", 5, 45);
+
+            Assert.Throws<InvalidOperationException>(() => attacker.Attack(defender));
+        }
+
+        [Test]
+        public void Test_WarriorShouldNotAttackEnemyWith30HpOrLess()
+        {
+            var attacker = new Warrior("Pato", 10, 45);
+            var defender = new Warrior("Stefcho", 5, 25);
+
+            Assert.Throws<InvalidOperationException>(() => attacker.Attack(defender));
+        }
+
+        [Test]
+        public void Test_WarriorShouldNotAttackEnemyWithBiggerDamageThanHisHealth()
+        {
+            var attacker = new Warrior("Pato", 10, 35);
+            var defender = new Warrior("Stefcho", 45, 100);
+
+            Assert.Throws<InvalidOperationException>(() => attacker.Attack(defender));
+        }
+
+        [Test]
+        public void Test_EnemyHpShouldBeSetToZeroIfWarriorDamageIsGreaterThanHisHp()
+        {
+            var attacker = new Warrior("Pato", 50, 100);
+            var defender = new Warrior("Onq", 45, 40);
+
+            attacker.Attack(defender);
+
+            var expectedAttackerHp = 55;
+            var expectedDefenderHp = 0;
+
+            Assert.AreEqual(expectedAttackerHp, attacker.HP);
+            Assert.AreEqual(expectedDefenderHp, defender.HP);
+        }
     }
 }
