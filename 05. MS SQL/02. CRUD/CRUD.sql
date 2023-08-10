3cbf902813ffd67b127a29750e39af0f093261cd
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-08-10 14:45:10 +0300

    Added 20. Last 7 Hired Employees

--- 05. MS SQL/02. CRUD/CRUD.sql
+++ 05. MS SQL/02. CRUD/CRUD.sql
@@ -121,3 +121,12 @@ FROM [Employees]
 
 SELECT TOP 10 * FROM [Projects]
 ORDER BY [StartDate], [Name]
+
+-- 20. Last 7 Hired Employees
+
+SELECT TOP 7
+    [FirstName]
+    ,[LastName]
+    ,[HireDate]
+FROM [Employees]
+ORDER BY [HireDate] DESC
