f9ac6d0a86492d45b3acfec154a2f127cda325d9
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-08-10 12:13:12 +0300

    Added 20. Basic Select All Fields and Order Them

--- 05. MS SQL/01. Databases Introduction/Databases_Introduction.sql
+++ 05. MS SQL/01. Databases Introduction/Databases_Introduction.sql
@@ -519,3 +519,11 @@ SELECT * FROM [Departments]
 
 SELECT * FROM [Employees]
 
+-- 20. Basic Select All Fields and Order Them
+
+SELECT * FROM [Towns] ORDER BY [Name]
+
+SELECT * FROM [Departments] ORDER BY [Name];
+
+SELECT * FROM [Employees] ORDER BY [Salary] DESC
+
