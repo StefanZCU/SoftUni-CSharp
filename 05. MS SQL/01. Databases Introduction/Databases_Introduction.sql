ae9148b2d47abf225a9681ec0e88d2ec19297cd2
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-08-10 12:13:32 +0300

    Added 21. Basic Select Some Fields

--- 05. MS SQL/01. Databases Introduction/Databases_Introduction.sql
+++ 05. MS SQL/01. Databases Introduction/Databases_Introduction.sql
@@ -527,3 +527,11 @@ SELECT * FROM [Departments] ORDER BY [Name];
 
 SELECT * FROM [Employees] ORDER BY [Salary] DESC
 
+-- 21. Basic Select Some Fields
+
+SELECT [Name] FROM [Towns] ORDER BY [Name];
+
+SELECT [Name] FROM [Departments] ORDER BY [Name];
+
+SELECT [FirstName], [LastName], [JobTitle], [Salary] FROM [Employees] ORDER BY [Salary] DESC;
+
