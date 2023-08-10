1dd19470c221b429a8fc0b3df5e1418f13c26b6b
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-08-10 13:55:44 +0300

    Added 09. Find Names of All Employees by Salary in Range

--- 05. MS SQL/02. CRUD/CRUD.sql
+++ 05. MS SQL/02. CRUD/CRUD.sql
@@ -32,3 +32,14 @@ FROM [Employees]
 
 SELECT * FROM [Employees]
 WHERE [JobTitle] = 'Sales Representative'
+
+-- 09. Find Names of All Employees by Salary in Range
+
+SELECT
+    [FirstName]
+    ,[LastName]
+    ,[JobTitle]
+FROM [Employees]
+WHERE
+    [Salary] >= 20000
+    AND [Salary] <= 30000
