9b4606da583a62d0bda60b480dbac3fac77c7fb0
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2023-08-14 18:07:24 +0300

    Added 05-Aug-2023 Skeleton

---
+++ 04. C# - OOP/0.1 Final Exam - Prep/05-Aug-2023/Bank Loan/BankLoan/Utilities/Messages/ExceptionMessages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLoan.Utilities.Messages
+{
+    public static class ExceptionMessages
+    {
+        public const string ClientNameNullOrWhitespace = "Client name cannot be null or empty.";
+        public const string ClientIdNullOrWhitespace = "Client's ID cannot be null or empty.";
+        public const string ClientIncomeBelowZero = "Income cannot be below or equal to 0.";
+        public const string BankNameNullOrWhiteSpace = "Bank name cannot be null or empty.";
+        public const string NotEnoughCapacity = "Not enough capacity for this client.";
+        public const string BankTypeInvalid = "Invalid bank type.";
+        public const string LoanTypeInvalid = "Invalid loan type.";
+        public const string MissingLoanFromType = "Loan of type {0} is missing.";
+        public const string ClientTypeInvalid = "Invalid client type.";
+    }
+}
