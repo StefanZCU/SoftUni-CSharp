6721f91d54b33eafd0cc4e1e79533508a54d15c8
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-09-27 11:04:23 +0300

    Added 01. One-To-One Relationship

--- 05. MS SQL/03. Table Relations/Table_Relations.sql
+++ 05. MS SQL/03. Table Relations/Table_Relations.sql
@@ -1,10 +1,12 @@
+-- 01. One-To-One Relationship
+
 CREATE TABLE Passports(
     [PassportId] INT PRIMARY KEY IDENTITY(101, 1) NOT NULL,
     [PassportNumber] VARCHAR(20) NOT NULL
 )
 
 CREATE TABLE Persons(
-    [PersonID] INT PRIMARY KEY IDENTITY (1, 1) NOT NULL,
+    [PersonID] INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
     [FirstName] NVARCHAR(25) NOT NULL,
     [Salary] DECIMAL(18, 2) NOT NULL,
     [PassportID] INT FOREIGN KEY REFERENCES Passports(PassportId) NOT NULL
@@ -15,3 +17,9 @@ VALUES
     ('N34FG21B'),
     ('K65LO4R7'),
     ('ZE657QP2')
+
+INSERT INTO Persons
+VALUES
+    ('Roberto', 43300.00, 102),
+    ('Tom', 56100.00, 103),
+    ('Yana', 60200.00, 101)
