072b8f88799caa2e95f5f0cc6e1d852b2072f263
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-09-27 11:38:04 +0300

    Added 03. Many-To-Many Relationship

--- 05. MS SQL/03. Table Relations/Table_Relations.sql
+++ 05. MS SQL/03. Table Relations/Table_Relations.sql
@@ -53,3 +53,43 @@ VALUES
     ('Model 3', 2),
     ('Nova', 3)
 
+-- 03. Many-To-Many Relationship
+
+CREATE TABLE [Students](
+    [StudentID] INT PRIMARY KEY IDENTITY(1, 1),
+    [Name] VARCHAR(20) NOT NULL
+)
+
+CREATE TABLE [Exams](
+    [ExamID] INT PRIMARY KEY IDENTITY(101, 1),
+    [Name] VARCHAR(20)
+)
+
+CREATE TABLE [StudentsExams](
+    [StudentID] INT FOREIGN KEY REFERENCES Students(StudentID) NOT NULL,
+    [ExamID] INT FOREIGN KEY REFERENCES Exams(ExamID) NOT NULL,
+    CONSTRAINT PK_StudentsExams PRIMARY KEY (StudentID, ExamID)
+)
+
+INSERT INTO [Students]
+VALUES
+	('Mila'),
+	('Toni'),
+	('Ron')
+
+INSERT INTO [Exams]
+VALUES
+	('SpringMVC'),
+	('Neo4j'),
+	('Oracle 11g')
+
+INSERT INTO [StudentsExams]
+VALUES
+	(1, 101),
+	(1, 102),
+	(2, 101),
+	(3, 103),
+	(2, 102),
+	(2, 103)
+
+
