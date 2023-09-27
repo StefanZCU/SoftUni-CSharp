fbd5f2382132b21a4fe25b29c07a2c6960f6b41e
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-09-27 11:41:01 +0300

    Added 04. Self-Referencing

--- 05. MS SQL/03. Table Relations/Table_Relations.sql
+++ 05. MS SQL/03. Table Relations/Table_Relations.sql
@@ -92,4 +92,20 @@ VALUES
 	(2, 102),
 	(2, 103)
 
+-- 04. Self-Referencing
+
+CREATE TABLE [Teachers](
+    [TeacherID] INT PRIMARY KEY IDENTITY(101, 1),
+    [Name] VARCHAR(20) NOT NULL,
+    [ManagerID] INT FOREIGN KEY REFERENCES Teachers(TeacherID)
+)
+
+INSERT INTO [Teachers]
+VALUES
+    ('John', NULL),
+	('Maya', 106),
+	('Silvia', 106),
+	('Ted', 105),
+	('Mark', 101),
+	('Greta', 101)
 
