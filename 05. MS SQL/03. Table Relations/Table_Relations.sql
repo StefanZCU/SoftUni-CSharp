f69120039f4ce031b3ceb00524dbd4f23d5a26a5
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-09-27 11:19:13 +0300

    Added 02. One-To-Many Relationship

--- 05. MS SQL/03. Table Relations/Table_Relations.sql
+++ 05. MS SQL/03. Table Relations/Table_Relations.sql
@@ -23,3 +23,33 @@ VALUES
     ('Roberto', 43300.00, 102),
     ('Tom', 56100.00, 103),
     ('Yana', 60200.00, 101)
+
+-- 02. One-To-Many Relationship
+
+CREATE TABLE [Manufacturers](
+    [ManufacturerID] INT PRIMARY KEY IDENTITY(1, 1),
+    [Name] VARCHAR(20) NOT NULL,
+    [EstablishedOn] DATETIME2 NOT NULL
+)
+
+CREATE TABLE [Models](
+    [ModelID] INT PRIMARY KEY IDENTITY(1, 1),
+    [Name] VARCHAR(20) NOT NULL,
+    [ManufacturerID] INT FOREIGN KEY REFERENCES Manufacturers([ManufacturerID]) NOT NULL
+)
+
+INSERT INTO [Manufacturers]
+VALUES
+    ('BMW', '07/03/1916'),
+    ('Tesla', '01/01/2003'),
+    ('Lada', '01/05/1966')
+
+INSERT INTO [Models]
+VALUES
+    ('X1', 1),
+    ('i6', 1),
+    ('Model S', 2),
+    ('Model X', 2),
+    ('Model 3', 2),
+    ('Nova', 3)
+
