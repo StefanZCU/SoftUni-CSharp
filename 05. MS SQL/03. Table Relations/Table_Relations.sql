26bfb667f480be88c3a8bfdd5d8acc1e9a2d56b3
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-09-27 10:59:33 +0300

    Create Table_Relations.sql

---
+++ 05. MS SQL/03. Table Relations/Table_Relations.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Passports(
+    [PassportId] INT PRIMARY KEY IDENTITY(101, 1) NOT NULL,
+    [PassportNumber] VARCHAR(20) NOT NULL
+)
+
+CREATE TABLE Persons(
+    [PersonID] INT PRIMARY KEY IDENTITY (1, 1) NOT NULL,
+    [FirstName] NVARCHAR(25) NOT NULL,
+    [Salary] DECIMAL(18, 2) NOT NULL,
+    [PassportID] INT FOREIGN KEY REFERENCES Passports(PassportId) NOT NULL
+)
+
+INSERT INTO Passports
+VALUES
+    ('N34FG21B'),
+    ('K65LO4R7'),
+    ('ZE657QP2')
