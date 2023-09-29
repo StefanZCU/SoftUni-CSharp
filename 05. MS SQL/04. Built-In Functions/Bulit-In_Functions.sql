3fc8c026b302742978cc9afb1048bc52a838de40
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-09-29 12:59:29 +0300

    Added 07. Find Towns Not Starting With

--- 05. MS SQL/04. Built-In Functions/Bulit-In_Functions.sql
+++ 05. MS SQL/04. Built-In Functions/Bulit-In_Functions.sql
@@ -30,3 +30,9 @@ ORDER BY [Name]
 SELECT * FROM Towns
 WHERE [Name] LIKE '[MKBE]%'
 ORDER BY [Name]
+
+-- 07. Find Towns Not Starting With
+
+SELECT * FROM Towns
+WHERE [Name] NOT LIKE '[RBD]%'
+ORDER BY [Name]
