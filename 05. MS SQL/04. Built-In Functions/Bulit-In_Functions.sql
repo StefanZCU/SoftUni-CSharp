d709b51114b99f49efb1fb45f5fabf550f303c91
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-09-29 15:39:32 +0300

    Added 19. People Table

--- 05. MS SQL/04. Built-In Functions/Bulit-In_Functions.sql
+++ 05. MS SQL/04. Built-In Functions/Bulit-In_Functions.sql
@@ -139,3 +139,26 @@ SELECT [ProductName], [OrderDate],
        DATEADD(DAY, 3, [OrderDate]) AS [Pay Due],
        DATEADD(MONTH, 1, [OrderDate]) AS [Delivery Due]
 FROM Orders
+
+-- 19. People Table
+
+CREATE TABLE [People](
+    Id INT IDENTITY(1,1) PRIMARY KEY
+    , [Name] VARCHAR(50) NOT NULL
+    , Birthdate DATE NOT NULL
+)
+
+INSERT INTO People
+VALUES
+    ('Victor', '2000-12-07'),
+    ('Steven', '1992-09-10'),
+    ('Stephen', '1910-09-19'),
+    ('John', '2010-01-06')
+
+SELECT [Name],
+       DATEDIFF(YEAR, [Birthdate], GETDATE()) AS [Age in Years],
+       DATEDIFF(MONTH, [Birthdate], GETDATE()) AS [Age in Months],
+       DATEDIFF(DAY, [Birthdate], GETDATE()) AS [Age in Days],
+       DATEDIFF(MINUTE, [Birthdate], GETDATE()) AS [Age in Minutes]
+FROM People
+
