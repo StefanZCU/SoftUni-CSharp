ca5d95f3e35c3afed6ecde259396eb711d80fa2e
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-09-29 15:00:05 +0300

    Added 16. Get Users with IP Address Like Pattern

--- 05. MS SQL/04. Built-In Functions/Bulit-In_Functions.sql
+++ 05. MS SQL/04. Built-In Functions/Bulit-In_Functions.sql
@@ -105,3 +105,9 @@ ORDER BY [Start], [Name]
 SELECT [Username], SUBSTRING([Email], CHARINDEX('@', [Email]) + 1, LEN([Email]) - CHARINDEX('@', [Email])) AS [Email Provider]
 FROM Users
 ORDER BY [Email Provider], [Username]
+
+-- 16. Get Users with IP Address Like Pattern
+
+SELECT [Username], [IpAddress] FROM Users
+WHERE [IpAddress] LIKE '___.1_%._%.___'
+ORDER BY [Username]
