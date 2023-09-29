9784ba82311f1a10afbed6fb67804d331b90a098
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-09-29 14:33:38 +0300

    Added 11. Find All Employees with Rank 2

--- 05. MS SQL/04. Built-In Functions/Bulit-In_Functions.sql
+++ 05. MS SQL/04. Built-In Functions/Bulit-In_Functions.sql
@@ -58,3 +58,14 @@ SELECT [EmployeeID]
 FROM Employees
 WHERE [Salary] BETWEEN 10000 AND 50000
 ORDER BY [Salary] DESC
+
+-- 11. Find All Employees with Rank 2
+
+SELECT * FROM (
+    SELECT [EmployeeID], [FirstName], [LastName], [Salary],
+    DENSE_RANK() OVER(PARTITION BY [Salary] ORDER BY [EmployeeID]) AS [Rank]
+    FROM Employees
+)
+AS [Selection]
+WHERE [Salary] BETWEEN 10000 AND 50000 AND [Rank] = 2
+ORDER BY [Salary] DESC
