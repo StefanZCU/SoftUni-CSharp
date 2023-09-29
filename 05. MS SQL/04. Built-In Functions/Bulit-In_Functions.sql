3ec3cbd5b5e3bfe7c60ea3e192cbc7a796068e11
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-09-29 13:06:59 +0300

    Added 09. Length of Last Name

--- 05. MS SQL/04. Built-In Functions/Bulit-In_Functions.sql
+++ 05. MS SQL/04. Built-In Functions/Bulit-In_Functions.sql
@@ -42,3 +42,8 @@ ORDER BY [Name]
 CREATE VIEW V_EmployeesHiredAfter2000 AS
 SELECT [FirstName], [LastName] FROM Employees
 WHERE YEAR(HireDate) > 2000
+
+-- 09. Length of Last Name
+
+SELECT [FirstName], [LastName] FROM Employees
+WHERE [LastName] LIKE '_____';
