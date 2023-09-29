4e775c18e9e48fd997a346233a02568fbc1aeefd
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-09-29 14:45:24 +0300

    Added 13. Mix of Peak and River Names

--- 05. MS SQL/04. Built-In Functions/Bulit-In_Functions.sql
+++ 05. MS SQL/04. Built-In Functions/Bulit-In_Functions.sql
@@ -81,3 +81,11 @@ SELECT [CountryName] AS [Country Name]
 FROM Countries
 WHERE [CountryName] LIKE '%a%a%a%'
 ORDER BY [IsoCode]
+
+-- 13. Mix of Peak and River Names
+
+SELECT [PeakName],  [RiverName],
+       LOWER(CONCAT(SUBSTRING([PeakName], 1, LEN([PeakName]) - 1), [RiverName])) AS [Mix]
+FROM [Peaks], [Rivers]
+WHERE RIGHT([PeakName], 1) = LEFT([RiverName], 1)
+ORDER BY [Mix]
