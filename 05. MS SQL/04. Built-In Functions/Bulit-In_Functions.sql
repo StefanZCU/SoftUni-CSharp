01239ac6b88d10fcde66563acd34c5022d5f7f11
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-09-29 12:42:24 +0300

    Added 02. Find Names of All Employees by Last Name

--- 05. MS SQL/04. Built-In Functions/Bulit-In_Functions.sql
+++ 05. MS SQL/04. Built-In Functions/Bulit-In_Functions.sql
@@ -2,3 +2,8 @@
 
 SELECT [FirstName], [LastName] FROM Employees
 WHERE FirstName LIKE 'Sa%'
+
+-- 02. Find Names of All Employees by Last Name
+
+SELECT [FirstName], [LastName] FROM Employees
+WHERE [LastName] LIKE '%ei%'
