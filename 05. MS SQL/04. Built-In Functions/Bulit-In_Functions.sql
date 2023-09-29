bf6f6937cf275e25bd1a25c8cdc3caf4b8dd733e
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-09-29 15:26:57 +0300

    Added 17. Show All Games with Duration & Part of the Day

--- 05. MS SQL/04. Built-In Functions/Bulit-In_Functions.sql
+++ 05. MS SQL/04. Built-In Functions/Bulit-In_Functions.sql
@@ -111,3 +111,20 @@ ORDER BY [Email Provider], [Username]
 SELECT [Username], [IpAddress] FROM Users
 WHERE [IpAddress] LIKE '___.1_%._%.___'
 ORDER BY [Username]
+
+-- 17. Show All Games with Duration & Part of the Day
+
+SELECT [Name],
+    CASE
+        WHEN DATEPART(HOUR, [Start]) BETWEEN 0 AND 11 THEN 'Morning'
+        WHEN DATEPART(HOUR, [Start]) BETWEEN 12 AND 17 THEN 'Afternoon'
+        WHEN DATEPART(HOUR, [Start]) BETWEEN 18 AND 23 THEN 'Evening'
+    END AS [Part of the Day],
+    CASE
+        WHEN [Duration] BETWEEN 0 AND 3 THEN 'Extra Short'
+        WHEN [Duration] BETWEEN 4 AND 6 THEN 'Short'
+        WHEN [Duration] > 6 THEN 'Long'
+        WHEN [Duration] IS NULL THEN 'Extra Long'
+    END AS [Duration]
+FROM Games
+ORDER BY [Name], [Duration], [Part of the Day]
