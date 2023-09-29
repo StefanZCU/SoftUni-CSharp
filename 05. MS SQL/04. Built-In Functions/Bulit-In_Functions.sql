0dcd11cae39f92d5eed00315d40edf7414d17c83
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-09-29 12:48:25 +0300

    Added 03. Find First Names of All Employees

--- 05. MS SQL/04. Built-In Functions/Bulit-In_Functions.sql
+++ 05. MS SQL/04. Built-In Functions/Bulit-In_Functions.sql
@@ -1,9 +1,15 @@
 -- 01. Find Names of All Employees by First Name
 
 SELECT [FirstName], [LastName] FROM Employees
-WHERE FirstName LIKE 'Sa%'
+WHERE FirstName LIKE 'Sa%';
 
 -- 02. Find Names of All Employees by Last Name
 
 SELECT [FirstName], [LastName] FROM Employees
-WHERE [LastName] LIKE '%ei%'
+WHERE [LastName] LIKE '%ei%';
+
+-- 03. Find First Names of All Employees
+
+SELECT [FirstName] FROM Employees
+WHERE [DepartmentID] = 3 OR [DepartmentID] = 10
+AND YEAR(HireDate) BETWEEN 1995 AND 2005;
