b9d09995ff7b4803883fe0fcc4f0ad20dcb48d29
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-09-29 14:31:08 +0300

    Added 10. Rank Employees by Salary

--- 05. MS SQL/04. Built-In Functions/Bulit-In_Functions.sql
+++ 05. MS SQL/04. Built-In Functions/Bulit-In_Functions.sql
@@ -47,3 +47,14 @@ WHERE YEAR(HireDate) > 2000
 
 SELECT [FirstName], [LastName] FROM Employees
 WHERE [LastName] LIKE '_____';
+
+-- 10. Rank Employees by Salary
+
+SELECT [EmployeeID]
+     , [FirstName]
+     , [LastName]
+     , [Salary]
+     , DENSE_RANK() OVER(PARTITION BY [Salary] ORDER BY [EmployeeID]) AS [Rank]
+FROM Employees
+WHERE [Salary] BETWEEN 10000 AND 50000
+ORDER BY [Salary] DESC
