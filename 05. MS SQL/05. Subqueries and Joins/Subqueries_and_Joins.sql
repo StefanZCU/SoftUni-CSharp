af7952d5d06c566d0c0c8a55670198b19c3b0666
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-09-30 11:29:50 +0300

    Added 02. Addresses with Towns

--- 05. MS SQL/05. Subqueries and Joins/Subqueries_and_Joins.sql
+++ 05. MS SQL/05. Subqueries and Joins/Subqueries_and_Joins.sql
@@ -9,3 +9,15 @@ FROM
     Employees AS e JOIN Addresses AS a ON e.AddressID = a.AddressID
 ORDER BY a.AddressID
 
+-- 02. Addresses with Towns
+
+SELECT TOP 50
+    e.FirstName
+    , e.LastName
+    , t.[Name]
+    , a.AddressText
+FROM
+    Employees AS e
+        JOIN Addresses as a ON e.AddressID = a.AddressID
+        JOIN Towns as t ON a.TownID = t.TownID
+ORDER BY e.FirstName, e.LastName
