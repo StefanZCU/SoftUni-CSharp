c27542cb604258062c1f4387082a305db5a4d115
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-09-30 11:38:48 +0300

    Added 04. Employee Departments

--- 05. MS SQL/05. Subqueries and Joins/Subqueries_and_Joins.sql
+++ 05. MS SQL/05. Subqueries and Joins/Subqueries_and_Joins.sql
@@ -33,3 +33,15 @@ FROM
     Employees AS e JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
 WHERE d.Name = 'Sales'
 ORDER BY e.EmployeeID
+
+-- 04. Employee Departments
+
+SELECT TOP 5
+    e.EmployeeID
+    , e.FirstName
+    , e.Salary
+    , d.Name AS [DepartmentName]
+FROM
+    Employees AS e JOIN Departments AS D ON e.DepartmentID = d.DepartmentID
+WHERE e.Salary > 15000
+ORDER BY d.DepartmentID
