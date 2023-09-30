540d4eb9325bccec9a80870439810bd3f020ee42
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-09-30 11:34:31 +0300

    Added 03. Sales Employees

--- 05. MS SQL/05. Subqueries and Joins/Subqueries_and_Joins.sql
+++ 05. MS SQL/05. Subqueries and Joins/Subqueries_and_Joins.sql
@@ -21,3 +21,15 @@ FROM
         JOIN Addresses as a ON e.AddressID = a.AddressID
         JOIN Towns as t ON a.TownID = t.TownID
 ORDER BY e.FirstName, e.LastName
+
+-- 03. Sales Employees
+
+SELECT
+    e.EmployeeID
+    , e.FirstName
+    , e.LastName
+    , d.Name AS [DepartmentName]
+FROM
+    Employees AS e JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+WHERE d.Name = 'Sales'
+ORDER BY e.EmployeeID
