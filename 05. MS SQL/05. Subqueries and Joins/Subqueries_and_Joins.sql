a8932512183f748bc3e00942e2253cb991a92bf6
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-09-30 11:59:49 +0300

    Added 06. Employees Hired After

--- 05. MS SQL/05. Subqueries and Joins/Subqueries_and_Joins.sql
+++ 05. MS SQL/05. Subqueries and Joins/Subqueries_and_Joins.sql
@@ -56,3 +56,18 @@ FROM
 WHERE ep.EmployeeID IS NULL
 ORDER BY e.EmployeeID
 
+-- 06. Employees Hired After
+
+SELECT
+    e.FirstName
+    , e.LastName
+    , e.HireDate
+    , d.Name AS [DeptName]
+FROM
+    Employees AS e JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+WHERE e.HireDate > '1999-1-1'
+AND d.Name = 'Sales' OR d.Name = 'Finance'
+ORDER BY e.HireDate
+
+-- 07. Employees With Project
+
