8213bb7a5f44d11d564b9c2205dd1eef9856513d
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-09-30 13:39:09 +0300

    Added 09. Employee Manager

--- 05. MS SQL/05. Subqueries and Joins/Subqueries_and_Joins.sql
+++ 05. MS SQL/05. Subqueries and Joins/Subqueries_and_Joins.sql
@@ -94,3 +94,17 @@ FROM
         JOIN EmployeesProjects AS ep ON e.EmployeeID = ep.EmployeeID
         JOIN Projects AS p ON ep.ProjectID = p.ProjectID
 WHERE e.EmployeeID = 24
+
+-- 09. Employee Manager
+
+SELECT
+    e.EmployeeID
+    , e.FirstName
+    , e.ManagerID
+    , e2.FirstName AS ManagerName
+FROM
+    Employees AS e JOIN Employees AS e2 ON e.ManagerID = e2.EmployeeID
+WHERE e.ManagerID IN (3, 7)
+ORDER BY e.EmployeeID
+
+
