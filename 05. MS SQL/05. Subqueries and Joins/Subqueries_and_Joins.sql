1192511f89533a250a4f94f7306e3460d746063c
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-09-30 13:40:40 +0300

    Added 10. Employees Summary

--- 05. MS SQL/05. Subqueries and Joins/Subqueries_and_Joins.sql
+++ 05. MS SQL/05. Subqueries and Joins/Subqueries_and_Joins.sql
@@ -107,4 +107,15 @@ FROM
 WHERE e.ManagerID IN (3, 7)
 ORDER BY e.EmployeeID
 
+-- 10. Employees Summary
 
+SELECT TOP 50
+    e1.EmployeeID
+    , CONCAT(e1.FirstName, ' ', e1.LastName) AS [EmployeeName]
+    , CONCAT(e2.FirstName, ' ', e2.LastName) AS [ManagerName]
+    , d.[Name] AS [DepartmentName]
+FROM
+    Employees AS e1
+        JOIN Employees AS e2 ON e1.ManagerID = e2.EmployeeID
+        JOIN Departments AS d ON e1.DepartmentID = d.DepartmentID
+ORDER BY e1.EmployeeID
