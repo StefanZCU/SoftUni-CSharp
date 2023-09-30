29d93f4c4348c8aa1555dd3ed7c534ddc00128dd
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-09-30 12:08:00 +0300

    Added 07. Employees With Project

--- 05. MS SQL/05. Subqueries and Joins/Subqueries_and_Joins.sql
+++ 05. MS SQL/05. Subqueries and Joins/Subqueries_and_Joins.sql
@@ -71,3 +71,15 @@ ORDER BY e.HireDate
 
 -- 07. Employees With Project
 
+SELECT TOP 5
+    e.EmployeeID
+    , e.FirstName
+    , p.Name AS [ProjectName]
+FROM
+    Employees AS e
+        JOIN EmployeesProjects AS ep ON e.EmployeeID = ep.EmployeeID
+        JOIN Projects AS p ON ep.ProjectID = p.ProjectID
+WHERE p.StartDate > '2002-08-13'
+AND p.EndDate IS NULL
+ORDER BY e.EmployeeID
+
