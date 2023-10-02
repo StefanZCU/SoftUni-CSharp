407404e88205e919a1a76316427475891fe5d79e
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-10-02 12:49:39 +0300

    Added 17. Highest Peak and Longest River by Country

--- 05. MS SQL/05. Subqueries and Joins/Subqueries_and_Joins.sql
+++ 05. MS SQL/05. Subqueries and Joins/Subqueries_and_Joins.sql
@@ -203,5 +203,18 @@ FROM
     LEFT JOIN MountainsCountries AS mc ON c.CountryCode = mc.CountryCode
 WHERE mc.CountryCode IS NULL
 
+-- 17. Highest Peak and Longest River by Country
 
+SELECT TOP 5
+    c.CountryName,
+	MAX(p.Elevation) AS [HighestPeakElevation],
+	MAX(r.Length) AS [LongestRiverLength]
+FROM
+    Countries AS c
+    FULL JOIN MountainsCountries AS mc ON c.CountryCode = mc.CountryCode
+    FULL JOIN Peaks AS p ON mc.MountainId = p.MountainId
+    FULL JOIN CountriesRivers AS cr ON c.CountryCode = cr.CountryCode
+    FULL JOIN Rivers AS r ON cr.RiverId = r.Id
+GROUP BY CountryName
+ORDER BY HighestPeakElevation DESC, LongestRiverLength DESC, CountryName
 
