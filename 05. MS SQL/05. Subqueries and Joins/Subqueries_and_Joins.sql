c78b47fcb25e8cb29e6281c5a65518d4ceb91497
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-10-02 10:19:28 +0300

    Added 12. Highest Peaks in Bulgaria

--- 05. MS SQL/05. Subqueries and Joins/Subqueries_and_Joins.sql
+++ 05. MS SQL/05. Subqueries and Joins/Subqueries_and_Joins.sql
@@ -132,3 +132,19 @@ FROM
 	FROM [Employees] AS [e]
 	GROUP BY e.[DepartmentID]
 ) AS [a]
+
+-- 12. Highest Peaks in Bulgaria
+
+USE Geography
+
+SELECT
+    mc.CountryCode
+    , m.MountainRange
+    , p.PeakName
+    , p.Elevation
+FROM
+    Peaks AS p
+    JOIN MountainsCountries AS mc ON p.MountainId = mc.MountainId
+    JOIN Mountains AS m ON p.MountainId = m.Id
+WHERE mc.CountryCode = 'BG' AND p.Elevation > 2835
+ORDER BY p.Elevation DESC
