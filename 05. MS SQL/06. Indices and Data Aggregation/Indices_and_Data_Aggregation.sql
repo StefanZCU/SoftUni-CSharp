a4a1c10ccdd1c573d2ce5775d9b986a87b2cfcbe
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-10-03 13:35:39 +0300

    Added 18. 3rd Highest Salary

--- 05. MS SQL/06. Indices and Data Aggregation/Indices_and_Data_Aggregation.sql
+++ 05. MS SQL/06. Indices and Data Aggregation/Indices_and_Data_Aggregation.sql
@@ -169,3 +169,16 @@ SELECT
     COUNT(SALARY) AS [Count]
 FROM Employees
 WHERE ManagerID IS NULL
+
+-- 18. 3rd Highest Salary
+
+SELECT [DepartmentID]
+     , MAX([Salary]) AS [ThirdHighestSalary]
+FROM
+    (SELECT [DepartmentID]
+            , [Salary]
+            , DENSE_RANK() OVER (PARTITION BY [DepartmentID] ORDER BY [Salary] DESC ) AS [ThirdHighestSalary]
+    FROM Employees
+    ) AS [sal]
+WHERE ThirdHighestSalary = 3
+GROUP BY [DepartmentID]
