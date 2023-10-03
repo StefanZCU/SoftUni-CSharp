a9ac28373f62e1a32630be9fe10a276ba5d23a07
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-10-03 13:17:33 +0300

    Added 14. Employees Minimum Salaries

--- 05. MS SQL/06. Indices and Data Aggregation/Indices_and_Data_Aggregation.sql
+++ 05. MS SQL/06. Indices and Data Aggregation/Indices_and_Data_Aggregation.sql
@@ -120,3 +120,14 @@ SELECT
 FROM Employees
 GROUP BY DepartmentID
 ORDER BY DepartmentID
+
+-- 14. Employees Minimum Salaries
+
+SELECT
+    DepartmentID
+    , MIN(Salary) AS [MinimumSalary]
+FROM Employees
+WHERE DepartmentID IN (2, 5, 7)
+  AND HireDate > '2000-01-01'
+GROUP BY DepartmentID
+
