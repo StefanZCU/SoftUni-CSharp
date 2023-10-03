bce3d5ee374572cc9b134be9b17b07731634a949
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-10-03 13:01:15 +0300

    Added 09. Age Groups

--- 05. MS SQL/06. Indices and Data Aggregation/Indices_and_Data_Aggregation.sql
+++ 05. MS SQL/06. Indices and Data Aggregation/Indices_and_Data_Aggregation.sql
@@ -61,3 +61,24 @@ SELECT
 FROM WizzardDeposits
 GROUP BY DepositGroup, MagicWandCreator
 ORDER BY MagicWandCreator, DepositGroup
+
+-- 09. Age Groups
+
+SELECT
+    [AgeGroup]
+    , COUNT([AgeGroup]) AS [WizardCount]
+FROM
+(
+    SELECT
+    CASE
+        WHEN [Age] <= 10 THEN '[0-10]'
+        WHEN [Age] BETWEEN 11 AND 20 THEN '[11-20]'
+        WHEN [Age] BETWEEN 21 AND 30 THEN '[21-30]'
+        WHEN [Age] BETWEEN 31 AND 40 THEN '[31-40]'
+        WHEN [Age] BETWEEN 41 AND 50 THEN '[41-50]'
+        WHEN [Age] BETWEEN 51 AND 60 THEN '[51-60]'
+        WHEN [Age] > 60 THEN '[61+]'
+    END AS [AgeGroup]
+FROM WizzardDeposits
+    ) AS w
+GROUP BY [AgeGroup]
