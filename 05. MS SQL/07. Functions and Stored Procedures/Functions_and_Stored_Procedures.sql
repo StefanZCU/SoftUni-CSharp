bfe814c73e0741e96822c43fe8e30277a746a97a
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-10-04 13:14:43 +0300

    Added 08. Delete Employees and Departments

--- 05. MS SQL/07. Functions and Stored Procedures/Functions_and_Stored_Procedures.sql
+++ 05. MS SQL/07. Functions and Stored Procedures/Functions_and_Stored_Procedures.sql
@@ -106,3 +106,40 @@ END
 
 SELECT [dbo].[ufn_IsWordComprised]('ostmiafh', 'Sofia')
 
+-- 08. Delete Employees and Departments
+
+CREATE PROC usp_DeleteEmployeesFromDepartment
+(@departmentId INT)
+AS
+BEGIN
+	ALTER TABLE [Departments]
+	ALTER COLUMN [ManagerID] INT NULL
+
+	DELETE FROM [EmployeesProjects]
+	WHERE [EmployeeID] IN
+	(
+		SELECT [EmployeeID] FROM [Employees]
+		WHERE [DepartmentID] = @departmentId
+	)
+
+	UPDATE [Employees]
+	SET [ManagerID] = NULL
+	WHERE [ManagerID] IN
+	(
+		SELECT [EmployeeID] FROM [Employees]
+		WHERE [DepartmentID] = @departmentId
+	)
+
+	UPDATE [Departments]
+	SET [ManagerID] = NULL
+	WHERE [DepartmentID] = @departmentId
+
+ 	DELETE FROM [Employees]
+	WHERE [DepartmentID] = @departmentId
+
+	DELETE FROM [Departments]
+	WHERE [DepartmentID] = @departmentId
+
+	SELECT COUNT(*) FROM [Employees]
+	WHERE [DepartmentID] = @departmentId
+END
