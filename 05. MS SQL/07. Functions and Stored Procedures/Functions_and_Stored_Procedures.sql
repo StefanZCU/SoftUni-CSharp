ba77b9f3fe1535daf3cfc9dabfe4b839907d2e1c
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-10-04 12:39:03 +0300

    Added 05. Salary Level Function

--- 05. MS SQL/07. Functions and Stored Procedures/Functions_and_Stored_Procedures.sql
+++ 05. MS SQL/07. Functions and Stored Procedures/Functions_and_Stored_Procedures.sql
@@ -54,3 +54,18 @@ BEGIN
 end
 
 EXEC [dbo].[usp_GetEmployeesFromTown] 'Sofia'
+
+-- 05. Salary Level Function
+
+CREATE FUNCTION [ufn_GetSalaryLevel](@salary DECIMAL(18,4))
+RETURNS VARCHAR(20)
+BEGIN
+    RETURN CASE
+        WHEN @salary < 30000 THEN 'Low'
+        WHEN @salary BETWEEN 30000 AND 50000 THEN 'Average'
+        WHEN @salary > 50000 THEN 'High'
+    END
+end
+
+EXEC [dbo].[ufn_GetSalaryLevel] 13500.00
+
