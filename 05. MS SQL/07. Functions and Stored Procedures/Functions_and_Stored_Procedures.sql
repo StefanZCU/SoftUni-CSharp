b7f818bdab3498c068cf3c5daee53bea3fd6709c
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-10-04 13:43:03 +0300

    Added 11. Future Value Function

--- 05. MS SQL/07. Functions and Stored Procedures/Functions_and_Stored_Procedures.sql
+++ 05. MS SQL/07. Functions and Stored Procedures/Functions_and_Stored_Procedures.sql
@@ -182,3 +182,15 @@ BEGIN
 END
 
 EXEC [dbo].[usp_GetHoldersWithBalanceHigherThan] 55555
+
+-- 11. Future Value Function
+
+CREATE FUNCTION [ufn_CalculateFutureValue](@I DECIMAL(18, 4), @R FLOAT, @T INT)
+RETURNS DECIMAL(18, 4)
+AS
+BEGIN
+    RETURN @I * (POWER(1 + @R, @T));
+END
+
+
+
