3d102259a8292f0db8b60c8abb34d71c4da260d1
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-10-04 13:59:36 +0300

    Added 13. Cash in User Games Odd Rows

--- 05. MS SQL/07. Functions and Stored Procedures/Functions_and_Stored_Procedures.sql
+++ 05. MS SQL/07. Functions and Stored Procedures/Functions_and_Stored_Procedures.sql
@@ -211,4 +211,27 @@ END
 
 EXEC [dbo].[usp_CalculateFutureValueForAccount] 1, 0.1
 
--- 
+-- 13. Cash in User Games Odd Rows
+
+USE [Diablo]
+
+GO
+
+CREATE FUNCTION ufn_CashInUsersGames
+(@gameName VARCHAR(50))
+RETURNS TABLE
+AS
+	RETURN SELECT
+	SUM([Cash]) AS [SumCash]
+	FROM
+	(
+		SELECT
+			u.[GameId],
+			u.[Cash],
+			ROW_NUMBER() OVER(ORDER BY u.[Cash] DESC) AS [RowNumber]
+		FROM [UsersGames] AS [u]
+		JOIN [Games] AS [g] ON u.[GameId] = g.[Id]
+		WHERE g.[Name] = @gameName
+		GROUP BY u.[GameId], u.[Cash]
+	) AS [t]
+	WHERE t.[RowNumber] % 2 = 1
