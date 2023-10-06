1466ba9dcbdc510133313be38c7007d50f2d37f3
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-10-06 19:37:48 +0300

    Added 06. Sites with Their Location and Category

--- 05. MS SQL/00.1 Final Exam - Prep/10-Aug-2022/10_Aug_2022.sql
+++ 05. MS SQL/00.1 Final Exam - Prep/10-Aug-2022/10_Aug_2022.sql
@@ -96,3 +96,15 @@ SELECT
     , Nationality
 FROM Tourists
 ORDER BY Nationality, Age DESC, [Name]
+
+-- 06. Sites with Their Location and Category
+
+SELECT
+    s.[Name] AS [Site]
+    , l.[Name] AS [Location]
+    , s.Establishment
+    , c.[Name] AS [Category]
+FROM Sites AS s
+    JOIN Locations AS l ON s.LocationId = l.Id
+    JOIN Categories AS c ON s.CategoryId = c.Id
+ORDER BY Category DESC, Location, Site
