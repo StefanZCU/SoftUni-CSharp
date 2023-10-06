a2bc077ecc325190393f11856262181b034872a3
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-10-06 13:13:26 +0300

    Added 06. Boardgames by Category

--- 05. MS SQL/00.1 Final Exam - Prep/19-Feb-2023/19_Feb_2023.sql
+++ 05. MS SQL/00.1 Final Exam - Prep/19-Feb-2023/19_Feb_2023.sql
@@ -101,3 +101,15 @@ SELECT
     , Rating
 FROM Boardgames
 ORDER BY YearPublished, Name DESC
+
+-- 06. Boardgames by Category
+
+SELECT
+    b.Id
+    , b.[Name]
+    , b.YearPublished
+    , c.Name AS [CategoryName]
+FROM Boardgames AS b
+    JOIN Categories AS c ON b.CategoryId = c.Id
+WHERE c.Name IN ('Strategy Games', 'Wargames')
+ORDER BY b.YearPublished DESC
