3eb2ea6ace3698c0c0baa66e36b538960ed29dec
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-10-06 13:41:20 +0300

    Added 12. Search for Boardgame with Specific Category

--- 05. MS SQL/00.1 Final Exam - Prep/19-Feb-2023/19_Feb_2023.sql
+++ 05. MS SQL/00.1 Final Exam - Prep/19-Feb-2023/19_Feb_2023.sql
@@ -181,3 +181,24 @@ BEGIN
     )
     RETURN @totalNumberBoardgames
 end
+
+-- 12. Search for Boardgame with Specific Category
+
+CREATE PROC [usp_SearchByCategory](@category VARCHAR(50))
+AS
+BEGIN
+    SELECT
+        bg.Name
+        , bg.YearPublished
+        , bg.Rating
+        , c.Name AS [CategoryName]
+        , p.Name AS [PublisherName]
+        , CONCAT(pr.PlayersMin, ' people') AS [MinPlayers]
+        , CONCAT(pr.PlayersMax, ' people') AS [MaxPlayers]
+    FROM Boardgames AS bg
+        JOIN Categories AS c ON bg.CategoryId = c.Id
+        JOIN Publishers AS p ON bg.PublisherId = p.Id
+        JOIN PlayersRanges AS pr ON bg.PlayersRangeId = pr.Id
+    WHERE c.Name = @category
+    ORDER BY PublisherName, YearPublished DESC
+end
