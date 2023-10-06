5ad61f8e55b1b0cf76831447825843483a4dab59
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-10-06 12:53:42 +0300

    Added 04. Delete

--- 05. MS SQL/00.1 Final Exam - Prep/19-Feb-2023/19_Feb_2023.sql
+++ 05. MS SQL/00.1 Final Exam - Prep/19-Feb-2023/19_Feb_2023.sql
@@ -87,3 +87,10 @@ UPDATE Boardgames
 SET [Name] = CONCAT([Name], 'V2')
 WHERE YearPublished > 2019
 
+-- 04. Delete
+
+DELETE FROM CreatorsBoardgames WHERE BoardgameId IN (1,16,31,47)
+DELETE FROM Boardgames WHERE PublisherId IN (1,16)
+DELETE FROM Publishers WHERE AddressId IN (5)
+DELETE FROM Addresses WHERE Town LIKE 'L%'
+
