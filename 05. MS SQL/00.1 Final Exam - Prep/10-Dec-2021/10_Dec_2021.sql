9d1dace9631c37110d456826386bfeb0d6d237d3
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-10-07 15:11:06 +0300

    Added 06. Pilots and Aircraft

--- 05. MS SQL/00.1 Final Exam - Prep/10-Dec-2021/10_Dec_2021.sql
+++ 05. MS SQL/00.1 Final Exam - Prep/10-Dec-2021/10_Dec_2021.sql
@@ -90,3 +90,18 @@ SELECT
     , Condition
 FROM Aircraft
 ORDER BY FlightHours DESC
+
+-- 06. Pilots and Aircraft
+
+SELECT
+    p.FirstName
+    , p.LastName
+    , a.Manufacturer
+    , a.Model
+    , a.FlightHours
+FROM
+    Pilots AS p
+    JOIN PilotsAircraft AS pa on p.Id = pa.PilotId
+    JOIN Aircraft AS a on pa.AircraftId = a.Id
+WHERE a.FlightHours IS NOT NULL AND a.FlightHours <= 304
+ORDER BY a.FlightHours DESC, p.FirstName
