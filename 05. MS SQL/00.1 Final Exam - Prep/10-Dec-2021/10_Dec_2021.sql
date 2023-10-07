a84a13b52941514d47de7bded1fcc4fd217681cc
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-10-07 14:50:39 +0300

    Added 01. DDL

---
+++ 05. MS SQL/00.1 Final Exam - Prep/10-Dec-2021/10_Dec_2021.sql
@@ -0,0 +1,61 @@
+CREATE DATABASE Airport
+
+GO
+
+USE Airport
+
+GO
+
+-- 01. DDL
+
+CREATE TABLE Passengers(
+    [Id] INT PRIMARY KEY IDENTITY(1, 1),
+    [FullName] VARCHAR(100) UNIQUE NOT NULL,
+    [Email] VARCHAR(50) UNIQUE NOT NULL
+)
+
+CREATE TABLE Pilots(
+    [Id] INT PRIMARY KEY IDENTITY(1, 1),
+    [FirstName] VARCHAR(30) UNIQUE NOT NULL,
+    [LastName] VARCHAR(30) UNIQUE NOT NULL,
+    [Age] TINYINT CHECK (Age >= 21 AND AGE <= 62) NOT NULL,
+    [Rating] FLOAT CHECK (Rating >= 0.0 AND Rating <= 10.0)
+)
+
+CREATE TABLE AircraftTypes(
+    [Id] INT PRIMARY KEY IDENTITY(1, 1),
+    [TypeName] VARCHAR(30) UNIQUE NOT NULL
+)
+
+CREATE TABLE Aircraft(
+    [Id] INT PRIMARY KEY IDENTITY(1, 1),
+    [Manufacturer] VARCHAR(25) NOT NULL,
+    [Model] VARCHAR(30) NOT NULL,
+    [Year] INT NOT NULL,
+    [FlightHours] INT,
+    [Condition] CHAR(1) NOT NULL,
+    [TypeId] INT FOREIGN KEY REFERENCES AircraftTypes([Id]) NOT NULL
+)
+
+CREATE TABLE PilotsAircraft(
+    [AircraftId] INT NOT NULL,
+    [PilotId] INT NOT NULL,
+    CONSTRAINT PK_AircraftsPilots PRIMARY KEY (AircraftId, PilotId),
+    CONSTRAINT FK_AircraftsPilots_Aircraft FOREIGN KEY (AircraftId) REFERENCES Aircraft([Id]),
+    CONSTRAINT FK_AircraftsPilots_Pilot FOREIGN KEY (PilotId) REFERENCES Pilots([Id])
+)
+
+CREATE TABLE Airports(
+    [Id] INT PRIMARY KEY IDENTITY(1, 1),
+    [AirportName] VARCHAR(70) UNIQUE NOT NULL,
+    [Country] VARCHAR(100) UNIQUE NOT NULL
+)
+
+CREATE TABLE FlightDestinations(
+    [Id] INT PRIMARY KEY IDENTITY(1, 1),
+    [AirportId] INT FOREIGN KEY REFERENCES Airports([Id]) NOT NULL,
+    [Start] DATETIME NOT NULL,
+    [AircraftId] INT FOREIGN KEY REFERENCES Aircraft([Id]) NOT NULL,
+    [PassengerId] INT FOREIGN KEY REFERENCES Passengers([Id]) NOT NULL,
+    [TicketPrice] DECIMAL(16, 2) NOT NULL DEFAULT 15
+)
