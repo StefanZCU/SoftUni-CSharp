6041ccdc5a044d62d869f3357b85185124c82d91
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-10-07 13:51:04 +0300

    Added 08. Owners, Animals and Cages

--- 05. MS SQL/00.1 Final Exam - Prep/19-Jun-2022/19_Jun_2022.sql
+++ 05. MS SQL/00.1 Final Exam - Prep/19-Jun-2022/19_Jun_2022.sql
@@ -112,3 +112,17 @@ FROM
     JOIN Animals AS a ON o.Id = a.OwnerId
 GROUP BY o.[Name]
 ORDER BY CountOfAnimals DESC, Owner
+
+-- 08. Owners, Animals and Cages
+
+SELECT
+    CONCAT(o.[Name], '-', a.[Name]) AS [OwnersAnimals]
+    , o.PhoneNumber
+    , ac.CageId
+FROM
+    Owners AS o
+    JOIN Animals AS a ON o.Id = a.OwnerId
+    JOIN AnimalTypes AS at ON a.AnimalTypeId = at.Id
+    JOIN AnimalsCages AS ac on a.Id = ac.AnimalId
+WHERE at.AnimalType = 'Mammals'
+ORDER BY o.[Name], a.[Name] DESC
