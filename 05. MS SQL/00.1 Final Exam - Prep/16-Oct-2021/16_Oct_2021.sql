5d4c78ba6b6fcee109c24ab4e6b8ab470321591d
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-10-08 14:37:37 +0300

    Added 02. Insert

--- 05. MS SQL/00.1 Final Exam - Prep/16-Oct-2021/16_Oct_2021.sql
+++ 05. MS SQL/00.1 Final Exam - Prep/16-Oct-2021/16_Oct_2021.sql
@@ -60,3 +60,19 @@ CREATE TABLE ClientsCigars(
     CONSTRAINT FK_ClientsCigars_Client FOREIGN KEY (ClientId) REFERENCES Clients([Id]),
     CONSTRAINT FK_ClientsCigars_Cigar FOREIGN KEY (CigarId) REFERENCES Cigars([Id])
 )
+
+-- 02. Insert
+
+INSERT INTO Cigars (CigarName, BrandId, TastId, SizeId, PriceForSingleCigar, ImageURL)
+VALUES
+    ('COHIBA ROBUSTO', 9, 1, 5, 15.50, 'cohiba-robusto-stick_18.jpg'),
+    ('COHIBA SIGLO I', 9, 1, 10, 410.00, 'cohiba-siglo-i-stick_12.jpg'),
+    ('HOYO DE MONTERREY LE HOYO DU MAIRE', 14, 5, 11, 7.5, 'hoyo-du-maire-stick_17.jpg'),
+    ('HOYO DE MONTERREY LE HOYO DE SAN JUAN', 14, 4, 15, 32.00, 'hoyo-de-san-juan-stick_20.jpg'),
+    ('TRINIDAD COLONIALES', 2, 3, 8, 85.21, 'trinidad-coloniales-stick_30.jpg')
+
+INSERT INTO Addresses (Town, Country, Streat, ZIP)
+VALUES
+    ('Sofia', 'Bulgaria', '18 Bul. Vasil levski', '1000'),
+    ('Athens', 'Greece', '4342 McDonald Avenue', '10435'),
+    ('Zagreb', 'Croatia', '4333 Lauren Drive', '10000')
