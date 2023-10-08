1f63226f506a7821ed38a9a0310f7d923bbbe328
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-10-08 15:21:06 +0300

    Added 12. Search for Cigar with Specific Taste

--- 05. MS SQL/00.1 Final Exam - Prep/16-Oct-2021/16_Oct_2021.sql
+++ 05. MS SQL/00.1 Final Exam - Prep/16-Oct-2021/16_Oct_2021.sql
@@ -194,3 +194,24 @@ BEGIN
         )
     RETURN @countCigars
 end
+
+-- 12. Search for Cigar with Specific Taste
+
+CREATE PROC [usp_SearchByTaste](@taste VARCHAR(20))
+AS
+BEGIN
+    SELECT
+        c.CigarName
+        , CONCAT('$', c.PriceForSingleCigar) AS [Price]
+        , t.TasteType
+        , b.BrandName
+        , CONCAT(s.Length, ' cm') AS [CigarLength]
+        , CONCAT(s.RingRange, ' cm') AS [CigarRingRange]
+    FROM
+        Cigars AS c
+        JOIN Tastes AS t ON c.TastId = t.Id
+        JOIN Brands AS b ON c.BrandId = b.Id
+        JOIN Sizes AS s ON c.SizeId = s.Id
+    WHERE t.TasteType = @taste
+    ORDER BY s.Length, s.RingRange DESC
+end
