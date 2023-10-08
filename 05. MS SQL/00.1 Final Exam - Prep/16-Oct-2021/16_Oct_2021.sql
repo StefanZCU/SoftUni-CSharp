96f3e7d058bfe2b93220f1d7e2ed11b252afdcfc
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-10-08 14:48:56 +0300

    Added 06. Cigars by Taste

--- 05. MS SQL/00.1 Final Exam - Prep/16-Oct-2021/16_Oct_2021.sql
+++ 05. MS SQL/00.1 Final Exam - Prep/16-Oct-2021/16_Oct_2021.sql
@@ -110,3 +110,16 @@ SELECT
     , ImageURL
 FROM Cigars
 ORDER BY PriceForSingleCigar, CigarName DESC
+
+-- 06. Cigars by Taste
+
+SELECT
+    c.Id
+    , c.CigarName
+    , c.PriceForSingleCigar
+    , t.TasteType
+    , t.TasteStrength
+FROM Cigars AS c
+    JOIN Tastes AS t ON c.TastId = t.Id
+WHERE t.TasteType IN ('Earthy', 'Woody')
+ORDER BY c.PriceForSingleCigar DESC
