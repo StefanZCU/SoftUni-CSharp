a65b15752f5131dbc18761c82917127018105929
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-10-08 15:16:17 +0300

    Added 11. Client with Cigars

--- 05. MS SQL/00.1 Final Exam - Prep/16-Oct-2021/16_Oct_2021.sql
+++ 05. MS SQL/00.1 Final Exam - Prep/16-Oct-2021/16_Oct_2021.sql
@@ -179,3 +179,18 @@ FROM
 WHERE cc.CigarId IS NOT NULL
 GROUP BY c.LastName
 ORDER BY CigarLength DESC
+
+-- 11. Client with Cigars
+
+CREATE FUNCTION [udf_ClientWithCigars](@name NVARCHAR(30))
+RETURNS INT
+BEGIN
+    DECLARE @countCigars INT = (
+        SELECT
+            COUNT(*)
+        FROM Clients AS c
+            JOIN ClientsCigars AS cc ON c.Id = cc.ClientId
+        WHERE c.FirstName = @name
+        )
+    RETURN @countCigars
+end
