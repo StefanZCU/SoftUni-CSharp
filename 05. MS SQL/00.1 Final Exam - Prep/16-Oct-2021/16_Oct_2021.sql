3e1d1d4f6d7932c75a12a54f2294dbb6a00c61d6
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-10-08 14:33:07 +0300

    Added 01. DDL

---
+++ 05. MS SQL/00.1 Final Exam - Prep/16-Oct-2021/16_Oct_2021.sql
@@ -0,0 +1,62 @@
+CREATE DATABASE CigarShop
+
+GO
+
+USE CigarShop
+
+GO
+
+-- 01. DDL
+
+CREATE TABLE Sizes(
+    [Id] INT PRIMARY KEY IDENTITY(1, 1),
+    [Length] INT CHECK(Length BETWEEN 10 AND 25) NOT NULL,
+    [RingRange] DECIMAL(2, 1) CHECK(RingRange BETWEEN 1.5 AND 7.5) NOT NULL
+)
+
+CREATE TABLE Tastes(
+    [Id] INT PRIMARY KEY IDENTITY(1, 1),
+    [TasteType] VARCHAR(20) NOT NULL,
+    [TasteStrength] VARCHAR(15) NOT NULL,
+    [ImageURL] NVARCHAR(100) NOT NULL
+)
+
+CREATE TABLE Brands(
+    [Id] INT PRIMARY KEY IDENTITY(1, 1),
+    [BrandName] VARCHAR(30) UNIQUE NOT NULL,
+    [BrandDescription] VARCHAR(MAX)
+)
+
+CREATE TABLE Cigars(
+    [Id] INT PRIMARY KEY IDENTITY(1, 1),
+    [CigarName] VARCHAR(80) NOT NULL,
+    [BrandId] INT FOREIGN KEY REFERENCES Brands([Id]) NOT NULL,
+    [TastId] INT FOREIGN KEY REFERENCES Tastes([Id]) NOT NULL,
+    [SizeId] INT FOREIGN KEY REFERENCES Sizes([Id]) NOT NULL,
+    [PriceForSingleCigar] MONEY NOT NULL,
+    [ImageURL] NVARCHAR(100) NOT NULL
+)
+
+CREATE TABLE Addresses(
+    [Id] INT PRIMARY KEY IDENTITY(1, 1),
+    [Town] VARCHAR(30) NOT NULL,
+    [Country] NVARCHAR(30) NOT NULL,
+    [Streat] NVARCHAR(100) NOT NULL,
+    [ZIP] VARCHAR(20) NOT NULL
+)
+
+CREATE TABLE Clients(
+    [Id] INT PRIMARY KEY IDENTITY(1, 1),
+    [FirstName] NVARCHAR(30) NOT NULL,
+    [LastName] NVARCHAR(30) NOT NULL,
+    [Email] NVARCHAR(50) NOT NULL,
+    [AddressId] INT FOREIGN KEY REFERENCES Addresses([Id]) NOT NULL
+)
+
+CREATE TABLE ClientsCigars(
+    [ClientId] INT NOT NULL,
+    [CigarId] INT NOT NULL,
+    CONSTRAINT PK_ClientsCigars PRIMARY KEY (ClientId, CigarId),
+    CONSTRAINT FK_ClientsCigars_Client FOREIGN KEY (ClientId) REFERENCES Clients([Id]),
+    CONSTRAINT FK_ClientsCigars_Cigar FOREIGN KEY (CigarId) REFERENCES Cigars([Id])
+)
