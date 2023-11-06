f3320f77a7d053ae26df8d1db11c76d1050c0712
Stefan Zhivkov <stefan.r.zhivkov@gmail.com>
2023-11-06 16:22:14 +0200

    Create Config.cs

---
+++ 06. Entity Framework Core/01. ADO.NET/AdoDotNetExercise/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace AdoDotNetExercise
+{
+    internal static class Config
+    {
+        public const string ConnectionString =
+            @"Server=localhost\SQLEXPRESS;Database=MinionsDB;Trusted_Connection=True;TrustServerCertificate=True";
+    }
+}
