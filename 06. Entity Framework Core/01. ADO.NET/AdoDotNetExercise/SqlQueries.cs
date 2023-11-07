543436d4b9a9af04bbe3f95dadd25a59daae05d9
Stefan Zhivkov <stefan.r.zhivkov@gmail.com>
2023-11-07 12:53:56 +0200

    Update SqlQueries.cs

--- 06. Entity Framework Core/01. ADO.NET/AdoDotNetExercise/SqlQueries.cs
+++ 06. Entity Framework Core/01. ADO.NET/AdoDotNetExercise/SqlQueries.cs
@@ -33,5 +33,15 @@
         public const string InsertIntoMinionsTable = "INSERT INTO Minions (Name, Age, TownId) VALUES (@name, @age, @townId)";
         public const string InsertIntoTownsTable = "INSERT INTO Towns (Name) VALUES (@townName)";
 
+
+        public const string ChangeCasesOfTownsTable = @"UPDATE Towns
+                                                           SET Name = UPPER(Name)
+                                                         WHERE CountryCode = (SELECT c.Id FROM Countries AS c WHERE c.Name = @countryName)";
+
+        public const string GetTownsForCountry = @" SELECT t.Name 
+                                                       FROM Towns as t
+                                                       JOIN Countries AS c ON c.Id = t.CountryCode
+                                                      WHERE c.Name = @countryName";
+
     }
 }
