d5c5e73b27ffb3a6b50fd095a700dc9309424b59
Stefan Zhivkov <stefan.r.zhivkov@gmail.com>
2023-11-10 15:55:44 +0200

    Added 02. Database First

---
+++ 06. Entity Framework Core/03. Entity Framework Introduction/SoftUni/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+            Console.WriteLine("connected");
+        }
+    }
+}
