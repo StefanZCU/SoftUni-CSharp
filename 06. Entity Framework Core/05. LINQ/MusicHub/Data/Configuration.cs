d8dc921f39a145c5c8f0c4a5e87a05654d2145ce
Stefan Zhivkov <stefan.r.zhivkov@gmail.com>
2023-11-14 17:03:18 +0200

    Added 01. MusicHub Database

---
+++ 06. Entity Framework Core/05. LINQ/MusicHub/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace MusicHub.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString =
+            @"Server=localhost\SQLEXPRESS;Database=MusicHub;Trusted_Connection=True;TrustServerCertificate=True";
+    }
+}
