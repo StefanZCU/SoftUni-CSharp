2b490eb4a686c283284d37de9b026ce4f8934eab
Stefan Zhivkov <stefan.r.zhivkov@gmail.com>
2023-11-29 17:10:28 +0200

    Added 09. Import Suppliers

---
+++ 06. Entity Framework Core/07. JavaScript Object Notation/02. Car Dealer/CarDealer/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            //Problem 01.
+            //string inputJson = File.ReadAllText(@"../../../Datasets/suppliers.json");
+            //Console.WriteLine(ImportSuppliers(context, inputJson));
+        }
+
+        //Problem 01.
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            List<Supplier> suppliers = JsonConvert.DeserializeObject<List<Supplier>>(inputJson)!;
+            context.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count}.";
+        }
+    }
+}
