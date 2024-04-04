eaf022627616e661088d88dabe33bb997ea3885b
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2024-04-04 12:22:25 +0300

    Initial commit

---
+++ 07. ASP.NET/ASP.NET Fundamentals/TaskBoardApp/TaskBoardApp/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TaskBoardApp.Models;
+
+namespace TaskBoardApp.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ 07. ASP.NET/ASP.NET Fundamentals/TaskBoardApp/TaskBoardApp/Data/DataConstants.cs
@@ -0,0 +1,6 @@
+namespace TaskBoardApp.Data;
+
+public class DataConstants
+{
+    
+}
