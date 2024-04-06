f6fce909e9cb06086bc153a3d97dcd614b0ea92c
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2024-04-06 09:30:47 +0300

    Incomplete skeleton - deleted

--- 07. ASP.NET/ASP.NET Fundamentals/SoftUniTeams (21 Dec 2022)/Contacts/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Contacts.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
