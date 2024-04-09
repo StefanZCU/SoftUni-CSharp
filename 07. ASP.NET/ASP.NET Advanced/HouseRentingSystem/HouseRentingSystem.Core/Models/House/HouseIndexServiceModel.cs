502cb8c6a0bec6b737bac68d21ac6022da839a0a
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2024-04-09 21:22:00 +0300

    Add HouseService with LastThreeHouses method
    A new service named HouseService has been added to the project. Inside this service, a method called LastThreeHouses is implemented. This method is responsible for retrieving the last three houses from the repository. Additionally, a model called HouseIndexServiceModel was created to map the relevant house information.

---
+++ 07. ASP.NET/ASP.NET Advanced/HouseRentingSystem/HouseRentingSystem.Core/Models/House/HouseIndexServiceModel.cs
@@ -0,0 +1,10 @@
+namespace HouseRentingSystem.Core.Models.House;
+
+public class HouseIndexServiceModel
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string ImageUrl { get; set; } = null!;
+}
