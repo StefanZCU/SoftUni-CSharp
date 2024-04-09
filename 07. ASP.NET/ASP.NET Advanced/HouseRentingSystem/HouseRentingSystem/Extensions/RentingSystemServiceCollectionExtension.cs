788245cd53f418260d3d93ca26f392193e3414e9
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2024-04-09 21:22:30 +0300

    Add HouseService and Repository to IServiceCollection
    In the RentingSystemServiceCollectionExtension, the HouseService and Repository were added to the IServiceCollection. This was done to introduce scoped services for HouseService and Repository which would allow service instances to be shared within a single request scope.

--- 07. ASP.NET/ASP.NET Advanced/HouseRentingSystem/HouseRentingSystem/Extensions/RentingSystemServiceCollectionExtension.cs
+++ 07. ASP.NET/ASP.NET Advanced/HouseRentingSystem/HouseRentingSystem/Extensions/RentingSystemServiceCollectionExtension.cs
@@ -1,4 +1,7 @@
+using HouseRentingSystem.Core.Contracts.House;
+using HouseRentingSystem.Core.Services.House;
 using HouseRentingSystem.Infrastructure.Data;
+using HouseRentingSystem.Infrastructure.Data.Common;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +11,7 @@ public static class RentingSystemServiceCollectionExtension
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
+        services.AddScoped<IHouseService, HouseService>();
         return services;
     }
     
@@ -17,6 +21,8 @@ public static class RentingSystemServiceCollectionExtension
 
         services.AddDbContext<HouseRentingDbContext>(options =>
             options.UseSqlServer(connectionString));
+
+        services.AddScoped<IRepository, Repository>();
         
         services.AddDatabaseDeveloperPageExceptionFilter();
         
