df6523da4821bbb663b4ed4c38328f6b53f4380e
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2024-04-09 10:06:33 +0300

    Added SoftUni Teams skeleton

---
+++ 07. ASP.NET/ASP.NET Fundamentals/SoftUni Teams/Contacts/Contacts/Data/ContactsDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contacts.Data;
+
+public class ContactsDbContext : IdentityDbContext
+{
+    public ContactsDbContext(DbContextOptions<ContactsDbContext> options)
+        : base(options)
+    {
+    }
+}
