e6fbfb2c6300876e030d3183e3bf7100bce67ba7
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2024-04-10 10:10:32 +0300

    BecomeAgentModel created

---
+++ 07. ASP.NET/ASP.NET Advanced/HouseRentingSystem/HouseRentingSystem.Core/Constants/MessageConstants.cs
@@ -0,0 +1,8 @@
+namespace HouseRentingSystem.Core.Constants;
+
+public static class MessageConstants
+{
+    public const string RequiredErrorMessage = "The {0} field is required";
+
+    public const string StringFieldLengthErrorMessage = "The {0} field must be between {2} and {1} characters long";
+}
--- 07. ASP.NET/ASP.NET Advanced/HouseRentingSystem/HouseRentingSystem.Core/Models/Agent/BecomeAgentFormModel.cs
+++ 07. ASP.NET/ASP.NET Advanced/HouseRentingSystem/HouseRentingSystem.Core/Models/Agent/BecomeAgentFormModel.cs
@@ -1,6 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem.Core.Constants.MessageConstants;
+using static HouseRentingSystem.Infrastructure.Data.Constants.DataConstants;
+
 namespace HouseRentingSystem.Core.Models.Agent;
 
 public class BecomeAgentFormModel
 {
-    
+    [Required(ErrorMessage = RequiredErrorMessage)]
+    [StringLength(AgentPhoneNumberMaxLength,
+        MinimumLength = AgentPhoneNumberMinLength,
+        ErrorMessage = StringFieldLengthErrorMessage)]
+    [Display(Name = "Phone Number")]
+    [Phone]
+    public string PhoneNumber { get; set; } = null!;
 }
