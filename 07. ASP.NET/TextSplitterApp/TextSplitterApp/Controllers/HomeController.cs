49fee370fb5215ea5f2cd464f32395fa43d7d65e
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2025-10-01 05:35:10 +0300

    Finished simple Text Splitter - Added client-side validation as well

--- 07. ASP.NET/TextSplitterApp/TextSplitterApp/Controllers/HomeController.cs
+++ 07. ASP.NET/TextSplitterApp/TextSplitterApp/Controllers/HomeController.cs
@@ -6,26 +6,19 @@ namespace TextSplitterApp.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+    public IActionResult Index(TextViewModel model) => View(model);
 
-    public IActionResult Index()
+    [HttpPost]
+    public IActionResult Split(TextViewModel model)
     {
-        return View();
-    }
+        var splitTextArray = model
+            .Text
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        
+        model.SplitText = string.Join(Environment.NewLine, splitTextArray);
 
-    public IActionResult Privacy()
-    {
-        return View();
+        return RedirectToAction(nameof(Index), model);
     }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+    
 }
---
+++ 07. ASP.NET/TextSplitterApp/TextSplitterApp/Models/TextViewModel.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TextSplitterApp.Models;
+
+public class TextViewModel
+{
+    [Required]
+    [StringLength(30, MinimumLength = 2)]
+    public string Text { get; set; } = null!;
+
+    public string SplitText { get; set; } = null!;
+    
+}
