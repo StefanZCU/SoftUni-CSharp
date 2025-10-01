14c721f294d726a2bf25c01565028b4b82d59697
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2025-10-01 05:12:26 +0300

    Finished Basic Chat App

---
+++ 07. ASP.NET/ChatApp/ChatApp/Models/Message/ChatViewModel.cs
@@ -0,0 +1,8 @@
+namespace ChatApp.Models.Message;
+
+public class ChatViewModel
+{
+    public MessageViewModel CurrentMessage { get; set; } = null!;
+
+    public List<MessageViewModel> Messages { get; set; } = null!;
+}
---
+++ 07. ASP.NET/ChatApp/ChatApp/Models/Message/MessageViewModel.cs
@@ -0,0 +1,7 @@
+namespace ChatApp.Models.Message;
+
+public class MessageViewModel
+{
+    public string Sender { get; set; } = null!;
+    public string MessageText { get; set; } = null!;
+}
