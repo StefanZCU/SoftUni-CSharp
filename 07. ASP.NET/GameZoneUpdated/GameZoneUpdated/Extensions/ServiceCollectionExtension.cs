78682d49d8908709e61228fa4d4e0a0c72971e94
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2025-10-14 12:14:40 +0300

    Debloated Program.cs by extending the service collection

---
+++ 07. ASP.NET/GameZoneUpdated/GameZoneUpdated/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,40 @@
+using GameZoneUpdated.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        return services;
+    }
+    
+    public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+    {
+        var connectionString = config.GetConnectionString("DefaultConnection") ??
+                               throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        
+        services.AddDbContext<GameZoneDbContext>(options =>
+            options.UseSqlServer(connectionString));
+        
+        services.AddDatabaseDeveloperPageExceptionFilter();
+        
+        return services;
+    }
+    
+    public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+            })
+            .AddEntityFrameworkStores<GameZoneDbContext>();
+        
+        return services;
+    }
+}
