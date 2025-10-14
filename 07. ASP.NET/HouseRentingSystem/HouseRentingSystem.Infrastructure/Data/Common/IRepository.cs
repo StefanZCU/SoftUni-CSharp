2bd4cb4a14fd71ba1c7a51da8bb7859f1a409d81
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2025-10-14 05:00:12 +0300

    Repository added

---
+++ 07. ASP.NET/HouseRentingSystem/HouseRentingSystem.Infrastructure/Data/Common/IRepository.cs
@@ -0,0 +1,8 @@
+namespace HouseRentingSystem.Infrastructure.Data.Common;
+
+public interface IRepository
+{
+    IQueryable<T> All<T>() where T : class;
+    
+    IQueryable<T> AllReadOnly<T>() where T : class;
+}
---
+++ 07. ASP.NET/HouseRentingSystem/HouseRentingSystem.Infrastructure/Data/Common/Repository.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem.Infrastructure.Data.Common;
+
+public class Repository : IRepository
+{
+    private readonly DbContext _context;
+
+    public Repository(HouseRentingDbContext context)
+    {
+        _context = context;
+    }
+
+    private DbSet<T> DbSet<T>() where T : class
+    {
+        return _context.Set<T>();
+    }
+    
+    public IQueryable<T> All<T>() where T : class
+    {
+        return DbSet<T>();
+    }
+
+    public IQueryable<T> AllReadOnly<T>() where T : class
+    {
+        return DbSet<T>()
+            .AsNoTracking();
+    }
+}
