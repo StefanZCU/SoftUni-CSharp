5d1afd4a2cb4fb5aa4a6818cf95aa0c3edb3785d
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2025-10-15 05:18:32 +0300

    AgentService implemented

--- 07. ASP.NET/HouseRentingSystem/HouseRentingSystem.Core/Contracts/AgentServices/IAgentService.cs
+++ 07. ASP.NET/HouseRentingSystem/HouseRentingSystem.Core/Contracts/AgentServices/IAgentService.cs
@@ -2,5 +2,11 @@ namespace HouseRentingSystem.Core.Contracts.AgentServices;
 
 public interface IAgentService
 {
+    Task<bool> ExistsByIdAsync(string userId);
     
+    Task<bool> UserWithPhoneNumberExistsAsync(string phoneNumber);
+    
+    Task<bool> UserHasRentsAsync(string userId);
+    
+    Task CreateAsync(string userId, string phoneNumber);
 }
--- 07. ASP.NET/HouseRentingSystem/HouseRentingSystem.Core/Services/AgentServices/AgentService.cs
+++ 07. ASP.NET/HouseRentingSystem/HouseRentingSystem.Core/Services/AgentServices/AgentService.cs
@@ -1,8 +1,47 @@
 using HouseRentingSystem.Core.Contracts.AgentServices;
+using HouseRentingSystem.Infrastructure.Data.Common;
+using HouseRentingSystem.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace HouseRentingSystem.Core.Services.AgentServices;
 
 public class AgentService : IAgentService
 {
+    private readonly IRepository _repository;
     
+    public AgentService(IRepository repository)
+    {
+        _repository = repository;
+    }
+    
+    public async Task<bool> ExistsByIdAsync(string userId)
+    {
+        return await _repository
+            .AllReadOnly<Agent>()
+            .AnyAsync(a => a.UserId == userId);
+    }
+
+    public async Task<bool> UserWithPhoneNumberExistsAsync(string phoneNumber)
+    {
+        return await _repository
+            .AllReadOnly<Agent>()
+            .AnyAsync(a => a.PhoneNumber == phoneNumber);
+    }
+
+    public async Task<bool> UserHasRentsAsync(string userId)
+    {
+        return await _repository.AllReadOnly<House>().AnyAsync(a => a.RenterId == userId);
+    }
+
+    public async Task CreateAsync(string userId, string phoneNumber)
+    {
+        await _repository.AddAsync(new Agent()
+        {
+            UserId = userId,
+            PhoneNumber = phoneNumber
+        });
+        
+        await _repository.SaveChangesAsync();
+    }
 }
