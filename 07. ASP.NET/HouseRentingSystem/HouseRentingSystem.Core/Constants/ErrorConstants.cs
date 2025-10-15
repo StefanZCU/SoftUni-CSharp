9157e5f5beba85d9769d8ff5e47a5fbc502dd8e9
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2025-10-15 05:35:03 +0300

    BecomeAgentFormModel.cs added

---
+++ 07. ASP.NET/HouseRentingSystem/HouseRentingSystem.Core/Constants/ErrorConstants.cs
@@ -0,0 +1,8 @@
+namespace HouseRentingSystem.Core.Constants;
+
+public static class ErrorConstants
+{
+    public const string RequiredFieldError = "The {0} field is required.";
+    
+    public const string InvalidFieldLengthError = "The {0} field must be between {2} and {1} characters long.";
+}
--- 07. ASP.NET/HouseRentingSystem/HouseRentingSystem.Core/Models/AgentModels/BecomeAgentFormModel.cs
+++ 07. ASP.NET/HouseRentingSystem/HouseRentingSystem.Core/Models/AgentModels/BecomeAgentFormModel.cs
@@ -1,6 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using HouseRentingSystem.Infrastructure.Constants;
+using static HouseRentingSystem.Core.Constants.ErrorConstants;
+
 namespace HouseRentingSystem.Core.Models.AgentModels;
+using static DataConstants;
 
 public class BecomeAgentFormModel
 {
-    
+    [Required(ErrorMessage = RequiredFieldError)]
+    [StringLength(
+        maximumLength:AgentPhoneNumberMaxLength,
+        MinimumLength = AgentPhoneNumberMinLength,
+        ErrorMessage = InvalidFieldLengthError)]
+    [Display(Name = "Phone Number")]
+    [Phone]
+    public string PhoneNumber { get; set; } = null!;
 }
