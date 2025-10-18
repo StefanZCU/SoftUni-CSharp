cbf8c8f92305b267db35d4ee78787feb92d0b156
Stefan Zhivkov <114801161+StefanZCU@users.noreply.github.com>
2025-10-18 05:37:29 +0300

    Added DecimalModelBinder

---
+++ 07. ASP.NET/HouseRentingSystem/HouseRentingSystem/ModelBinders/DecimalModelBinder.cs
@@ -0,0 +1,42 @@
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace HouseRentingSystem.ModelBinders;
+
+public class DecimalModelBinder : IModelBinder
+{
+    public Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+        ValueProviderResult valueResult = bindingContext.ValueProvider
+            .GetValue(bindingContext.ModelName);
+
+
+        if (valueResult != ValueProviderResult.None && !string.IsNullOrEmpty(valueResult.FirstValue))
+        {
+            decimal result = 0m;
+            bool success = false;
+
+            try
+            {
+                string strValue = valueResult.FirstValue.Trim();
+                strValue = strValue.Replace(".", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+                strValue = strValue.Replace(",", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+                
+                result = Convert.ToDecimal(strValue, CultureInfo.CurrentCulture);
+                success = true;
+            }
+            catch (FormatException fe)
+            {
+                bindingContext.ModelState.AddModelError(bindingContext.ModelName, fe, bindingContext.ModelMetadata);
+            }
+            
+            if (success)
+            {
+                bindingContext.Result = ModelBindingResult.Success(result);
+            }
+        }
+
+
+        return Task.CompletedTask;
+    }
+}
